7b2391a417877d8facb7efb44682228a979908f3
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-27 13:08:45 -0400

    Faster provider for Vimeo

--- plugins/domains/vimeo.com.js
+++ plugins/domains/vimeo.com.js
@@ -1,10 +1,37 @@
 module.exports = {
 
     mixins: [
-        "og-title",
-        "twitter-player-responsive",
-        "og-image"
+        "oembed-title",
+        "oembed-thumbnail",
+        "oembed-author",
+        "oembed-duration",
+        "oembed-site",
+        "oembed-description"
     ],
+
+    getData: function(oembed) {
+
+        return {
+            canonical: "http://vimeo.com/" + oembed.video_id
+        };
+    },
+
+
+    getLink: function(oembed) {
+
+        return [{
+            href: "//player.vimeo.com/video/" + oembed.video_id,
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.player, CONFIG.R.iframely],
+            "aspect-ratio": oembed.width / oembed.height
+        }, {
+            href: "http://a.vimeocdn.com/images_v6/apple-touch-icon-72.png",
+            type: CONFIG.T.image,
+            rel: [CONFIG.R.icon, CONFIG.R.iframely],
+            width: 72,
+            height: 72   
+        }];
+    },
 
     tests: [{
         feed: "http://vimeo.com/channels/staffpicks/videos/rss"
