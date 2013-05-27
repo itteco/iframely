5dfa232d2f61b8fad9a5b0d1720bac906965ade7
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-27 13:09:48 -0400

    Initial for gogoyoko provider

---
+++ plugins/domains/gogoyoko.com.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    re: [
+        /^http:\/\/www\.gogoyoko\.com\/song\/([0-9\-]+)/i
+    ],
+
+    mixins: [
+        "image_src"
+    ],
+
+    getLink: function(meta) {
+
+        return {
+            title: meta['html-title'],
+            href: meta.video_src.replace('&autoPlaySong=1', ''),
+            type: meta.video_type,
+            rel: CONFIG.R.player,
+            width: meta.video_width,
+            height: meta.video_height
+        };        
+    },
+
+    tests: [
+        "http://www.gogoyoko.com/song/873990"
+    ]
+};
