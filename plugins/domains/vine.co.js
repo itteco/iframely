8f94fa67ada5203e99fba6076d045a19ed04de1d
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-27 13:09:01 -0400

    more meta for Vine

--- plugins/domains/vine.co.js
+++ plugins/domains/vine.co.js
@@ -2,14 +2,10 @@ module.exports = {
 
     mixins: [
         "twitter-image",
-        "twitter-player-responsive"
+        "twitter-player-responsive",
+        "og-title",
+        "og-site"
     ],
-
-    getData: function(meta) {
-        return {
-            title: meta.twitter.description
-        }
-    },
 
     tests: ["https://vine.co/v/bjHh0zHdgZT"]
 };
