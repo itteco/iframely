dfdf444b0a5346f8e09ff286ca6fd079088b0cc0
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-27 16:52:02 -0400

    Speeding up 9Gag (took >10sec before!)
    17ms now :)

--- plugins/domains/9gag.com.js
+++ plugins/domains/9gag.com.js
@@ -1,10 +1,24 @@
 module.exports = {
 
-    mixins: [
-        "og-title",
-        "og-image",
-        "twitter-image"
+    re: [
+        /^http:\/\/9gag\.com\/gag\/([a-z0-9\-]+)/i
     ],
+
+    // No other meta data is returned for 9gags as otherwise the response time would be more than 10sec
+    getData: function(urlMatch) {
+        return {
+            canonical: 'http:\\9gag.com/gag' + urlMatch[1]
+        }
+    },
+
+    getLink: function(urlMatch) {
+
+        return {
+            href: '//d24w6bsrhbeh9d.cloudfront.net/photo/' + urlMatch[1] + '_700b.jpg',
+            type: CONFIG.T.image,
+            rel: [CONFIG.R.image,CONFIG.R.iframely]
+        }
+    },
 
     tests: [ {
         page: "http://9gag.com",
