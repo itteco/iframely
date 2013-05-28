2374518885341e3074463392ba8ae326ede5cece
Nazar Leush <n.leush@gmail.com>
2013-05-28 20:13:45 +0300

    add imgur.com thumbnail 160x160

--- plugins/domains/imgur.com.js
+++ plugins/domains/imgur.com.js
@@ -10,6 +10,20 @@ module.exports = {
         "twitter-image-rel-image"
     ],
 
+    getLink: function(meta) {
+
+        var src;
+        if (meta.twitter && meta.twitter.image && (src = meta.twitter.image.url) && src.match(/\.jpg$/)) {
+            return {
+                href: src.replace(/\.jpg$/, "b.jpeg"),
+                rel: CONFIG.R.thumbnail,
+                type: CONFIG.T.image_jpeg,
+                width: 160,
+                height: 160
+            };
+        }
+    },
+
     tests: [{
         pageWithFeed: "http://imgur.com/"
     },
