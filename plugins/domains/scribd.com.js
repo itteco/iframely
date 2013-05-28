0813939ba9604704696b4e9e0cc3876a4f812a02
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-28 10:27:50 -0400

    [Domains] Add Scribd

---
+++ plugins/domains/scribd.com.js
@@ -0,0 +1,43 @@
+var jquery = require('jquery');
+
+module.exports = {
+
+    mixins: [
+        "oembed-title",
+        "oembed-thumbnail",
+        "oembed-site",
+        "oembed-author"
+    ],
+
+    getLink: function(oembed) {
+
+        var $container = jquery('<div>');
+        try {
+            $container.html(oembed.html);
+        } catch(ex) {}
+
+        var $iframe = $container.find('iframe');
+        var doc; 
+
+        if ($iframe.length == 1) {
+            doc = {
+                href: $iframe.attr('src'),
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.reader, CONFIG.R.iframely, CONFIG.R.oembed],
+                "aspect-ratio": $iframe.attr('data-aspect-ratio')
+            }
+        }
+
+        return [doc, {
+                href: '//www.scribd.com/favicon.ico',
+                type: CONFIG.T.image,
+                rel: [CONFIG.R.icon, CONFIG.R.iframely],
+        }]
+    },
+
+    tests: [
+        "http://www.scribd.com/doc/116154615/Australia-Council-Arts-Funding-Guide-2013"
+    ]
+
+
+};
