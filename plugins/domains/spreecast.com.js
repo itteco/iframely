972dd69cb99cca5a470d17dd98d72a5bb272b1c5
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-28 15:09:52 -0400

    [Domains] Spreecast added

---
+++ plugins/domains/spreecast.com.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+    re: [
+        /^https?:\/\/(?:www\.)?spreecast\.com\/events\/([a-z0-9\-]+)/i
+    ],
+
+    mixins: [
+        "og-title",
+        "og-image",
+        "og-description",
+        "favicon"
+    ],
+
+    getLink: function(urlMatch, meta) {
+
+        return [{
+            href: "http://www.spreecast.com/events/" + urlMatch[1] + "/embed-large",
+            type: CONFIG.T.text_html,
+            rel: CONFIG.R.player,
+            width: 900,
+            height: 470
+        }, {
+            href: "http://www.spreecast.com/events/" + urlMatch[1] + "/embed-medium",
+            type: CONFIG.T.text_html,
+            rel: CONFIG.R.player,
+            width: 500,
+            height: 470        	
+        }, {
+        	href: "https://d35ncmvcdy3liz.cloudfront.net/production/201305171505_88dc0c8/images/favicon.ico",
+        	type: CONFIG.T.image,
+        	rel: CONFIG.R.icon
+        }]
+    },
+
+    tests: [ 
+        "http://www.spreecast.com/events/how-to-invest-with-index-funds-and-etfs/"
+    ]
+};
