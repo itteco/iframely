0e5db0939e95bfcf52d8f529d8a81885715568f0
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-27 20:08:53 -0400

    Final BandCamp

--- plugins/domains/bandcamp.com.js
+++ plugins/domains/bandcamp.com.js
@@ -3,8 +3,8 @@ module.exports = {
     mixins: [
         "og-title",
         "og-description",
-        "og-video",
-        "og-image"
+        "og-image",
+        "favicon"
     ],
 
     getMeta: function(meta) {
@@ -12,6 +12,30 @@ module.exports = {
             site: "Bandcamp",
             author: meta.og.site_name
         };
+    },
+
+    getLink: function(meta) {
+
+        if (!meta.og.video.url) return;
+
+        var og_player_url = meta.og.video.url;
+        var id;
+
+        og_player_url.split('/').forEach(function(str) {
+            if (str.lastIndexOf('album', 0) === 0 || str.lastIndexOf('track', 0) === 0) {
+                id = str;
+            }
+        });
+
+        if (!id) return;
+        
+        return {
+            href: '//bandcamp.com/EmbeddedPlayer/v=2/' + id + '/size=venti/bgcol=FFFFFF/linkcol=4285BB/',
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.player, CONFIG.R.iframely],
+            width: 400,
+            height: 100
+        }
     },
 
     tests: [{
