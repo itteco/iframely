b158edba6b1cdd08ef2c5b35d8a91914b4b2f627
Nazar Leush <n.leush@gmail.com>
2013-05-28 19:22:36 +0300

    imgur.com image plugin

---
+++ plugins/domains/imgur.com.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    mixins: [
+        "twitter-title",
+        "description",
+        "canonical",
+        "keywords",
+
+        "favicon",
+        "twitter-image-rel-image"
+    ],
+
+    tests: [{
+        pageWithFeed: "http://imgur.com/"
+    },
+        "http://imgur.com/Ks3qs"
+    ]
+};
