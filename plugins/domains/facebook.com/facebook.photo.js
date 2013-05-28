1da73d76789c4ea84fb38bdb6091a7aa49b0852d
Nazar Leush <n.leush@gmail.com>
2013-05-28 18:42:24 +0300

    added facebook.photo test url

--- plugins/domains/facebook.com/facebook.photo.js
+++ plugins/domains/facebook.com/facebook.photo.js
@@ -19,5 +19,7 @@ module.exports = {
                 rel: CONFIG.R.image
             };
         }
-    }
+    },
+
+    tests: "http://www.facebook.com/photo.php?fbid=4253313542476&set=a.4253312782457.2159583.1574932468&type=1"
 };
