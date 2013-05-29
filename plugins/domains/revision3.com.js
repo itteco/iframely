014df63aa43550cb64abf0a6c500a8595dcc8d25
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-29 13:40:48 -0400

    [Domains] Added Revision3

---
+++ plugins/domains/revision3.com.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    mixins: [
+        "oembed-title",
+        "oembed-author",
+        "oembed-site",
+        "oembed-video-responsive",
+
+        "canonical",
+        "video",
+        "og-description",
+        "favicon"
+    ],
+
+    tests: [{
+        feed: "http://revision3.com/osalt"
+    },
+        "http://revision3.com/osalt/htpc-software"
+    ]
+};
