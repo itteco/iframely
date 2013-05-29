a3e18097a41d6c01d41b2090c8eaa5d40bb8eebe
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-29 19:11:47 -0400

    [Domains] Added Twitpic

---
+++ plugins/domains/twitpic.com.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    mixins: [
+        "canonical",
+        "og-site",
+        "og-title",
+        "og-image",
+        "author",
+        "favicon"
+    ],
+
+    getLink: function (meta) {
+
+        if (meta.twitter.image) return {
+            href: meta.twitter.image.url,
+            type: CONFIG.T.image,
+            rel: CONFIG.R.image,
+            width: meta.twitter.image.width,
+            height: meta.twitter.image.height
+        }
+    },
+
+    tests: [ 
+        "http://twitpic.com/8kors1"
+    ]
+};
