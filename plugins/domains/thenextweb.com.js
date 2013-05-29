7b74e7ca8d1200bbe81722d67be5ad7d0f7fe742
Nazar Leush <n.leush@gmail.com>
2013-05-29 14:35:03 +0300

    thenextweb.com plugin

---
+++ plugins/domains/thenextweb.com.js
@@ -0,0 +1,56 @@
+var jquery = require('jquery');
+
+module.exports = {
+
+    re: /^https?:\/\/thenextweb\.com\/(\w+\/)?\d{4}\/\d{2}\/\d{2}\/.*/i,
+
+    mixins: [
+        "author",
+        "canonical",
+        "og-description",
+        "keywords",
+        "shortlink",
+        "og-site",
+        "og-title",
+        "date",
+
+        "favicon"
+    ],
+
+    getLink: function(meta) {
+        return {
+            href: meta.og.image,
+            type: CONFIG.T.image,
+            rel: [CONFIG.R.thumbnail, CONFIG.R.og],
+            width: 300,
+            height: 250
+        };
+    },
+
+    getData: function($selector) {
+
+        var $content = $selector('.article-body');
+
+        if ($content.length) {
+            var html = $content.html();
+
+            var $image = $selector('.article-featured-image img');
+
+            if ($image.length) {
+                html = $image.parent().html() + html;
+            }
+
+            return {
+                html_for_readability: html,
+                ignore_readability_error: false
+            };
+        }
+    },
+
+    tests: [{
+        pageWithFeed: "http://thenextweb.com/"
+    },
+        "http://thenextweb.com/media/2013/04/15/creating-a-youtube-smash/?fromcat=all",
+        "http://thenextweb.com/insider/2013/04/15/e-commerce-startup-buyreply-raises-1m-seed-round-from-peter-thiels-firm-and-others/?fromcat=all"
+    ]
+};
--- plugins/generic/meta/date.js
+++ plugins/generic/meta/date.js
@@ -4,7 +4,7 @@ module.exports = {
 
     getMeta: function(meta) {
         return {
-            date: meta.date || meta.pubdate || meta.lastmod
+            date: meta.date || meta.pubdate || meta.lastmod || (meta.article && meta.article.published_time)
         };
     }
 };
