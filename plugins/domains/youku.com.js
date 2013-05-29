de29ab411b57f3b80c539d32d7f47ed8764e4bb2
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-29 17:39:59 -0400

    [Domains] Added Youku

---
+++ plugins/domains/youku.com.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    //http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842    
+    re: [
+        /^https?:\/\/v\.youku\.com\/v_show\/id_(\w{3,})\.html/i
+    ],
+
+    mixins: [
+        "html-title",
+        "description",
+        "keywords"
+    ],
+
+    getLink: function (urlMatch) {
+
+        return {
+            href: "http://player.youku.com/embed/"+ urlMatch[1],
+            type: CONFIG.T.text_html,            
+            rel: CONFIG.R.player
+        }
+    },
+
+    tests: [
+        "http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842"
+    ]
+};
