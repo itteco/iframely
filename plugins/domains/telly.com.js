f2d18082d22466053ee11a48fe3f5e18e6ad8c42
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-29 19:48:09 -0400

    [Domains] Added Telly

---
+++ plugins/domains/telly.com.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    //twitvid will re-direct here
+    mixins: [
+        "canonical",
+        "og-site",
+        "og-title",
+        "twitter-image",
+        "twitter-player-responsive",
+        "favicon"
+    ],
+
+    tests: [ 
+        "http://twitpic.com/8kors1",
+        "http://www.twitvid.com/POQOWF"
+    ]
+};
