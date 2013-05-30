978bebce84ebac02b9807d14a1f667b632ffb198
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-29 20:35:27 -0400

    [Domains] Added Dailymotion

---
+++ plugins/domains/dailymotion.com.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    mixins: [
+        "canonical",
+        "video",
+        "oembed-title",
+        "oembed-author",
+        "oembed-site",
+        "oembed-thumbnail",
+        "oembed-video-responsive",
+        "favicon"
+    ],
+
+    tests: [ 
+        "http://www.dailymotion.com/video/x10bix2_ircam-mani-feste-2013-du-29-mai-au-30-juin-2013_creation#.Uaac62TF1XV"
+    ]
+};
