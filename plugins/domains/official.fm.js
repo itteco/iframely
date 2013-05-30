ec711d5f711053963e33caef4310423c068aef66
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-30 11:49:31 -0400

    [Domains] Removed sites that shut down

---
+++ plugins/domains/official.fm.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    mixins: [
+        "og-title",
+        "og-image",
+        "canonical",
+
+        "twitter-player",
+        "favicon"
+    ],
+
+    tests: [
+        "http://official.fm/tracks/MJqX",
+        "http://official.fm/playlists/Jmpp"
+    ]
+};
