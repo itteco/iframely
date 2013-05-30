e855b8ab359cd7e33753ac121ed91d565d97f461
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-30 10:10:02 -0400

    [Domains] Kickstarter added

---
+++ plugins/domains/kickstarter.com.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    mixins: [
+        "og-title",
+        "og-description",
+        "canonical",
+        "oembed-site",
+        "oembed-author",
+        "shortlink",
+
+        "oembed-video-responsive",        
+        "og-image",
+        "favicon"
+    ],
+
+    tests: [
+        "http://www.kickstarter.com/projects/1104350651/taktik-premium-protection-system-for-the-iphone"
+    ]
+};
