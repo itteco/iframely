ab3e679cc5a1cb4c351fc916056b56c77528361d
Ivan Paramonau <i.paramonau@gmail.com>
2013-05-31 16:10:16 -0400

    [Domains] Screenr added

---
+++ plugins/domains/screenr.com.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    mixins: [
+        "oembed-title",
+        "oembed-thumbnail",
+        "oembed-author",
+        "oembed-site",
+        "oembed-video-responsive"
+    ],
+
+    getLink: function () {
+
+        return {
+            href: "http://www.screenr.com/public/images/apple-touch-icon.png",
+            rel: CONFIG.R.icon,
+            type: CONFIG.T.image
+        }
+    },
+
+    tests: [ {
+        pageWithFeed: "http://www.screenr.com/stream"
+    },
+        "http://www.screenr.com/embed/e4HH"
+    ]
+};
