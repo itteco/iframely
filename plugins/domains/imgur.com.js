4e589416d4a710ea8f4bc4a865229e424b4307b3
Ivan Paramonau <i.paramonau@gmail.com>
2013-06-12 11:00:04 -0400

    [domains] support gifs in Imgur

--- plugins/domains/imgur.com.js
+++ plugins/domains/imgur.com.js
@@ -13,11 +13,11 @@ module.exports = {
     getLink: function(meta) {
 
         var src;
-        if (meta.twitter && meta.twitter.image && (src = meta.twitter.image.url) && src.match(/\.(jpg|png)$/)) {
+        if (meta.twitter && meta.twitter.image && (src = meta.twitter.image.url) && src.match(/\.(jpg|png|gif)$/)) {
             return {
-                href: src.replace(/\.(jpg|png)$/, "b.$1"),
+                href: src.replace(/\.(jpg|png|gif)$/, "b.$1"),
                 rel: CONFIG.R.thumbnail,
-                type: CONFIG.T.image_jpeg,
+                type: CONFIG.T.image,
                 width: 160,
                 height: 160
             };
