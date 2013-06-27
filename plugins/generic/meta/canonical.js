8e53cd35dad33ce5bf7d50871606deddbaa911a5
Nazar Leush <n.leush@gmail.com>
2013-06-27 08:59:53 +0300

    [meta] use canonical.href if canonical.type specified

--- plugins/generic/meta/canonical.js
+++ plugins/generic/meta/canonical.js
@@ -2,7 +2,7 @@ module.exports = {
 
     getMeta: function(meta) {
         return {
-            canonical: meta.canonical || (meta.og && meta.og.url) || (meta.twitter && meta.twitter.url)
+            canonical: (meta.canonical && meta.canonical.href || meta.canonical) || (meta.og && meta.og.url) || (meta.twitter && meta.twitter.url)
         }
     }
 };
