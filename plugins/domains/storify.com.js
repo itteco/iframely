59a8b6815f3dd3cf86151cea5c983d5031bc20e6
Nazar Leush <n.leush@gmail.com>
2013-06-28 22:44:12 +0300

    [domain plugins] update sorify.com re's - add "_" to slug

--- plugins/domains/storify.com.js
+++ plugins/domains/storify.com.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/storify\.com\/([a-zA-Z0-9\-]+)\/([a-zA-Z0-9\-]+)/i
+        /^https?:\/\/storify\.com\/([a-zA-Z0-9_\-]+)\/([a-zA-Z0-9_\-]+)/i
     ],
 
     mixins: [
@@ -41,6 +41,7 @@ module.exports = {
     tests: [{
         feed: "http://storify.com/rss/featured"
     },
-        "https://storify.com/miniver/our-leaders-willfully-wrong-response-to-the-econom/"
+        "https://storify.com/miniver/our-leaders-willfully-wrong-response-to-the-econom/",
+        "http://storify.com/Kevin_H_Nielsen/nba-draft-live"
     ]
 };
