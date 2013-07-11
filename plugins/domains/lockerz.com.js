b3b9e990436c21397e278bffff6a4b138a0a5493
Nazar Leush <n.leush@gmail.com>
2013-07-11 16:45:44 +0300

    [domain plugins] lockerz meta.site

--- plugins/domains/lockerz.com.js
+++ plugins/domains/lockerz.com.js
@@ -12,6 +12,12 @@ module.exports = {
         "twitter-image-rel-image"
     ],
 
+    getMeta: function() {
+        return {
+            site: "Lockerz"
+        };
+    },
+
     tests: [ 
         "http://lockerz.com/u/patricia.varela/decalz/25781980/louis_3"
     ]
