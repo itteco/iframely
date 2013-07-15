914f05ef78d559c3c95283fb0434c0e32a37654c
Nazar Leush <n.leush@gmail.com>
2013-07-15 16:28:21 +0300

    [domain plugins] fix facebook.video plugin

---
+++ plugins/domains/facebook.com/facebook.video.js
@@ -0,0 +1,39 @@
+var qs = require('querystring');
+
+module.exports = {
+
+    re: [
+        /^https?:\/\/www\.facebook\.com\/video\/video\.php.*[\?&]v=(\d{5,})(?:$|&)/i,
+        /^https?:\/\/www\.facebook\.com\/photo\.php.*[\?&]v=(\d{5,})(?:$|&)/i,
+        /^https?:\/\/www\.facebook\.com\/video\/video\.php\?v=(\d{5,})$/i
+    ],
+
+    mixins: [
+        "favicon"
+    ],
+
+    getMeta: function(meta) {
+        return {
+            site: "Facebook",
+            title: meta["html-title"].replace(" | Facebook", "")
+        };
+    },
+
+    getLinks: function(urlMatch) {
+
+        return {
+            href: "//www.facebook.com/video/embed?video_id=" + urlMatch[1],
+            type: CONFIG.T.flash,
+            rel: CONFIG.R.player,
+            "aspect-ratio": 1.5
+        };
+    },
+
+    tests: [
+        "http://www.facebook.com/video/video.php?v=4253262701205&set=vb.1574932468&type=2",
+        "http://www.facebook.com/photo.php?v=4253262701205&set=vb.1574932468&type=2&theater",
+        {
+            noFeeds: true
+        }
+    ]
+};
