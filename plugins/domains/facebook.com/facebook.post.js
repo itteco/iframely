714fd7a55d4678603bd88e129affe033e9af3893
Nazar Leush <n.leush@gmail.com>
2013-08-06 11:37:03 +0300

    [domain plugins] facebook.post tests

--- plugins/domains/facebook.com/facebook.post.js
+++ plugins/domains/facebook.com/facebook.post.js
@@ -18,5 +18,14 @@ module.exports = {
             width: 552,
             height: 586
         }
-    }
+    },
+
+    tests: [
+        "https://www.facebook.com/noven.roman/posts/555607674475258",
+        "https://www.facebook.com/logvynenko/posts/10151487164961783",
+        "https://www.facebook.com/photo.php?fbid=530060777048531&set=a.215094428545169.62692.100001338405848&type=1",
+        {
+            noFeeds: true
+        }
+    ]
 };
