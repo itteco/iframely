e62abaaa413d4861af274529ed4267bfdfc4820a
Nazar Leush <n.leush@gmail.com>
2013-08-09 19:42:42 +0300

    [domain plugins] tumblr photo

--- plugins/domains/tumblr.com/tumblr.photo.js
+++ plugins/domains/tumblr.com/tumblr.photo.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 module.exports = {
 
     getLinks: function(tumblr_post) {
@@ -7,18 +9,31 @@ module.exports = {
         }
 
         var links = [];
-        tumblr_post.photos.forEach(function(photo) {
-            var title = photo.caption;
-            photo.alt_sizes.forEach(function(image, idx) {
-                links.push({
-                    title: title,
-                    href: image.url,
-                    type: CONFIG.T.image_jpeg,
-                    rel: idx == 0 ? CONFIG.R.image : CONFIG.R.thumbnail,
-                    width: image.width,
-                    height: image.height
-                });
+
+        function addImage(title, image, rel) {
+            links.push({
+                title: title,
+                href: image.url,
+                type: CONFIG.T.image,
+                rel: rel,
+                width: image.width,
+                height: image.height
             });
+        }
+
+        tumblr_post.photos.forEach(function(photo) {
+
+            var title = photo.caption;
+
+            addImage(title, photo.original_size, CONFIG.R.image);
+            var originalWidth = photo.original_size.width;
+
+            var image = _.find(photo.alt_sizes, function(image) {
+                if (image.width < originalWidth && image.width <= 400) {
+                    return true;
+                }
+            });
+            addImage(title, image, CONFIG.R.thumbnail);
         });
 
         return links;
