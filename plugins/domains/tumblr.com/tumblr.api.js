aee4851b95306194d7c63bfa4085755439596b6b
Nazar Leush <n.leush@gmail.com>
2013-08-09 11:54:33 +0300

    [domain plugins] generic tumblr meta

--- plugins/domains/tumblr.com/tumblr.api.js
+++ plugins/domains/tumblr.com/tumblr.api.js
@@ -1,15 +1,29 @@
+var $ = require('jquery');
+var _ = require('underscore');
+
 module.exports = {
 
     re: /^http:\/\/([a-z0-9-]+\.tumblr\.com)\/post\/(\d+)(?:\/[a-z0-9-]+)?/i,
 
     getMeta: function(tumblr_post) {
         return {
-            //title: tumblr_post.caption
+            title: $('<div>').html(tumblr_post.caption).text(),
+            site: 'tumblr',
+            author: tumblr_post.blog_name,
+            author_url: 'http://' + tumblr_post.blog_name + '.tumblr.com',
+            canonical: tumblr_post.post_url,
+            tags: _.unique([].concat(tumblr_post.tags, tumblr_post.featured_in_tag || [])).join(', '),
+            shortlink: tumblr_post.short_url,
+            date: tumblr_post.timestamp * 1000
         };
     },
 
+    mixins: [
+        "favicon"
+    ],
+
     getData: function(urlMatch, request, cb) {
-        
+
         request({
             uri: "http://api.tumblr.com/v2/blog/" + urlMatch[1] + "/posts",
             qs: {
