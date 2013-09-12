9fbdfc16721a60d375b9254e2866f22eea1db9e2
Ivan Paramonau <i.paramonau@gmail.com>
2013-09-12 14:37:55 -0400

    [Generic] Allow video_src without defined MIME type

--- plugins/generic/video_src.js
+++ plugins/generic/video_src.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-    getLink: function(meta) {
+    getLink: function (meta) {
 
         return {
             href: meta.video_src,
-            type: meta.video_type,
+            type: meta.video_type || CONFIG.T.text_html,
             rel: CONFIG.R.player,
             width: meta.video_width,
             height: meta.video_height
