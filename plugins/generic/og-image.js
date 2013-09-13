4cd194d4059d22d32377b2eb525bd08772f6511d
Nazar Leush <n.leush@gmail.com>
2013-09-13 11:05:47 +0300

    using whitelist.twitter.photo tag in twitter-image.js and og-image.js plugins

--- plugins/generic/og-image.js
+++ plugins/generic/og-image.js
@@ -2,23 +2,31 @@ var _ = require("underscore");
 
 module.exports = {
 
-    getLinks: function(meta) {
+    getLinks: function(meta, whitelistRecord) {
 
         if (!meta.og)
             return;
+
+        var rel;
+
+        if ((!meta.twitter || !meta.twitter.image) && whitelistRecord.twitter && whitelistRecord.twitter.photo) {
+            rel = [CONFIG.R.image, CONFIG.R.og, CONFIG.R.twitter];
+        } else {
+            rel = [CONFIG.R.thumbnail, CONFIG.R.og];
+        }
 
         if (meta.og.image instanceof Array) {
             return _.flatten(meta.og.image.map(function(image) {
                 return [{
                     href: image.url || image,
                     type: image.type || CONFIG.T.image,
-                    rel: [CONFIG.R.thumbnail, CONFIG.R.og],
+                    rel: rel,
                     width: image.width,
                     height: image.height
                 }, {
                     href: image.secure_url,
                     type: CONFIG.T.image,
-                    rel: CONFIG.R.thumbnail,
+                    rel: rel,
                     width: image.width,
                     height: image.height
                 }]
@@ -27,13 +35,13 @@ module.exports = {
             return [{
                 href: meta.og.image.url || meta.og.image,
                 type: meta.og.image.type || CONFIG.T.image,
-                rel: [CONFIG.R.thumbnail, CONFIG.R.og],
+                rel: rel,
                 width: meta.og.image.width,
                 height: meta.og.image.height
             }, {
                 href: meta.og.image.secure_url,
                 type: meta.og.image.type || CONFIG.T.image,
-                rel: [CONFIG.R.thumbnail, CONFIG.R.og],
+                rel: rel,
                 width: meta.og.image.width,
                 height: meta.og.image.height
             }];
@@ -41,7 +49,7 @@ module.exports = {
     },
 
     tests: [{
-        pageWithFeed: "http://digg.com/",
+        pageWithFeed: "http://digg.com/"
     }, {
         skipMethods: [
             "getLinks"
