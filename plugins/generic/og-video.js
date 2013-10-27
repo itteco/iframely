b6d1be1ec629f844c3132bcbd6d23a5f4d63ce9e
Ivan Paramonau <i.paramonau@gmail.com>
2013-10-26 23:38:24 -0400

    Allow og, twitter & html-meta players only if whitelisted

--- plugins/generic/og-video.js
+++ plugins/generic/og-video.js
@@ -1,22 +1,22 @@
 module.exports = {
 
-    getLinks: function(meta) {
+    getLinks: function(meta, whitelistRecord) {
 
-        if (!meta.og || !meta.og.video)
-            return;
-
-        return [{
-            href: meta.og.video.url || meta.og.video,
-            type: meta.og.video.type || CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.og],
-            width: meta.og.video.width,
-            height: meta.og.video.height
-        }, {
-            href: meta.og.video.secure_url,
-            type: meta.og.video.type || CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.og],
-            width: meta.og.video.width,
-            height: meta.og.video.height
-        }];
+        if (meta.og && meta.og.video && whitelistRecord.isAllowed && whitelistRecord.isAllowed('og.video')) {
+        
+            return [{
+                href: meta.og.video.url || meta.og.video,
+                type: meta.og.video.type || CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.og],
+                width: meta.og.video.width,
+                height: meta.og.video.height
+            }, {
+                href: meta.og.video.secure_url,
+                type: meta.og.video.type || CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.og],
+                width: meta.og.video.width,
+                height: meta.og.video.height
+            }];
+        }
     }
 };
