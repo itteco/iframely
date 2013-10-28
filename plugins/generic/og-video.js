8a024e319ba43accbfeb35e5326b39fb93bdb8bb
Ivan Paramonau <i.paramonau@gmail.com>
2013-10-28 14:47:50 -0400

    Don't send og secure_url if not in whitelist

--- plugins/generic/og-video.js
+++ plugins/generic/og-video.js
@@ -3,20 +3,26 @@ module.exports = {
     getLinks: function(meta, whitelistRecord) {
 
         if (meta.og && meta.og.video && whitelistRecord.isAllowed && whitelistRecord.isAllowed('og.video')) {
-        
-            return [{
+
+            var players = [{
                 href: meta.og.video.url || meta.og.video,
                 type: meta.og.video.type || CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.og],
                 width: meta.og.video.width,
                 height: meta.og.video.height
-            }, {
-                href: meta.og.video.secure_url,
-                type: meta.og.video.type || CONFIG.T.text_html,
-                rel: [CONFIG.R.player, CONFIG.R.og],
-                width: meta.og.video.width,
-                height: meta.og.video.height
-            }];
+            }]
+
+            if (whitelistRecord.isAllowed('og.video', 'ssl')) {
+                players.push({
+                    href: meta.og.video.secure_url,
+                    type: meta.og.video.type || CONFIG.T.text_html,
+                    rel: [CONFIG.R.player, CONFIG.R.og],
+                    width: meta.og.video.width,
+                    height: meta.og.video.height
+                })
+            }
+        
+            return players;
         }
     }
 };
