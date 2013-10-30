66c78c5c1f87d4149e67a9a999957e8ad5278494
Ivan Paramonau <i.paramonau@gmail.com>
2013-10-30 10:11:42 -0400

    Fix titles of blogs hosted on WordPress.com

--- plugins/generic/meta/meta-title.js
+++ plugins/generic/meta/meta-title.js
@@ -2,7 +2,8 @@ module.exports = {
 
     getMeta: function(meta) {
 
-        if (meta.title)
+        if (meta.title && meta.title.toLowerCase().indexOf('wordpress.com') == -1)
+        // Aparently, WordPress.com is using title field incorrectly.
         return {
             title: meta.title
         };
