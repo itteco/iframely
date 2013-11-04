a21e5f250a30285017a16ac144a12d1f6e7abf45
Ivan Paramonau <i.paramonau@gmail.com>
2013-11-04 13:52:22 -0500

    [Domains] Gawker Family: gawker.com, deadspin, lifehacker, jezebel, gizmodo, io9

---
+++ plugins/domains/gawker.js
@@ -0,0 +1,56 @@
+module.exports = {
+
+
+    re: [
+        /^http:\/\/lifehacker\.com\/[a-z0-9-]+/i,
+        /^http:\/\/(\w+\.)?gawker\.com\/[a-z0-9-]+/i,
+        /^http:\/\/jezebel\.com\/[a-z0-9-]+/i,
+        /^http:\/\/(\w+\.)?gizmodo\.com\/[a-z0-9-]+/i,
+        /^http:\/\/deadspin\.com\/[a-z0-9-]+/i,
+        /^http:\/\/io9\.com\/[a-z0-9-]+/i
+    ],
+
+
+    mixins: [
+        "twitter-image",
+        "favicon",
+        "twitter-author",
+        "canonical",
+        "og-description",
+        "keywords",
+        "og-site",
+        "og-title"
+    ],
+
+    getData: function(meta, $selector) {
+
+        var $html = $selector('div.post-content');
+
+        if ($html.length) {
+
+            var $image = $selector('span.img-border.mbs img');
+            var $iframe = $selector('span.flex-video iframe');
+
+            var html = '';
+
+            if ($image.length) {
+                html = $image.parent().html();
+            } else if ($iframe.length) {
+                html = $iframe.parent().html();
+            }
+
+            html += $html.html();
+
+            return {
+                readability_data: {
+                    html: html
+                }
+            };
+        }
+
+    },    
+
+    tests: [
+        "http://lifehacker.com/iframely-for-gmail-adds-inline-previews-to-links-in-gma-1441190492"
+    ]
+};
