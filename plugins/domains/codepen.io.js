23a8e7fb9333db12fbbb623343029d9dc60b8ed1
Ivan Paramonau <i.paramonau@gmail.com>
2013-11-15 12:29:54 -0500

    [Domains] Codepen.io

---
+++ plugins/domains/codepen.io.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+    re: /http:\/\/codepen\.io\/([a-z0-9\-]+)\/(pen|details)\/([a-z0-9\-]+)/i,
+
+    mixins: [
+        "twitter-image",
+        "favicon",
+        "canonical",
+        "twitter-description",
+        "og-site",
+        "twitter-title"
+    ],
+
+    getLink: function(urlMatch) {
+
+        return {
+            type: CONFIG.T.safe_html,
+            rel: CONFIG.R.reader,
+            html: '<p data-height="600" data-theme-id="0" data-slug-hash="' + urlMatch[3] + '" data-user="' + urlMatch[1] 
+                        + '" data-default-tab="result" class="codepen"></p><script async src="//codepen.io/assets/embed/ei.js"></script>',
+            height: 600
+        };
+    },
+
+    tests: [ {
+        pageWithFeed: "http://codepen.io/popular/",
+        selector: ".cover-link"
+    },
+        "http://codepen.io/kevinjannis/pen/pyuix",
+        "http://codepen.io/nosecreek/details/sprDl",
+        "http://codepen.io/dudleystorey/pen/HrFBx"
+    ]
+
+};
