02438f7abc58935ced85f60a494fa213dd685b1d
Ivan Paramonau <i.paramonau@gmail.com>
2013-11-18 10:27:48 -0500

    [General] Pull thumbnails from Twitter Gallery cards

--- plugins/generic/twitter-image.js
+++ plugins/generic/twitter-image.js
@@ -1,11 +1,12 @@
 module.exports = {
 
-    getLink: function(meta, whitelistRecord) {
+    getLinks: function(meta, whitelistRecord) {
 
         if (!meta.twitter || !meta.twitter.image)
             return;
 
         var rel;
+        var links = [];
 
         // TODO: make whitelistRecord.isAllowed always existing method?
         if (meta.twitter.card == "photo" && whitelistRecord.isAllowed && whitelistRecord.isAllowed('twitter.photo')) {
@@ -14,12 +15,27 @@ module.exports = {
             rel = CONFIG.R.thumbnail;
         }
 
-        return {
+        links.push({
             href: meta.twitter.image.url || meta.twitter.image.src || meta.twitter.image,
             type: CONFIG.T.image,
             rel: [rel, CONFIG.R.twitter],
             width: meta.twitter.image.width,
             height: meta.twitter.image.height
-        };
+        });
+
+        if (meta.twitter.card == "gallery") {
+            var i; // JSLint :\\
+
+            for (i=3; i>=0; i--) {
+                if (meta.twitter['image'+i]) 
+                    links.push({
+                        href: meta.twitter['image'+i].src || meta.twitter['image'+i],
+                        type: CONFIG.T.image,
+                        rel: [CONFIG.R.thumbnail, CONFIG.R.twitter],                        
+                    });
+            }
+        }
+
+        return links;
     }
 };
