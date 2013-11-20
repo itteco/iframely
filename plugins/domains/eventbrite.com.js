b4ad8219de4664d07781427217a36f0d169f76cc
Ivan Paramonau <i.paramonau@gmail.com>
2013-11-20 11:18:28 -0500

    [Domains] Fix Eventbrite according to new (+international) domain scheme

--- plugins/domains/eventbrite.com.js
+++ plugins/domains/eventbrite.com.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-    re: /^http:\/\/www\.eventbrite\.com\/event\/([0-9\-]+)/i,
+    // re: /^http:\/\/www\.eventbrite\.com\/event\/([0-9\-]+)/i,
+    re: /^https?:\/\/(\w+)?\.(eventbrite)\.(com|ca|fr|it|jp|es|co\.uk)\//i,
 
     mixins: [
         "canonical",
@@ -13,17 +14,27 @@ module.exports = {
         "favicon"
     ],
 
-    getLinks: function(urlMatch) {
+    getLinks: function(meta) {
 
-        return {
-            href: "http://www.eventbrite.com/tickets-external?v=2&eid=" + urlMatch[1],
-            type: CONFIG.T.text_html,
-            rel: CONFIG.R.reader,
-            "min-width": "500"
-        };
+        if (!meta.canonical || !meta.og || (meta.og && !meta.og.type == "eventbriteog:event")) return;
+
+        var eventId = meta.canonical.match(/\d+$/); // Number at the end of the string
+
+        if (eventId) {
+
+            return {
+                href: "//www.eventbrite.com/tickets-external?v=2&eid=" + eventId,
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.reader,
+                "min-width": "500"
+            };
+        }
     },
 
     tests: [
-        "http://www.eventbrite.com/event/4420461720?ref=etckt"
+        "http://womenshiftdigital.eventbrite.com",
+        "http://egyptdemocracy.eventbrite.com/",
+        "http://www.eventbrite.com/e/egypt-the-struggle-for-democracy-registration-9349712241?aff=estw",
+        "https://www.eventbrite.co.uk/e/the-top-30-charity-ceos-on-social-media-launch-tickets-9069048769"
     ]
 };
