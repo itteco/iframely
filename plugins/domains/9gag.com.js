b37e30e91ee1cdd042a92f64dde38c9176dff544
Ivan Paramonau <i.paramonau@gmail.com>
2014-01-13 11:39:48 -0500

    [Domains] Fix 9gag photos

--- plugins/domains/9gag.com.js
+++ plugins/domains/9gag.com.js
@@ -4,26 +4,15 @@ module.exports = {
         /^http:\/\/9gag\.com\/gag\/([a-z0-9\-]+)/i
     ],
 
-    // No other meta data is returned for 9gags as otherwise the response time would be more than 10sec
-    getMeta: function(urlMatch) {
-        return {
-            canonical: 'http://9gag.com/gag' + urlMatch[1]
-        };
-    },
-
-    getLink: function(urlMatch) {
-
-        return [{
-            href: '//d24w6bsrhbeh9d.cloudfront.net/photo/' + urlMatch[1] + '_700b.jpg',
-            type: CONFIG.T.image,
-            rel: CONFIG.R.image
-        }, {
-            href: '//d24w6bsrhbeh9d.cloudfront.net/static/main/core/20130208_1360298583/img/favicon_v2.png',
-            type: CONFIG.T.image,
-            rel: CONFIG.R.icon
-        }];
-    },
-
+    mixins: [
+        "twitter-image-rel-image",
+        "favicon",
+        "og-description",
+        "og-site",
+        "og-title",
+        "canonical"
+    ],
+    
     tests: [ {
         page: "http://9gag.com",
         selector: "h1 a.badge-section-link-target"
