843e7d687e9ba4634943921f3b9c8fe7ef8439fe
Ivan Paramonau <i.paramonau@gmail.com>
2014-01-13 13:11:44 -0500

    [Domains] Twitpic now supports videos

--- plugins/domains/twitpic.com.js
+++ plugins/domains/twitpic.com.js
@@ -11,16 +11,25 @@ module.exports = {
 
     getLink: function (meta) {
 
-        if (meta.twitter.image) return {
-            href: meta.twitter.image.url,
-            type: CONFIG.T.image,
-            rel: CONFIG.R.image,
-            width: meta.twitter.image.width,
-            height: meta.twitter.image.height
-        }
+        if (meta.twitter.card === "player") return {
+                href: meta.twitter.player.value || meta.twitter.player,
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.player,
+                width: meta.twitter.player.width,
+                height: meta.twitter.player.height
+            };
+
+        else if (meta.twitter.image) return {
+                href: meta.twitter.image.url,
+                type: CONFIG.T.image,
+                rel: CONFIG.R.image,
+                width: meta.twitter.image.width,
+                height: meta.twitter.image.height
+            }
     },
 
     tests: [ 
-        "http://twitpic.com/8kors1"
+        "http://twitpic.com/8kors1",
+        "http://twitpic.com/dfxeqo"
     ]
 };
