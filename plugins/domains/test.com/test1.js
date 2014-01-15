cadcabba533de6cd5889b97535eb6aa45ff159e1
Nazar Leush <n.leush@gmail.com>
2014-01-15 19:33:48 +0200

    add method timeout callback; replace some forEach to for loop

--- plugins/domains/test.com/test1.js
+++ plugins/domains/test.com/test1.js
@@ -2,12 +2,12 @@ module.exports = {
 
     provides: 'test_data1',
 
-    getData: function(meta, cb) {
+    getData: function(cb) {
         setTimeout(function() {
             cb(null, {
                 test_data1: true
             });
-        }, 1000);
+        }, 100);
     },
 
     tests: {
