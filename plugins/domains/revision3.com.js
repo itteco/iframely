da308c3636bb8734e886b1cb25e83eec199b7330
Ivan Paramonau <i.paramonau@gmail.com>
2014-01-27 10:38:31 -0500

    [Domains] restore responsiveness of Revision3

--- plugins/domains/revision3.com.js
+++ plugins/domains/revision3.com.js
@@ -1,16 +1,36 @@
+var jquery = require('jquery');
+
 module.exports = {
 
     mixins: [
         "oembed-title",
         "oembed-author",
         "oembed-site",
-        "oembed-video-responsive",
 
         "canonical",
         "video",
         "og-description",
         "favicon"
     ],
+
+    getLink: function(oembed) {
+
+        var $container = jquery('<div>');
+        try{
+            $container.html(oembed.html5 || oembed.html);
+        } catch(ex) {}
+
+        var $iframe = $container.find('iframe');
+
+        if ($iframe.length == 1) {
+            return {
+                href: $iframe.attr('src').replace(/(&width=[\d]+|&height=[\d]+)/g, ""),
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.oembed],
+                "aspect-ratio": oembed.width / oembed.height
+            }
+        }
+    },
 
     tests: [{
         pageWithFeed: "http://revision3.com/osalt"
