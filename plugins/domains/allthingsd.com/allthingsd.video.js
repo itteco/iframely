ab90253034c51474015c7b4f72cc3f186a854833
Ivan Paramonau <i.paramonau@gmail.com>
2014-01-30 10:47:03 -0500

    [domains] switch to aspect-ratio where it is responsive players now

--- plugins/domains/allthingsd.com/allthingsd.video.js
+++ plugins/domains/allthingsd.com/allthingsd.video.js
@@ -16,8 +16,7 @@ module.exports = {
             href: "http://live.wsj.com/public/page/embed-" + urlMatch[1].replace(/-/g, "_") + ".html",
             type: CONFIG.T.text_html,
             rel: CONFIG.R.player,
-            width: 512,
-            height: 288
+            "aspect-ratio": 512 / 288
         };
     },
 
