528eab534e8c30c458651bee6b38400442075c39
Ivan Paramonau <i.paramonau@gmail.com>
2014-02-25 09:56:53 -0500

    merge article plugins into one and return as `reader` `inline`

--- plugins/links/article.js
+++ plugins/links/article.js
@@ -1,10 +1,23 @@
 module.exports = {
 
 	notPlugin:  !(CONFIG.providerOptions.readability && CONFIG.providerOptions.readability.enabled === true),
+	provides: 'readability_flag',
 
-    getData: function(readability) {
+    getLink: function(readability) {
         return {
-            safe_html: readability.getHTML()
+            html: readability.getHTML(),
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.reader, CONFIG.R.inline]
         };
+    },
+
+    getData: function(meta) {
+
+    	if (meta.og && meta.og.type === "article") {
+
+	        return {
+	            readability_flag: true
+	        };
+    	}
     }
 };
