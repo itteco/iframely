346e3f1b91db053081d927a6c7bfa58afa661c4a
Nazar Leush <n.leush@gmail.com>
2014-02-26 20:35:28 +0200

    return og-video and twitter-player whitelist check

--- plugins/links/og-video.js
+++ plugins/links/og-video.js
@@ -2,9 +2,7 @@ module.exports = {
 
     getLinks: function(og, whitelistRecord) {
 
-        if (og.video) {
-
-        //if (og.video && whitelistRecord.isAllowed && whitelistRecord.isAllowed('og.video')) {
+        if (og.video && (!whitelistRecord || (whitelistRecord.isAllowed && whitelistRecord.isAllowed('og.video')))) {
 
             var players = [{
                 href: og.video.url || og.video,
@@ -12,16 +10,16 @@ module.exports = {
                 rel: [CONFIG.R.player, CONFIG.R.og],
                 width: og.video.width,
                 height: og.video.height
-            }]
+            }];
 
-            if (whitelistRecord && whitelistRecord.isAllowed('og.video', 'ssl')) {
+            if (!whitelistRecord || whitelistRecord.isAllowed('og.video', 'ssl')) {
                 players.push({
                     href: og.video.secure_url,
                     type: og.video.type || CONFIG.T.text_html,
                     rel: [CONFIG.R.player, CONFIG.R.og],
                     width: og.video.width,
                     height: og.video.height
-                })
+                });
             }
         
             return players;
