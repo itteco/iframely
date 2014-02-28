17e38586a09ad01d0ae8cb1b204cf6ab46ab91a3
Nazar Leush <n.leush@gmail.com>
2014-02-28 18:19:10 +0200

    images goes before reader in response

--- config.js
+++ config.js
@@ -42,8 +42,8 @@
             "app",
             "player",
             "survey",
-            "reader",
             "image",
+            "reader",
             "thumbnail",
             "logo",
             "icon"
