a6a1bdb562e0be5ceb8414bb6badad7a6933379a
Ivan Paramonau <i.paramonau@gmail.com>
2014-03-14 10:55:50 -0400

    [Domains] Added GettyImages. Yay!

---
+++ plugins/domains/gettyimages.com.js
@@ -0,0 +1,63 @@
+var jquery = require('jquery');
+
+module.exports = {
+
+    // Photos only for now. TODO: Stay tuned for when video embeds become available
+    re: /^https?:\/\/www\.gettyimages\.(com|ca|com\.au|be|dk|de|es|fr|in|ie|it|nl|co\.nz|no|at|pt|ch|fi|se|ae|co\.uk|co\.jp)\/detail\/photo\/[a-z0-9\-]+\/(\d+)/i,
+
+    provides: 'getty',
+
+    mixins: [
+        "og-image",
+        "favicon",
+        "canonical",
+        "og-description",
+        "og-site",
+        "twitter-title"
+    ],
+
+    getData: function(urlMatch, request, cb) {
+
+        request({
+            uri: "http://embed.gettyimages.com/preview/" + urlMatch[2],
+            json: true,
+            limit: 1, 
+            timeout: 1000
+        }, function(error, response, body) {
+            
+            if (error) return cb(error);
+
+            cb(null, {
+                getty: body
+            });
+        });
+    },
+
+    getLink: function(getty) {
+
+
+        var $container = jquery('<div>');
+        try {
+            $container.html(getty.embedTag);
+        } catch (ex) {}
+
+        var $iframe = $container.find('iframe');
+
+
+        // if embed code contains <iframe>, return src
+        if ($iframe.length == 1) {
+            
+            return {
+                href: $iframe.attr('src'),
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.image,
+                "aspect-ratio": $iframe.attr('width') / $iframe.attr('height')
+            };
+
+        }
+    },
+
+    tests: [
+        "http://www.gettyimages.ca/detail/photo/reflection-of-trees-high-res-stock-photography/103260792"
+    ]
+};
