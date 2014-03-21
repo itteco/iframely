33e363a3d8edea983608b9d6fbc42886eb3a4ece
Ivan Paramonau <i.paramonau@gmail.com>
2014-03-21 15:28:11 -0400

    [General] whitelist all Hosted YouTube in open graph

---
+++ plugins/links/og-youtube.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    getLink: function(og) {
+
+        if (!(og.video || og.video.url)) return;
+        
+        var video_src = og.video.url || og.video;
+
+        var urlMatch = video_src.match(/^https?:\/\/www\.youtube\.com\/v\/([\-_a-zA-Z0-9]+)/i)
+                    || video_src.match(/^https?:\/\/www\.youtube-nocookie\.com\/v\/([\-_a-zA-Z0-9]+)/i)
+                    || video_src.match(/^https?:\/\/www\.youtube\.com\/embed\/([\-_a-zA-Z0-9]+)/i)
+                    || video_src.match(/^https?:\/\/youtube\.googleapis\.com\/v\/([\-_a-zA-Z0-9]+)/i) //youtube.googleapis.com/v/k3Cd2lvQlN4?rel=0
+                    || video_src.match(/https?:\/\/www.youtube\.com\/watch\?v=([\-_a-zA-Z0-9]+)/i);
+
+
+        if (urlMatch) {
+
+            var params = (CONFIG.providerOptions.youtube && CONFIG.providerOptions.youtube.get_params) ? CONFIG.providerOptions.youtube.get_params : "";
+
+            return {
+                href: "https://youtube.com/embed/" + urlMatch[1] + params,
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.player,
+                "aspect-ratio": (og.video.height && og.video.width) ? og.video.width / og.video.height : 4/3
+            }
+
+        }
+    }
+};
