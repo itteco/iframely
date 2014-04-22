7c163d92b3ccd68f9ec038385962d860db3c13f6
Ivan Paramonau <i.paramonau@gmail.com>
2014-04-22 18:30:54 -0400

    [domains] fix telly & fiddler tests

--- plugins/domains/telly.com.js
+++ plugins/domains/telly.com.js
@@ -9,7 +9,6 @@ module.exports = {
         "og-site",
         "og-title",
         "twitter-image",
-        "twitter-player-responsive",
         "favicon"
     ],
 
@@ -17,12 +16,13 @@ module.exports = {
         return {
             href: "https://telly.com/embed.php?guid=" + urlMatch[1] + "&autoplay=0",
             type: CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.twitter],
-            "aspect-ratio": meta.twitter.player.width / meta.twitter.player.height
+            rel: CONFIG.R.player,
+            "aspect-ratio": 480/360
         };
     },
 
     tests: [
-        "http://telly.com/Q84DI7"
+        "http://telly.com/Q84DI7",
+        "http://telly.com/1AUHUCN"
     ]
 };
