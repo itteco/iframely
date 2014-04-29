2a6a55d8cd17b828dbf5225ebbb8c8a892ac1ece
Ivan Paramonau <i.paramonau@gmail.com>
2014-04-28 21:10:08 -0400

    [domains] simplify Imgur (they fixed kittens in oEmbed)

--- plugins/domains/imgur.com.js
+++ plugins/domains/imgur.com.js
@@ -1,11 +1,15 @@
+var jquery = require('jquery');
+
 module.exports = {
 
     re: /https?:\/\/imgur\.com\/(?:\w+\/)?(\w+).*/i,
 
     mixins: [
         "favicon",
-        "oembed-video-responsive",
-        "og-site"
+        "canonical",
+        "oembed-author",
+        "twitter-image", // both as fall back, and as thumbnails for galleries
+        "oembed-site"
     ],
 
     getMeta: function(meta) {
@@ -14,53 +18,48 @@ module.exports = {
         };
     },
 
-    getLink: function(oembed, meta) {
+    getLink: function(oembed) {
 
-        var links = [];
+        if (oembed.type === "photo" && oembed.url) {
 
-        // processing photos. 
-        // But in some cases, oembed photo shows the kitten instead of proper image. 
-        // In this cases - fall back to twitter photo
-        if (oembed.type === "photo" && oembed.url && oembed.url === meta.twitter.image.url) {
-
-            links.push({
+            return {
                 href: oembed.url.replace("http://", "//"),
                 type: CONFIG.T.image,
                 rel: [CONFIG.R.image, CONFIG.R.thumbnail, CONFIG.R.oembed],
                 width: oembed.width,
                 height: oembed.height
-            });
-
-        } else if (meta.twitter.image && meta.twitter.image.url) { // the kitten!
-
-            links.push({
-                href: meta.twitter.image.url.replace("http://", "//"),
-                type: CONFIG.T.image,
-                rel: [CONFIG.R.image, CONFIG.R.thumbnail, CONFIG.R.twitter],
-                width: meta.twitter.image.width,
-                height: meta.twitter.image.height
-            });
-
-        } else { // likely a gallery, push thumbnail
-
-            links.push({
-                href: meta.image_src,
-                type: CONFIG.T.image,
-                rel: [CONFIG.R.thumbnail, CONFIG.R.og],
-            });
+            };
         }
 
-        return links;
+
+        if (oembed.type == "rich") {
+            var $container = jquery('<div>');
+            try{
+                $container.html(oembed.html5 || oembed.html);
+            } catch(ex) {}
+
+            var $iframe = $container.find('iframe');
+
+            if ($iframe.length == 1) {
+                return {
+                    href: $iframe.attr('src').replace("http://","//"),
+                    type: CONFIG.T.text_html,
+                    rel: [CONFIG.R.player, CONFIG.R.oembed],
+                    "aspect-ratio": oembed.width / oembed.height
+                };
+            }
+        }
     },    
 
     tests: [{
         pageWithFeed: "http://imgur.com/"
     }, {
-        skipMixins: ["oembed-video-responsive"]
+        skipMixins: ["oembed-author"]       // Available for Galleries only
     },    
         "http://imgur.com/Ks3qs",
         "http://imgur.com/gallery/IiDwq",
+        "http://imgur.com/a/yMoaT",
         "https://imgur.com/gallery/B3X48s9",
-        "http://imgur.com/r/aww/tFKv2zQ" // kitten bomb
+        "http://imgur.com/r/aww/tFKv2zQ"    // kitten bomb before, doesn't seem to show up any longer
     ]
 };
