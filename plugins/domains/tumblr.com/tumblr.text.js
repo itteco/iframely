277ab6bb9bc24794fae9d9e987a16c648c084f90
Ivan Paramonau <i.paramonau@gmail.com>
2014-04-28 22:26:37 -0400

    [domains] thumbnail for articles on Tumblr

--- plugins/domains/tumblr.com/tumblr.text.js
+++ plugins/domains/tumblr.com/tumblr.text.js
@@ -7,7 +7,24 @@ module.exports = {
         /^https?:\/\/([a-z-\.]+)\/(post|post)\/(\d{11})(?:\/[a-z0-9-]+)?/i
     ],
 
-    getData: function(tumblr_post) {
+    getLink: function (tumblr_post) {
+        if (tumblr_post.thumbnail_url) {
+            return;
+        };
+
+        var $post = jquery('<div>').html(tumblr_post.body);
+        var $images = $post.find('img');
+
+                    // Could be more than 1 image, true. 
+        return {    // But the response time will be unacceptable as post-processing will check alll image sizes.
+            href: $images[0].src,
+            title: $images[0].alt,
+            type: CONFIG.T.image,
+            rel: CONFIG.R.thumbnail
+        };
+    },
+
+    getData: function (tumblr_post) {
 
         if (tumblr_post.type !== "text") {
 
@@ -21,8 +38,8 @@ module.exports = {
     },
 
     tests: [
+        "http://blog.dribbble.com/post/83410866615",
         "http://tiffany-anc.tumblr.com/post/58056730720/truth-what-filthy-thought-are-you-thinking-right-now",
-        "http://starsshinetoobrightly.tumblr.com/post/58055596742/17",
         "http://asimpleweirdlass.tumblr.com/post/58054585454/nakakatakot-kanina-ang-dilim-sa-street-tapos",
         "http://soupsoup.tumblr.com/post/41952443284/think-of-yourself-less-of-a-journalist-and-more"
     ]
