ec12910fa4eb73dc976a9e5d0700124fa5234d8d
Nazar Leush <n.leush@gmail.com>
2014-05-21 17:39:45 +0300

    improve og-image/video-responsive plugins

--- plugins/custom/og-video-responsive.js
+++ plugins/custom/og-video-responsive.js
@@ -1,16 +1,31 @@
+var _ = require("underscore");
+
+function getVideoLinks(video) {
+
+    return [{
+        href: video.url || video,
+        type: video.type || CONFIG.T.text_html,
+        rel: [CONFIG.R.player, CONFIG.R.og],
+        "aspect-ratio": video.width / video.height
+    }, {
+        href: video.secure_url,
+        type: video.type || CONFIG.T.text_html,
+        rel: [CONFIG.R.player, CONFIG.R.og],
+        "aspect-ratio": video.width / video.height
+    }];
+}
+
 module.exports = {
 
     getLinks: function(og) {
-        return [{
-            href: og.video.url || og.video,
-            type: og.video.type || CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.og],
-            "aspect-ratio": og.video.width / og.video.height
-        }, {
-            href: og.video.secure_url,
-            type: og.video.type || CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.og],
-            "aspect-ratio": og.video.width / og.video.height
-        }];
+
+        if (og.video instanceof Array) {
+
+            return _.flatten(og.video.map(getVideoLinks));
+
+        } else if (og.video) {
+
+            return getVideoLinks(og.video);
+        }
     }
 };
