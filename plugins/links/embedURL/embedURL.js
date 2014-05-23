ec19ca00db8bf06b99d25a487724e98495d5a9f3
Nazar Leush <n.leush@gmail.com>
2014-05-23 19:38:21 +0300

    find single http://schema.org/VideoObject micro format object and parse it without inner objects

--- plugins/links/embedURL/embedURL.js
+++ plugins/links/embedURL/embedURL.js
@@ -1,18 +1,57 @@
 module.exports = {
 
-    getLink: function(cheerio, __allowEmbedURL) {
+    provides: 'microformatVideoObject',
 
-        var $el = cheerio('[itemprop="embedURL"]');
-        if ($el.length) {
-            var uri = $el.attr('content') || $el.attr('href');
+    getData: function(cheerio, __allowEmbedURL) {
 
-            if (uri) {
-                return {
-                    href: uri,
-                    type: CONFIG.T.text_html,
-                    rel: CONFIG.R.player
-                };
-            }
+        var videoObjectSchema = 'http://schema.org/VideoObject';
+
+        var $scope = cheerio('[itemscope][itemtype="' + videoObjectSchema + '"]');
+
+        if ($scope.length) {
+
+            var $aScope = cheerio($scope);
+
+            var result = {};
+
+            $aScope.find('[itemprop]').each(function() {
+                var $el = cheerio(this);
+
+                var scope = $el.attr('itemscope');
+                if (typeof scope !== 'undefined') {
+                    return;
+                }
+
+                var $parentScope = $el.parents('[itemscope]');
+                if ($parentScope.attr('itemtype') !== videoObjectSchema) {
+                    return;
+                }
+
+                var key = $el.attr('itemprop');
+                if (key) {
+                    var value = $el.attr('content') || $el.attr('href');
+                    result[key] = value;
+                }
+            });
+
+            return {
+                microformatVideoObject: result
+            };
+        }
+    },
+
+    // TODO: parse duration
+
+    getLink: function(microformatVideoObject) {
+
+        if (microformatVideoObject.embedURL) {
+            return {
+                href: microformatVideoObject.embedURL,
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.player,
+                width: microformatVideoObject.width,
+                height: microformatVideoObject.height
+            };
         }
     }
 };
