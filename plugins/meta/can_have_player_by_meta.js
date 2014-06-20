9a06264c73755c584c531ef37c9e1ef8549d412a
Nazar Leush <n.leush@gmail.com>
2014-06-20 20:12:27 +0300

    add fuzzy detect video plugin

---
+++ plugins/meta/can_have_player_by_meta.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    notPlugin: true,
+
+    getMeta: function(meta) {
+
+        var has = false;
+
+        if (meta.og) {
+            if (meta.og.video || (meta.og.type && meta.og.type.indexOf('video') === 0)) {
+                has = true;
+            }
+        }
+        if (meta.twitter) {
+            if (meta.twitter.player || meta.twitter.stream) {
+                has = true;
+            }
+        }
+
+        if (has) {
+            return {
+                can_have_player: true
+            };
+        }
+    }
+};
