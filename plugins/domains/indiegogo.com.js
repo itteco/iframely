0d145f876eba89b4875f7774c0dd9f36fac65881
Nazar Leush <n.leush@gmail.com>
2014-07-04 18:06:44 +0300

    [domains] indiegogo.com

---
+++ plugins/domains/indiegogo.com.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    re: /^https:\/\/www\.indiegogo\.com\/projects\/([\w-]+)/i,
+
+    mixins: [
+        "og-site",
+        "og-image",
+        "image_src",
+        "canonical",
+        "favicon",
+        "twitter-description",
+        "keywords",
+        "twitter-title"
+    ],
+
+    getLink: function(urlMatch) {
+        return {
+            href: 'https://www.indiegogo.com/project/' + urlMatch[1] + '/embedded',
+            type: CONFIG.T.text_html,
+            rel: CONFIG.R.app,
+            width: 222,
+            height: 445
+        };
+    },
+
+    tests: [{
+        pageWithFeed: 'https://www.indiegogo.com'
+    },
+        "https://www.indiegogo.com/projects/the-intel-80386-and-80486/"
+    ]
+};
