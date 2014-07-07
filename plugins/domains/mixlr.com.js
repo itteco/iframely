4faf9dc82cd99537b85484196cd3dbc8efe4c565
Nazar Leush <n.leush@gmail.com>
2014-07-07 17:54:27 +0300

    [domains] mixlr.com

---
+++ plugins/domains/mixlr.com.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+    mixins: [
+        "og-image",
+        "twitter-image",
+        "favicon",
+        "canonical",
+        "twitter-description",
+        "keywords",
+        "media-detector",
+        "twitter-title"
+    ],
+
+    getLinks: function(og, twitter) {
+        return [{
+            href: twitter.player.value,
+            type: CONFIG.T.text_html,
+            rel: CONFIG.R.player,
+            height: 185
+        }, {
+            href: og.video.url,
+            type: CONFIG.T.flash,
+            rel: [CONFIG.R.player, CONFIG.R.autoplay],
+            height: 185
+        }];
+    },
+
+    tests: [{
+        page: 'http://mixlr.com/live/acoustic-folk/',
+        selector: 'a.profile_image'
+    },
+        "http://mixlr.com/voice-of-charity/"
+    ]
+};
