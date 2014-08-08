0052b3c331dcfe096a242b86253e294bb17ee861
Ivan Paramonau <i.paramonau@gmail.com>
2014-08-08 13:48:33 -0400

    [general] fix embedURL dependency on whitelist and also allow httpS://schema.org links

--- plugins/links/embedURL/checkEmbedURL.js
+++ plugins/links/embedURL/checkEmbedURL.js
@@ -2,12 +2,12 @@ module.exports = {
 
     provides: '__allowEmbedURL',
 
-    getData: function(whitelistRecord) {
+    getData: function(meta, whitelistRecord) {
 
     	// Allow slow cheerio parser, if whitelisted, 
     	// But do not call, if there is twitter:player as it's usually the best one
         if (whitelistRecord && whitelistRecord.isAllowed('html-meta.embedURL') 
-        					&& !(whitelistRecord.isAllowed('twitter.player'))) {
+        					&& !(whitelistRecord.isAllowed('twitter.player') && meta.twitter && meta.twitter.card == "player")) {
             return {
                 __allowEmbedURL: true
             };
