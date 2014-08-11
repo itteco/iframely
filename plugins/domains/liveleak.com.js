4244b67751c3ee2be135ea497e29895944ec8ca4
Ivan Paramonau <i.paramonau@gmail.com>
2014-08-11 11:43:05 -0400

    [plugins] exclude hosted YT videos from Liveleak

--- plugins/domains/liveleak.com.js
+++ plugins/domains/liveleak.com.js
@@ -13,10 +13,15 @@ module.exports = {
         "favicon"
     ],
 
-    getLink: function (meta) {
+    getLink: function (og) {
 
         // http://edge.liveleak.com/80281E/u/u/thumbs/2013/Jun/4/5d54790ff19a_sf_12.jpg
-        var image_str = meta.og.image.split('/');
+        // For embedded videos (youtube) - 
+        // http://edge.liveleak.com/80281E/s/s/17/media17/2014/Jan/13/e477ff3a60ae_embed_thumbnail_... 
+        // So we can tell that if og:image contains "embed" - then it's not a hosted video
+        if (!og.image || og.image.indexOf ("embed") > -1) return;
+
+        var image_str = og.image.split('/');
         var image_name = image_str[image_str.length-1];
 
         var video_href = image_name.split('_')[0];
