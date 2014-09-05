f3006e46bded71d71e756cd2f588c4539a734d9f
Nazar Leush <n.leush@gmail.com>
2014-09-05 17:34:57 +0300

    [plugins] check ssl whitelist tag for oembed video

--- plugins/custom/oembed-video-responsive.js
+++ plugins/custom/oembed-video-responsive.js
@@ -2,7 +2,7 @@ var jquery = require('jquery');
 
 module.exports = {
 
-    getLink: function(oembed) {
+    getLink: function(oembed, whitelistRecord) {
 
         var $container = jquery('<div>');
         try{
@@ -12,12 +12,19 @@ module.exports = {
         var $iframe = $container.find('iframe');
 
         if ($iframe.length == 1) {
+
+            var href = $iframe.attr('src');
+
+            if (whitelistRecord && (whitelistRecord.isAllowed('oembed.video', 'ssl') || whitelistRecord.isAllowed('oembed.rich', 'ssl'))) {
+                href = href.replace(/^https?:\/\//i, '//');
+            }
+
             return {
-                href: $iframe.attr('src'),
+                href: href,
                 type: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.oembed, CONFIG.R.html5],
                 "aspect-ratio": oembed.width / oembed.height
-            }
+            };
         }
     }
 };
