ae558fb3363fa709ea4951af9df2eee35375c1b7
Ivan Paramonau <i.paramonau@gmail.com>
2014-09-16 12:49:44 -0400

    [domains] return slideshow view for Storify - reader has same image hotlinks anyway

--- plugins/domains/storify.com.js
+++ plugins/domains/storify.com.js
@@ -7,8 +7,7 @@ module.exports = {
     mixins: [
         "og-site",
         "og-image",
-        // apparently, Storify is hot-linking Twitter images, which are fine on twitter.com, but are not allowed on other domains.
-        // "twitter-player-responsive",
+        "twitter-player-responsive",
         "favicon"
     ],
 
