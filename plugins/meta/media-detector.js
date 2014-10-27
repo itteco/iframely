2a8c044e30436e4eac70192dd800dd35447da14b
Ivan Paramonau <i.paramonau@gmail.com>
2014-10-27 12:40:50 -0400

    [generic] reader media detector based on WP url slugs

--- plugins/meta/media-detector.js
+++ plugins/meta/media-detector.js
@@ -39,7 +39,10 @@ module.exports = {
 
         if (has_thumbnail) {
 
-            if (/article|blog|news|post|noticia/i.test(url) || (meta.og && meta.og.type && typeof meta.og.type === 'string' && meta.og.type.match(/article|post/i))) {
+            if (/article|blog|news|post|noticia/i.test(url) 
+                || (/\/(\d{4})\/(\d{2})\/(\d{2})/).test(url) 
+                || (meta.og && meta.og.type && typeof meta.og.type === 'string' && meta.og.type.match(/article|post/i))) {
+                
                 has_reader = true;
             }
         }
