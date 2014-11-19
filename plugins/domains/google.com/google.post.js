582d97ba6a60c9dec08ae46cb8cbf3d49c59f81a
Ivan Paramonau <i.paramonau@gmail.com>
2014-11-19 14:47:28 -0500

    [domains] Google Plus re-directs - finally under control

--- plugins/domains/google.com/google.post.js
+++ plugins/domains/google.com/google.post.js
@@ -5,9 +5,13 @@ module.exports = {
     https://developers.google.com/+/web/embedded-post/
     */
 
+    provides: "google_user_id",
+
     re: [
-        /https:\/\/plus\.google\.com\/(\d+)\/posts\/(\w+)/i,
-        /https:\/\/plus\.google\.com\/u\/0\/(\d+)\/posts\/(\w+)/i
+        /^https:\/\/plus\.google\.com\/(\d+)\/posts\/(\w+)/i,
+        /^https:\/\/plus\.google\.com\/u\/0\/(\d+)\/posts\/(\w+)/i,
+        /^https:\/\/plus\.google\.com\/(\+\w+)\/posts\/(\w+)/i, // e.g. https://plus.google.com/+UmarHansa/posts/2m8yF7aFonn
+        /^https:\/\/plus\.google\.com\/u\/0\/(\+\w+)\/posts\/(\w+)/i
     ],
 
 
@@ -18,7 +22,7 @@ module.exports = {
     ],
 
 
-    getLink: function(url, meta) {
+    getLink: function(urlMatch, google_user_id, meta) {
 
         var title = meta['html-title'];
 
@@ -27,7 +31,7 @@ module.exports = {
             rel: [CONFIG.R.app, CONFIG.R.inline, CONFIG.R.ssl],
             template_context: {
                 title: title,
-                uri: url.replace("u/0/", "")
+                uri: "https://plus.google.com/" + google_user_id + "/posts/" + urlMatch[2]
             },
             width: 440,
             height: 440
@@ -35,8 +39,35 @@ module.exports = {
 
     },
 
+    getData: function (urlMatch, cheerio) { 
+
+        if (urlMatch[1].indexOf ('+') == -1 ) {
+
+            return {
+                google_user_id: urlMatch[1]
+            }
+
+        } else {
+
+            var $author = cheerio('[oid][rel*=author]');
+
+            if ($author.length && $author.attr('oid')) {
+                return {
+                    google_user_id: $author.attr('oid')
+                };
+            }
+
+
+        }
+
+
+    },
+
     tests: [
         "https://plus.google.com/106601272932768187104/posts/iezWej2wQmw",
+        "https://plus.google.com/+UmarHansa/posts/2m8yF7aFonn",
+        "https://plus.google.com/u/0/106507463771539639056/posts/1t7Ga54vkrw",
+        "https://plus.google.com/110174288943220639247/posts/cfjDgZ7zK8o",
         {
             skipMixins: ["description"]
         }
