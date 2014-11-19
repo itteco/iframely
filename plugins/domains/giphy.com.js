f03b70232aa85ad8e41deca8c5778494ce048a7c
Nazar Leush <n.leush@gmail.com>
2014-11-19 16:58:13 +0200

    [domains] fix giphy.com

--- plugins/domains/giphy.com.js
+++ plugins/domains/giphy.com.js
@@ -17,8 +17,10 @@ module.exports = {
 
     getLinks: function(urlMatch) {
 
+        var id = urlMatch[1].split('-').slice(-1)[0];
+
         // http://media.giphy.com/media/YJ88JyDL61jeo/original.gif
-        var original = "http://media.giphy.com/media/" + urlMatch[1] + "/giphy.gif";
+        var original = "http://media.giphy.com/media/" + id  + "/giphy.gif";
 
         return {
             href: original,
@@ -27,7 +29,10 @@ module.exports = {
         };
     },
 
-    tests: [
-        "http://giphy.com/gifs/YJ88JyDL61jeo"
+    tests: [{
+        page: 'http://giphy.com',
+        selector: '.gif-link'
+    },
+        "http://giphy.com/gifs/emma-stone-kiss-oHBlKX1wbIye4"
     ]
 };
