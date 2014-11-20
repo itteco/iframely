0ad56a2afbd2a12d54d121092ab678046b5a2f10
Ivan Paramonau <i.paramonau@gmail.com>
2014-11-20 12:39:04 -0500

    [general] skip parsley keywords, if it's empty

--- plugins/meta/parsely_page.js
+++ plugins/meta/parsely_page.js
@@ -27,7 +27,7 @@ module.exports = {
             title: parsely_page.title,
             date: parsely_page.pub_date,
             author: parsely_page.author,
-            keywords: parsely_page.tags && parsely_page.tags.join(', '),
+            keywords: parsely_page.tags && typeof parsely_page.tags === "array" ? parsely_page.tags.join(', ') : null,
             category: parsely_page.section
         };
     }
