35a1ac79463835b6e72486866fd447c190c77f4d
Nazar Leush <n.leush@gmail.com>
2014-11-20 22:23:00 +0200

    Revert "[domains] remove ted.com (working ok with whitelist)"
    This reverts commit 2ba4c4979c55c8eb73c91a743cafaad4cdd8e9fd.

---
+++ plugins/domains/ted.com/ted.com.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    re: /^https?:\/\/(?:www\.)?ted\.com\/talks\//i,
+
+    mixins: [
+        "canonical",
+        "oembed-title",
+        "video",
+        "oembed-author",
+        "oembed-site",
+        "oembed-thumbnail",
+        "oembed-video-responsive",
+        "favicon"
+    ],
+
+    tests: [{
+        page: "http://www.ted.com/talks",
+        selector: "#content a"
+    },
+        "http://www.ted.com/talks/kent_larson_brilliant_designs_to_fit_more_people_in_every_city"
+    ]
+};
