9315fbf5f8c750e491a7cb233b86a3ff05011d92
Nazar Leush <n.leush@gmail.com>
2014-11-24 13:18:22 +0200

    [domains] make vk.com status plugin work

--- plugins/domains/vk.com/vk.status.js
+++ plugins/domains/vk.com/vk.status.js
@@ -1,23 +1,33 @@
+var DEFAULT_WIDTH = 500;
+
 module.exports = {
 
     re: [
-        /^https?:\/\/vk\.com\/[a-z0-9_-]+\?w=wall([0-9-]+)_(\d+)/i,
-        /^https?:\/\/vk\.com\/wall([0-9-]+)_(\d+)/i
+        /^https?:\/\/(?:m\.)?vk\.com\/[a-z0-9_-]+\?w=wall([0-9-]+)_(\d+)/i,
+        /^https?:\/\/(?:m\.)?vk\.com\/wall([0-9-]+)_(\d+)/i
     ],
 
-    getLink: function(urlMatch) {
-        var width = 500;
+    getLink: function(url, meta, urlMatch, options) {
+
+        var m = url.match(/hash=([\w-]+)/i);
+
+        var hash = (m && m[1]) || meta.embed_hash || meta.hash
+
         return {
             type: CONFIG.T.text_html,
-            rel: [CONFIG.R.app, CONFIG.R.inline, CONFIG.R.ssl],
+            rel: [CONFIG.R.app, CONFIG.R.ssl],
             template_context: {
                 element_id: urlMatch[1],
                 owner_id: urlMatch[2],
-                width: width,
-                hash: ''    // TODO: calucate hash
+                width: options.maxWidth || DEFAULT_WIDTH,
+                hash: hash
             },
-            width: width
+            width: options.maxWidth || DEFAULT_WIDTH
         };
-    }
+    },
+
+    tests: [
+        "http://m.vk.com/wall-6507719_32735?hash=FdsHbXcBqnCk3pJEr3WYxtexBU-9"
+    ]
 
 };
