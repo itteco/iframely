73649a43138ab1aa700b1675506421123552f55f
Nazar Leush <n.leush@gmail.com>
2014-11-25 19:30:37 +0200

    [domains] fix timeout for vk.video

--- plugins/domains/vk.com/vk.video.js
+++ plugins/domains/vk.com/vk.video.js
@@ -16,7 +16,9 @@ module.exports = {
 
     getLink: function(url, meta, cb) {
 
-        if (meta["html-title"] == "Error | VK") cb ({responseStatusCode: 403});
+        if (meta["html-title"] == "Error | VK") {
+            return cb({responseStatusCode: 403});
+        }
 
         var video_url = (meta.og && meta.og.video && meta.og.video.url) || url; //for direct links to VK videos
 
@@ -24,11 +26,13 @@ module.exports = {
         var vid = video_url.match(/vid=([\-_a-zA-Z0-9]+)/) || video_url.match(/\Wid=([\-_a-zA-Z0-9]+)/);
         var hash = video_url.match(/embed\_hash=([\-_a-zA-Z0-9]+)/) || video_url.match(/hash=([\-_a-zA-Z0-9]+)/);
 
-        if (!oid || !vid || !hash) return;
+        if (!oid || !vid || !hash) {
+            return cb(null, null);
+        }
 
         var aspect = (meta.og && meta.og.video && meta.og.video.height) ? meta.og.video.width / meta.og.video.height : 4/3;
 
-        cb (null, {
+        cb(null, {
             href: "//vk.com/video_ext.php?oid=" + oid[1] + "&id=" + vid[1] + "&hash=" + hash[1] + "&hd=1",
             type: CONFIG.T.text_html,
             rel: [CONFIG.R.player, CONFIG.R.html5],
