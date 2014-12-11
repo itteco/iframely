2e4f29bb575d2cd114a8fbb5e08fecbf712a38b7
Nazar Leush <n.leush@gmail.com>
2014-12-11 21:27:39 +0200

    fix whitespaces

--- plugins/links/google-docs-viewer.js
+++ plugins/links/google-docs-viewer.js
@@ -2,13 +2,13 @@ module.exports = {
 
     getLink: function(url, nonHtmlContentType) {
 
-    	if (nonHtmlContentType.match(/application\/pdf/)) {
-	        return {
-	            href: "https://docs.google.com/viewer?embedded=true&url=" + encodeURIComponent(url),
-	            type: CONFIG.T.text_html,
-	            rel: CONFIG.R.reader,
-	            "aspect-ratio": 1 / Math.sqrt(2) // standard sqrt(2) aspect for A4 and other standard A* format papers (except North America, of course)
-	        }
-    	}
+        if (nonHtmlContentType.match(/application\/pdf/)) {
+            return {
+                href: "https://docs.google.com/viewer?embedded=true&url=" + encodeURIComponent(url),
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.reader,
+                "aspect-ratio": 1 / Math.sqrt(2) // standard sqrt(2) aspect for A4 and other standard A* format papers (except North America, of course)
+            }
+        }
     }
 };
