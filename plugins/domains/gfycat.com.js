d8036655da2154a749f8a0040607e40517240273
Nazar Leush <n.leush@gmail.com>
2014-12-16 16:53:21 +0200

    [domains] fgycat.com better source selection

--- plugins/domains/gfycat.com.js
+++ plugins/domains/gfycat.com.js
@@ -9,9 +9,23 @@ module.exports = {
     ],
 
     getLink: function(twitter) {
+
+        var stream = twitter.player.stream;
+        if (!(stream instanceof Array)) {
+            stream = [stream];
+        }
+
+        var href;
+        stream.forEach(function(item) {
+            var url = item.value || item;
+            if (typeof url === 'string' && url.match(/\.mp4$/)) {
+                href = url;
+            }
+        });
+
         return {
-            href: twitter.player.stream[1].value,
-            type: CONFIG.T.video_webm,
+            href: href,
+            type: CONFIG.T.video_mp4,
             rel: [CONFIG.R.player, CONFIG.R.gifv],
             width: twitter.player.width,
             height: twitter.player.height
