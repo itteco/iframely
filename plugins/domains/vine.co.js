abdf73eaac2ed00c0a5aa425869283e638011eb7
Nazar Leush <n.leush@gmail.com>
2014-12-18 21:16:17 +0200

    [domains] vine.co mp4 secure player

--- plugins/domains/vine.co.js
+++ plugins/domains/vine.co.js
@@ -8,6 +8,18 @@ module.exports = {
         "oembed-thumbnail"
     ],
 
+    getLink: function(twitter) {
+        return {
+            href: twitter.player.stream.value,
+            type: CONFIG.T.video_mp4,
+            rel: CONFIG.R.player,
+            "aspect-ratio": twitter.player.width / twitter.player.height,
+            "max-width": twitter.player.width,
+            "max-height": twitter.player.height
+
+        }
+    },
+
     tests: [
         "https://vine.co/v/bjHh0zHdgZT",
         "https://vine.co/v/blhUa7aLYgr",
