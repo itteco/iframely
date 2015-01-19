0f228a05e36bf4c67b52b468beac1c80c847334d
Andrew Newdigate <andrew@troupe.co>
2015-01-19 11:42:29 +0000

    Add support for single gists

--- plugins/domains/github.gist.js
+++ plugins/domains/github.gist.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    re: /^https?:\/\/gist\.github\.com\/(\w+\/)(\w+)/i,
+    re: /^https?:\/\/gist\.github\.com\/(\w+\/)(\w+)(#(\w+))?/i,
 
     mixins: [
         "og-site",
@@ -8,20 +8,28 @@ module.exports = {
         "favicon"
     ],
 
-    getMeta: function(meta) {        
+    getMeta: function(meta) {
         return (meta.og && meta.og.title) ? {
             title: meta.og.title,
             description: meta["html-title"]
         } : {
             title: meta["html-title"]
-        }
+        };
     },
 
     getLink: function(urlMatch) {
+        var scriptUrl;
+        if (urlMatch[4]) {
+            scriptUrl = 'https://gist.github.com/' + urlMatch[2] +'.js?file=' + urlMatch[4];
+        } else {
+            scriptUrl = 'https://gist.github.com/' + urlMatch[2] +'.js';
+
+        }
+
         return {
             type: CONFIG.T.text_html,
             rel: [CONFIG.R.reader, CONFIG.R.ssl],
-            html: '<script type="text/javascript" src="https://gist.github.com/' + urlMatch[2] +'.js"></script>'
+            html: '<script type="text/javascript" src="' + scriptUrl + '"></script>'
         };
     },
 
@@ -34,6 +42,7 @@ module.exports = {
         "https://gist.github.com/3054754",
         "https://gist.github.com/2719090",
         "https://gist.github.com/schisamo/163c34f3f6335bc12d45",
-        "https://gist.github.com/iparamonau/635df38fa737a1d80d23"
+        "https://gist.github.com/iparamonau/635df38fa737a1d80d23",
+        "https://gist.github.com/suprememoocow/a26a7cc168a71cc3c69b#file-script-alert-1-script"
     ]
 };
