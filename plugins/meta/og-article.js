aaf3f6b1f57ac21b2c7a44733ec59bd00701f88c
Nazar Leush <n.leush@gmail.com>
2015-03-31 20:02:39 +0300

    [meta] og-article: convert keywords array to string

--- plugins/meta/og-article.js
+++ plugins/meta/og-article.js
@@ -2,16 +2,26 @@ module.exports = {
 
     getMeta: function(meta) {
 
-    	var article = (meta.og && meta.og.article) || meta.article;
+        var article = (meta.og && meta.og.article) || meta.article;
+
+        var k = article.tag;
+
+        if (k instanceof Array) {
+            k = k.join(', ');
+        }
+
+        if (typeof k !== "string") {
+            k = null;
+        }
 
         if (article) {
-	        return {
-	            author: article.author,
-	            category: article.section,
-	            keywords: article.tag
-	        };
-	        // example - http://www.entrepreneur.com/article/237644
-	        // https://open.bufferapp.com/customer-development-interviews-using-twitter/
-    	}
+            return {
+                author: article.author,
+                category: article.section,
+                keywords: k
+            };
+            // example - http://www.entrepreneur.com/article/237644
+            // https://open.bufferapp.com/customer-development-interviews-using-twitter/
+        }
     }
 };
