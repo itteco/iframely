a4e7b09d32f0f2c7ee9911e084859f276525ccb8
Nazar Leush <n.leush@gmail.com>
2015-04-02 15:56:44 +0300

    Revert "[domains] add tudou.com embed type"
    This reverts commit c0937590a2be08d62388156bf6127ef405a84fe2.

--- plugins/domains/tudou.com.js
+++ plugins/domains/tudou.com.js
@@ -1,9 +1,6 @@
 module.exports = {
 
-    re: [
-        /^http:\/\/www\.tudou\.com\/\w+\/([\w_-]+)\/([\w_-]+)\.html/i,
-        /^http:\/\/www\.tudou\.com\/programs\/view\/([\w_-]+)\//i
-    ],
+    re: /^http:\/\/www\.tudou\.com\/\w+\/([\w_-]+)\/([\w_-]+)\.html/i,
 
     mixins: [
         "favicon",
@@ -13,17 +10,8 @@ module.exports = {
     ],
 
     getLink: function(urlMatch) {
-
-        var href;
-
-        if (urlMatch[2]) {
-            href = 'http://www.tudou.com/programs/view/html5embed.action?type=1&code=' + urlMatch[2] + '&lcode=' + urlMatch[1];
-        } else {
-            href = 'http://www.tudou.com/programs/view/html5embed.action?type=0&code=' + urlMatch[1] + '&lcode=&resourceId=0_06_05_99';
-        }
-
         return {
-            href: href,
+            href: 'http://www.tudou.com/programs/view/html5embed.action?type=1&code=' + urlMatch[2] + '&lcode=' + urlMatch[1],
             type: CONFIG.T.text_html,
             rel: CONFIG.R.player,
             "aspect-ratio": 16/9
@@ -34,8 +22,7 @@ module.exports = {
         page: 'http://www.tudou.com/',
         selector: '.pic a'
     },
-        "http://www.tudou.com/listplay/Jy6tBwii48M/xLShdaQMhrU.html",
-        "http://www.tudou.com/programs/view/966_lDwfam8/"
+        "http://www.tudou.com/listplay/Jy6tBwii48M/xLShdaQMhrU.html"
     ]
 };
 
