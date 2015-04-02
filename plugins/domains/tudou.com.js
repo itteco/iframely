c0937590a2be08d62388156bf6127ef405a84fe2
Nazar Leush <n.leush@gmail.com>
2015-04-02 15:55:28 +0300

    [domains] add tudou.com embed type

--- plugins/domains/tudou.com.js
+++ plugins/domains/tudou.com.js
@@ -1,6 +1,9 @@
 module.exports = {
 
-    re: /^http:\/\/www\.tudou\.com\/\w+\/([\w_-]+)\/([\w_-]+)\.html/i,
+    re: [
+        /^http:\/\/www\.tudou\.com\/\w+\/([\w_-]+)\/([\w_-]+)\.html/i,
+        /^http:\/\/www\.tudou\.com\/programs\/view\/([\w_-]+)\//i
+    ],
 
     mixins: [
         "favicon",
@@ -10,8 +13,17 @@ module.exports = {
     ],
 
     getLink: function(urlMatch) {
+
+        var href;
+
+        if (urlMatch[2]) {
+            href = 'http://www.tudou.com/programs/view/html5embed.action?type=1&code=' + urlMatch[2] + '&lcode=' + urlMatch[1];
+        } else {
+            href = 'http://www.tudou.com/programs/view/html5embed.action?type=0&code=' + urlMatch[1] + '&lcode=&resourceId=0_06_05_99';
+        }
+
         return {
-            href: 'http://www.tudou.com/programs/view/html5embed.action?type=1&code=' + urlMatch[2] + '&lcode=' + urlMatch[1],
+            href: href,
             type: CONFIG.T.text_html,
             rel: CONFIG.R.player,
             "aspect-ratio": 16/9
@@ -22,7 +34,8 @@ module.exports = {
         page: 'http://www.tudou.com/',
         selector: '.pic a'
     },
-        "http://www.tudou.com/listplay/Jy6tBwii48M/xLShdaQMhrU.html"
+        "http://www.tudou.com/listplay/Jy6tBwii48M/xLShdaQMhrU.html",
+        "http://www.tudou.com/programs/view/966_lDwfam8/"
     ]
 };
 
