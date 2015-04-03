ebeb215c00d8e9037de05c0decef0fab023a46ef
Nazar Leush <n.leush@gmail.com>
2015-04-03 18:48:08 +0300

    [links] do not check twitter player type if has html5 rel

--- plugins/links/twitter-player.js
+++ plugins/links/twitter-player.js
@@ -4,9 +4,11 @@ module.exports = {
 
         if (twitter.player && whitelistRecord.isAllowed && whitelistRecord.isAllowed('twitter.player')) {
 
+            var type = whitelistRecord.isAllowed('twitter.player', 'html5') ? CONFIG.T.maybe_text_html : CONFIG.T.text_html;
+
             return {
                 href: twitter.player.value || twitter.player,
-                type: CONFIG.T.maybe_text_html,
+                type: type,
                 rel: [CONFIG.R.player, CONFIG.R.twitter],
                 width: twitter.player.width,
                 height: twitter.player.height
