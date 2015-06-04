4c23583441c7cab5ec19b81821a13ad2936732f1
Nazar Leush <n.leush@gmail.com>
2015-06-04 20:46:47 +0300

    [tests] minor fix

--- plugins/domains/wistia.com.js
+++ plugins/domains/wistia.com.js
@@ -36,7 +36,9 @@ module.exports = {
     },
 
 
-    tests: [
+    tests: [{
+        noFeeds: true
+    },
         "http://appsumo.wistia.com/medias/fudkgyoejs",
         "http://convinceandconvert.wistia.com/medias/52lpg5691w?utm_campaign=Argyle+Social-2013-04&utm_medium=Argyle+Social&utm_source=General+Use&utm_term=2013-04-24-13-23-55",
         "http://mcgallen.wistia.com/medias/3nd1iitk2s?embedType=seo"
