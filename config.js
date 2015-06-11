8cd2410c159951bd633417106b048795288c585a
Nazar Leush <n.leush@gmail.com>
2015-06-11 17:12:13 +0300

    [config] set default IMAGE_META_CACHE_TTL to 7 days

--- config.js
+++ config.js
@@ -16,6 +16,7 @@
         // Default cache engine to prevent warning.
         CACHE_ENGINE: 'node-cache',
         CACHE_TTL: 24 * 60 * 60,
+        IMAGE_META_CACHE_TTL: 7 *24 * 60 * 60,
 
         CACHE_TTL_PAGE_TIMEOUT: 10 * 60,
         CACHE_TTL_PAGE_404: 10 * 60,
