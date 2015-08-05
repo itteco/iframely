776160a23fe9589379de6bc888ca1e2a2f206e37
Ivan Paramonau <i.paramonau@gmail.com>
2015-08-05 19:14:16 +0300

    [domains] allow '-' in CartoDB usernames

--- plugins/domains/cartodb.com.js
+++ plugins/domains/cartodb.com.js
@@ -2,7 +2,7 @@ var cheerio = require('cheerio');
 
 module.exports = {
 
-    re: /^https?:(\/\/\w+\.cartodb\.com\/(?:u\/\w+\/)?viz\/[a-z0-9-]+)/i,
+    re: /^https?:(\/\/\w+\.cartodb\.com\/(?:u\/[\w-]+\/)?viz\/[a-z0-9-]+)/i,
 
     mixins: [
         // "oembed-title", oembed title is null :\\
@@ -43,6 +43,7 @@ module.exports = {
     tests: [
         "http://team.cartodb.com/u/andrew/viz/9ee7f88c-b530-11e4-8a2b-0e018d66dc29/public_map",
         "https://team.cartodb.com/u/andrew/viz/9ee7f88c-b530-11e4-8a2b-0e018d66dc29/embed_map",
-        "https://smb2stfinitesubs1.cartodb.com/viz/39e625ee-cef3-11e4-b8bb-0e0c41326911/public_map"
+        "https://smb2stfinitesubs1.cartodb.com/viz/39e625ee-cef3-11e4-b8bb-0e0c41326911/public_map",
+        "https://twittereurope.cartodb.com/u/kike-5/viz/456350ba-377e-11e5-8b42-0e853d047bba/public_map"
     ]
 };
