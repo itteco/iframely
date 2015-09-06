820a0e2f7b3cb33259ce0660b74222ada01c83d1
Ivan Paramonau <i.paramonau@gmail.com>
2015-09-06 14:31:13 -0400

    [domains] better aspect ratio for Spotify

--- plugins/domains/spotify.com.js
+++ plugins/domains/spotify.com.js
@@ -1,26 +1,43 @@
+var cheerio = require('cheerio');
+
 module.exports = {
 
     mixins: [
-        "oembed-title",
+        "twitter-title",
+        "twitter-image",
+        "canonical",
+        "favicon",
         "oembed-thumbnail",
-        "oembed-site",
-        "oembed-video-responsive"
+        "oembed-site"
     ],
 
-    // keep dependency on oEmbed only. Otherwise, there's redirect to relative path and no embeds as a result
     getLink: function(oembed) {
 
-        return {
-            href: "http://d2c87l0yth4zbw.cloudfront.net/i/_global/favicon.png",
-            type: CONFIG.T.image,
-            rel: CONFIG.R.icon
-        };
+        var $container = cheerio('<div>');
+
+        try {
+            $container.html(oembed.html5 || oembed.html);
+        } catch (ex) {}
+
+        var $iframe = $container.find('iframe');
+
+
+        // if embed code contains <iframe>, return src
+        if ($iframe.length == 1) {
+
+            return {
+                href: $iframe.attr('src'),
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.ssl, CONFIG.R.html5],
+                 "aspect-ratio":  100 / 115 // hardcode here as otherwise there's blank space beneath the player
+            }
+        }
+
     },
 
     tests: [
         "http://open.spotify.com/track/6ol4ZSifr7r3Lb2a9L5ZAB",
         "http://open.spotify.com/user/cgwest23/playlist/4SsKyjaGlrHJbRCQwpeUsz",
-        "http://open.spotify.com/user/davadija/playlist/3uIJTf7ArvF7VmeO41F8T5",
         "http://open.spotify.com/album/42jcZtPYrmZJhqTbUhLApi"
     ]
 };
