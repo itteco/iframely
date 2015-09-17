1587ae773a03814b067afc145f7392d1cb288426
Nazar Leush <n.leush@gmail.com>
2015-09-17 20:41:19 +0300

    [domains] huffingtonpost.live: remove sid from href if not specified

--- plugins/domains/huffingtonpost.com/huffingtonpost.live.js
+++ plugins/domains/huffingtonpost.com/huffingtonpost.live.js
@@ -15,19 +15,25 @@ module.exports = {
 
     getLinks: function(urlMatch, options) {
 
-        var sid = options.getProviderOptions('huffingtonpost.live.sid', 577);
+        var sid = options.getProviderOptions('huffingtonpost.live.sid');
+
+        if (sid) {
+            sid = '&sid=' + sid;
+        } else {
+            sid = '';
+        }
 
         return [{
             href: 'http://live.huffingtonpost.com/images/hufflive_favicon.ico',
             rel: CONFIG.R.icon,
             type: CONFIG.T.icon
         }, {
-            href: 'http://embed.live.huffingtonpost.com/HPLEmbedPlayer/?segmentId=' + urlMatch[1] + '&autoPlay=false&showPlaylist=true&sid=' + sid,
+            href: 'http://embed.live.huffingtonpost.com/HPLEmbedPlayer/?segmentId=' + urlMatch[1] + '&autoPlay=false&showPlaylist=true' + sid,
             rel: [CONFIG.R.player, CONFIG.R.html5],
             type: CONFIG.T.text_html,
             "aspect-ratio": 480/270
         }, {
-            href: 'https://s.embed.live.huffingtonpost.com/HPLEmbedPlayer/?segmentId=' + urlMatch[1] + '&autoPlay=false&showPlaylist=true&sid=' + sid,
+            href: 'https://s.embed.live.huffingtonpost.com/HPLEmbedPlayer/?segmentId=' + urlMatch[1] + '&autoPlay=false&showPlaylist=true' + sid,
             rel: [CONFIG.R.player, CONFIG.R.html5],
             type: CONFIG.T.text_html,
             "aspect-ratio": 480/270
