35b2e9c4cb96a425b3bf0a4bf0e4497714c2a8d6
Ivan Paramonau <i.paramonau@gmail.com>
2015-10-08 11:54:42 -0400

    add rel=file to direct links to pdfs and office docs

--- plugins/links/ms-doc-viewer.js
+++ plugins/links/ms-doc-viewer.js
@@ -6,7 +6,7 @@ module.exports = {
             return {
                 href: "https://view.officeapps.live.com/op/embed.aspx?src=" + encodeURIComponent(url),
                 type: CONFIG.T.text_html,
-                rel: [CONFIG.R.reader, CONFIG.R.ssl, CONFIG.R.HTML5],
+                rel: [CONFIG.R.reader, CONFIG.R.file, CONFIG.R.ssl, CONFIG.R.HTML5],
                 "aspect-ratio": /presentation|ms\-powerpoint|ms\-excel|ms\-office/.test(__nonHtmlContentData.type) ?  4/3 : 1 / Math.sqrt(2)
             }
         }
