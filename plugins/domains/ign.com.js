d36a69e3d8252400c617255d43492e8fab465bcb
Nazar Leush <n.leush@gmail.com>
2015-10-13 18:10:40 +0300

    [domains] ign.com

---
+++ plugins/domains/ign.com.js
@@ -0,0 +1,48 @@
+var _ = require('underscore');
+
+module.exports = {
+
+    re: [
+        /http:\/\/\w+\.ign\.com\/\w+\/videos\/\d+\/\w+\/[a-z0-9-]+/i,
+        /http:\/\/www\.ign\.com\/videos\/\d+\/\d+\/\d+\/[a-z0-9-]+/i
+    ],
+
+
+    mixins: [
+        "favicon",
+        "canonical",
+        "date",
+        "og-image",
+        "og-article",
+        "og-description",
+        "og-site",
+        "og-title",
+        "media-detector"
+    ],
+
+    getLink: function(meta) {
+
+        var url = _.find(meta.alternate, function(url) {
+            return url.match && url.match(/http:\/\/www\.ign\.com\/videos/)
+        });
+
+        if (url) {
+            return {
+                href: 'http://widgets.ign.com/video/embed/content.html?url=' + url,
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.player,
+                "aspect-ratio": 468 / 263
+            };
+        }
+    },
+
+    tests: [{
+        feed: 'http://me.ign.com/en/feed.xml',
+        getUrl: function(url) {
+            return url.match(/videos/) && url;
+        }
+    },
+        "http://me.ign.com/en/videos/112217/video/our-favorite-games-of-new-york-comic-con",
+        "http://me.ign.com/en/videos/111115/fix/ps4-firmware-30-new-battlefront-details-ign-daily"
+    ]
+};
