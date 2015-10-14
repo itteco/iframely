5c6bac80afba57526e4416d2b9d97999f3eda547
Nazar Leush <n.leush@gmail.com>
2015-10-14 17:47:21 +0300

    [domains] wikipedia.org

---
+++ plugins/domains/wikipedia.org.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+    re: /https:\/\/\w+\.wikipedia\.org\/wiki\/.+/i,
+
+    provides: 'wikiData',
+
+    mixins: ["*"],
+
+    getMeta: function(wikiData) {
+        return {
+            title: wikiData.title,
+            description: wikiData.description
+        };
+    },
+
+    getData: function(cheerio) {
+
+        var result = {};
+
+        var $head = cheerio('#firstHeading');
+
+        if ($head.length) {
+            result.title = $head.text();
+        }
+
+        var $img = cheerio('.image img');
+        if ($img.length) {
+            result.thumb = $img.attr('src');
+            result.thumb_width = $img.attr('width');
+            result.thumb_height = $img.attr('height');
+        }
+
+        var $p = cheerio('#mw-content-text p');
+        if ($p.length) {
+            result.description = cheerio($p[0]).text();
+        }
+
+        return {
+            wikiData: result
+        };
+    },
+
+    getLink: function(wikiData) {
+        return {
+            href: wikiData.thumb,
+            type: CONFIG.T.image,
+            rel: CONFIG.R.thumbnail,
+            width: wikiData.thumb_width,
+            height: wikiData.thumb_height
+        };
+    }
+};
