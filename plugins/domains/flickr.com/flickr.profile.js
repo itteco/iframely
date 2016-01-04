50cf1761dbde3b58f80770e98f7310f0d8b9e4c2
Ivan Paramonau <i.paramonau@gmail.com>
2016-01-04 11:17:09 -0500

    Revert "[domains] simplify Flickr parser"
    This reverts commit 80b385e85c7f3ed3371b2f925256740c14e56bbb.

--- plugins/domains/flickr.com/flickr.profile.js
+++ plugins/domains/flickr.com/flickr.profile.js
@@ -3,7 +3,12 @@ module.exports = {
     re: /^https?:\/\/www\.flickr\.com\/photos\/([@a-zA-Z0-9_\.-]+)\/?([?#].*)?$/,
 
     mixins: [
-        "*"
+        "og-image",
+        "favicon",
+        "canonical",
+        "twitter-description",
+        "og-site",
+        "og-title"
     ],
 
     getLink: function(urlMatch, og) {
