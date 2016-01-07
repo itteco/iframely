d2981e0f5ce22e91afddcff9ffcd6bfd81173e92
Nazar Leush <n.leush@gmail.com>
2016-01-07 20:06:50 +0200

    [cache] do not use cache for meta and oembed if options.refrsh=true

--- modules/api/views.js
+++ modules/api/views.js
@@ -70,7 +70,8 @@ module.exports = function(app) {
                     readability: getBooleanParam(req, 'readability'),
                     getWhitelistRecord: whitelist.findWhitelistRecordFor,
                     maxWidth: getIntParam(req, 'maxwidth') || getIntParam(req, 'max-width'),
-                    promoUri: req.query.promoUri
+                    promoUri: req.query.promoUri,
+                    refresh: getBooleanParam(req, 'refresh')
                 }, cb);
             }
 
@@ -375,7 +376,8 @@ module.exports = function(app) {
                     getWhitelistRecord: whitelist.findWhitelistRecordFor,
                     filterNonSSL: getBooleanParam(req, 'ssl'),
                     filterNonHTML5: getBooleanParam(req, 'html5'),
-                    maxWidth: getIntParam(req, 'maxwidth') || getIntParam(req, 'max-width')
+                    maxWidth: getIntParam(req, 'maxwidth') || getIntParam(req, 'max-width'),
+                    refresh: getBooleanParam(req, 'refresh')
                 }, cb);
             }
 
