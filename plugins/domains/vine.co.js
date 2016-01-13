d203178be8f6c1b24854cf21830a9d71ac575819
Ivan Paramonau <i.paramonau@gmail.com>
2016-01-13 07:40:53 -0500

    [domain] config option to disable Vine player on mobile

--- plugins/domains/vine.co.js
+++ plugins/domains/vine.co.js
@@ -5,7 +5,7 @@ module.exports = {
     ],
 
     mixins: [
-        "twitter-player",
+        //"twitter-player",
         "twitter-stream",
         //"oembed-video",
         "og-video",
@@ -17,6 +17,22 @@ module.exports = {
         "oembed-site",
         "oembed-title"
     ],
+
+    getLink: function (twitter, options) {
+
+        var rel = [CONFIG.R.player];
+
+        if (!(options.getProviderOptions("vine") && options.getProviderOptions("vine").disable_on_mobile)) {
+            rel.push(CONFIG.R.html5);
+        }
+
+        return {
+            href: twitter.player.value,
+            type: CONFIG.T.text_html,
+            rel: rel,
+            "aspect-ratio": twitter.player.width / twitter.player.height
+        }
+    },
 
     // plugin is left for tests and speed mostly, as it is well covered by generic plugins
     tests: [
