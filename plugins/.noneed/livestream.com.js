0ddcdf90d2e0362b4d20391302d9f70fe4bf092f
Ivan Paramonau <i.paramonau@gmail.com>
2016-03-22 09:28:41 -0400

    [domains] Livestream now is well covered by generic parsers

--- plugins/.noneed/livestream.com.js
+++ plugins/.noneed/livestream.com.js
@@ -16,7 +16,7 @@ module.exports = {
             return {
                 href: meta.twitter.player.value,
                 type: CONFIG.T.text_html,
-                rel: [CONFIG.R.player, CONFIG.R.twitter],
+                rel: [CONFIG.R.player, CONFIG.R.twitter, CONFIG.R.html5],
                 "aspect-ratio": meta.twitter.player.width / meta.twitter.player.height
             };
 
