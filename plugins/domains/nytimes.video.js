eb8f4662d41b54ff57155e96b9ba36644f6a1aa5
Ivan Paramonau <i.paramonau@gmail.com>
2016-04-05 08:40:19 -0400

    [domains] Revert NYTimes oembed support for videos
    Re #112 - Apparently, NYTimes rolled back their oEmbed for now

--- plugins/domains/nytimes.video.js
+++ plugins/domains/nytimes.video.js
@@ -1,5 +1,3 @@
-var cheerio = require('cheerio');
-
 module.exports = {
 
     re: [
@@ -8,43 +6,17 @@ module.exports = {
     ], 
 
     mixins: [
-        "oembed-thumbnail",
-        "domain-icon",
-        "oembed-author",
-        "oembed-canonical",
-        "media-detector",
-        "oembed-site",
-        "oembed-title",
-        "og-image"
+        "*"
     ],
 
-    getMeta: function(oembed) {
+    getLink: function(meta, urlMatch) {
 
-        return {
-            description: oembed.summary, 
-            date: oembed.publication_date
-        }
-    },
-
-    getLink: function(oembed) {
-
-        if (oembed.type === "video" || oembed.type === "rich" ) {
-
-            var $container = cheerio('<div>');
-            try {
-                $container.html(oembed.html5 || oembed.html);
-            } catch (ex) {}
-
-            var $iframe = $container.find('iframe');
-
-            if ($iframe.length == 1) {
-
-                return {
-                    href: $iframe.attr('src'),
-                    type: CONFIG.T.text_html,
-                    rel: [CONFIG.R.player, CONFIG.R.html5],
-                    "aspect-ratio": 1.45 // temporary until fixed padding is available or until oembed has proper width and height
-                }
+        if (meta.medium == "video") {
+            return {
+                href: "//graphics8.nytimes.com/bcvideo/1.0/iframe/embed.html?videoId=" + urlMatch[1] + "&playerType=embed",
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.html5],
+                "aspect-ratio": 1.45 // seems to work well on diff widths                
             }
         }
     },
