c727c49ebe5738037a76b9766cbf02aef2d26757
Ivan Paramonau <i.paramonau@gmail.com>
2016-04-20 09:37:38 -0400

    [domains] handle direct links to NPR players

--- plugins/domains/npr.org/npr.player.js
+++ plugins/domains/npr.org/npr.player.js
@@ -1,7 +1,8 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/www\.npr\.org\/player\/v2\/mediaPlayer\.html\?(?:[^&]+&)*id=(\d+)&m=(\d+)/i
+        /^https?:\/\/www\.npr\.org\/player\/v2\/mediaPlayer\.html\?(?:[^&]+&)*id=(\d+)&m=(\d+)/i,
+        /^https?:\/\/www\.npr\.org\/player\/embed\/(\d+)\/(\d+)/i
     ],
 
     mixins: ["*"],
@@ -19,5 +20,6 @@ module.exports = {
 
     tests: [
         "http://www.npr.org/player/v2/mediaPlayer.html?action=1&t=1&islist=false&id=368714299&m=368931763",
+        "http://www.npr.org/player/embed/473953959/473993224"
     ]
 };
