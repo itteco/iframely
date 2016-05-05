0f28038a5c48024ed37e766a1619f2e228e48e81
Ivan Paramonau <i.paramonau@gmail.com>
2016-05-05 17:16:42 -0400

    [twitter] exclude external videos e.g. NFL

--- plugins/domains/twitter.com/twitter.video.js
+++ plugins/domains/twitter.com/twitter.video.js
@@ -9,7 +9,9 @@ module.exports = {
     getData: function(__allow_twitter_video, og) {
 
         return {
-            twitter_video: og.video || false
+            twitter_video: (og.video && og.image && /^https?:\/\/pbs\.twimg\.com\//i.test(og.image.url || og.image.src || og.image)) ? og.video : false
+            // exclude proxy images, ex:
+            // https://twitter.com/nfl/status/648185526034395137
         }
     }
 };
