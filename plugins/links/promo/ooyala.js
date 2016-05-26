631ed08bed898f0f8d8e81613a69074b67b39fcd
Ivan Paramonau <i.paramonau@gmail.com>
2016-05-26 10:29:44 -0400

    [ooyala] autoplay option, better v3 detect and aspect-ratio

--- plugins/links/promo/ooyala.js
+++ plugins/links/promo/ooyala.js
@@ -19,7 +19,7 @@ module.exports = {
             return {
                 __ooyalaPlayer: {
                     embedCode: urlMatch[1],
-                    pbid: /&pbid=/.test(video_src) ? video_src.match(/pbid=([_a-zA-Z0-9\-]+)/)[1] : null,
+                    pbid: /&(pbid|player)=/.test(video_src) ? video_src.match(/(?:pbid|player)=([_a-zA-Z0-9\-]+)/)[1] : null,
                     width: (meta.twitter && meta.twitter.player && meta.twitter.player.width) || (meta.og && meta.og.video && meta.og.video.width),
                     height: (meta.twitter && meta.twitter.player && meta.twitter.player.height) || (meta.og && meta.og.video && meta.og.video.height)
                 }
@@ -46,23 +46,46 @@ module.exports = {
                 if (response.statusCode !== 200 || !response.headers || !response.headers['content-length'] || response.headers['content-length'] < 1000) {
                     return cb("ooyala video isn't valid: " + __ooyalaPlayer.embedCode);
                 } else {
-                    return cb(null, {
-                        href: "https://player.ooyala.com/tframe.html?embedCode=" + __ooyalaPlayer.embedCode + '&keepEmbedCode=true' 
-                            + (__ooyalaPlayer.pbid ? '&pbid=' + __ooyalaPlayer.pbid : ''),
-                        rel: [CONFIG.R.player, CONFIG.R.html5],
-                        type: CONFIG.T.text_html,
-                        "aspect-ratio": __ooyalaPlayer.width && __ooyalaPlayer.height ? __ooyalaPlayer.width / __ooyalaPlayer.height : CONFIG.DEFAULT_ASPECT_RATIO
-                    });
+
+                    var aspect = __ooyalaPlayer.width && __ooyalaPlayer.height && (__ooyalaPlayer.width / __ooyalaPlayer.height < 1.5) ? 4 / 3 : 16/9;
+                    var rel = [CONFIG.R.player];
+                    var href = 'https://player.ooyala.com/';
+                    var type = CONFIG.T.text_html;
+
+                    if (__ooyalaPlayer.pbid) {
+                        href = href + 'tframe.html?embedCode=' + __ooyalaPlayer.embedCode + '&keepEmbedCode=true' + '&pbid=' + __ooyalaPlayer.pbid;
+                        rel.push(CONFIG.R.html5);
+                    } else {
+                        href = href + 'player.swf?embedCode=' + __ooyalaPlayer.embedCode + '&keepEmbedCode=true';
+                        type = CONFIG.T.flash;
+                    }
+
+                    return cb(null, [{
+                        href: href,
+                        rel: rel,
+                        type: type,
+                        'aspect-ratio': aspect
+                    }, {
+                        href: type === CONFIG.T.text_html ? href + '&options[autoplay]=true' : href + '&autoplay=1',
+                        rel: rel.concat(CONFIG.R.autoplay),
+                        type: type,
+                        'aspect-ratio': aspect
+                    }]);
                 }
             }
         }, cb);
-    }
+    },
 
-    // gazzillion of examples. 
-    // http://www.torontofc.ca/video/2015/06/24/bacardi-match-highlights-mtl-vs-tor-june-24-2014
-    // http://www.theverge.com/2013/8/5/4588634/moto-x-hands-on-review-video
-    // http://www.revolutionsoccer.net/post/2016/01/26/outside-box-1-entrance-physicals-and-first-day-back?autoplay=true
-    // http://www.vox.com/explains/2014/4/23/5643382/how-bitcoin-is-like-the-internet-in-the-80s
-    // http://www.polygon.com/2013/10/23/4947500/cooperatives-pokemon-x-and-y
-    // http://www.orlandocitysc.com/post/2016/05/16/barber-shop-darwin-cer-n?autoplay=true
+    tests: [
+        "http://www.torontofc.ca/video/2015/06/24/bacardi-match-highlights-mtl-vs-tor-june-24-2014",
+        "http://www.theverge.com/2013/8/5/4588634/moto-x-hands-on-review-video",
+        "http://www.revolutionsoccer.net/post/2016/01/26/outside-box-1-entrance-physicals-and-first-day-back?autoplay=true",
+        "http://www.vox.com/explains/2014/4/23/5643382/how-bitcoin-is-like-the-internet-in-the-80s",
+        "http://www.polygon.com/2013/10/23/4947500/cooperatives-pokemon-x-and-y",
+        "http://www.orlandocitysc.com/post/2016/05/16/barber-shop-darwin-cer-n?autoplay=true",
+        "https://www.rei.com/video/2015/10/07/how-to-size-snowboard-boots.html",
+        "http://www.thisisinsider.com/cheesy-breakfast-potatoes-2016-5",
+        "http://www.unotv.com/videoblogs/tecnologia/gadgets-tecnologia-apps/detalle/javier-matuk-20-enero-como-te-gustaria-mesas-pinball-futuro-986876/",
+        "http://www.livescience.com/54668-spacex-lands-again-first-stage-on-droneship-despite-extreme-velocities-video.html"
+    ]
 };
