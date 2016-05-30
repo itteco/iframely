52b3c41f60728f42aefe7133c64bb754224be7b8
Ivan Paramonau <i.paramonau@gmail.com>
2016-05-30 11:36:14 -0400

    [domains] fix centering for timelines

---
+++ plugins/domains/twitter.com/twitter.center-fix.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    // remove this plugin when Twitter fixes centering
+
+    re: [
+        /^https?:\/\/twitter\.com\/\w+\/(?:timelines?|moments?|likes?)\/(\d+)/i,
+        /^https?:\/\/twitter\.com\/\w+$/i,
+        /^https?:\/\/twitter\.com\/\w+\/(?:timelines?|moments?|likes?|lists?)\//i,
+    ],
+
+    mixins: [
+        'domain-icon',
+        'oembed-site',
+        'html-title',
+        'description',
+        'og-image',
+        'canonical'
+    ],
+
+    getLink: function(oembed, options) {
+
+        var html = oembed.html;
+
+        if (options.getProviderOptions('twitter.center', true)) {
+            html = '<div align="center">' + html + '</div>';
+        }
+
+        return {
+            html: html,
+            rel: [CONFIG.R.reader, CONFIG.R.html5, CONFIG.R.ssl, CONFIG.R.inline],
+            type: CONFIG.T.text_html,
+            'max-width': oembed.width
+        }
+    },
+
+    tests: [
+        "https://twitter.com/potus",
+        "https://twitter.com/i/moments/737260069209972736",
+        "https://twitter.com/TwitterDev/timelines/539487832448843776",
+        "https://twitter.com/TwitterDev/lists/national-parks"
+    ]
+};
