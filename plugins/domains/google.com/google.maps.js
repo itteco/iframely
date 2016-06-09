b4c1a1955f82a8eae89d820faeedbb27478227ca
Ivan Paramonau <i.paramonau@gmail.com>
2016-06-09 13:47:37 -0400

    [google maps] support waypoints for directions

--- plugins/domains/google.com/google.maps.js
+++ plugins/domains/google.com/google.maps.js
@@ -10,6 +10,10 @@ module.exports = {
 
         // directions
         // https://www.google.com/maps/dir/41.1744197,-73.0089647/Church+of+Christ,+2+Drew+Circle,+Trumbull,+CT+06611/@41.171124,-73.145653,12z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x89e80968e8b48d6f:0xf267c1e26968b542!2m2!1d-73.194478!2d41.251582!3e1?hl=en-US
+
+        // one waypoint:        https://www.google.co.in/maps/dir/Red+Fort,+Netaji+Subhash+Marg,+Chandni+Chowk,+New+Delhi,+Delhi/Fatehpuri,+New+Delhi,+Delhi/Delhi+Gate,+New+Delhi,+Delhi/@28.648602,77.214052,14z/data=!3m1!4b1!4m20!4m19!1m5!1m1!1s0x390cfce26ec085ef:0x441e32f4fa5002fb!2m2!1d77.2410203!2d28.6561592!1m5!1m1!1s0x390cfd12465a7def:0xc97043c5bb5c08ba!2m2!1d77.2214745!2d28.6563683!1m5!1m1!1s0x390cfcd8811f60ed:0x5c53287ad4d4f7e6!2m2!1d77.2417726!2d28.6405645!3e0?hl=en
+        // multiple waypoints:  https://www.google.com/maps/dir/LOTAN+CHOLE+KULCHEY+WALA,+Govind+Ki+Gali,+Dariya+Ganj,+New+Delhi,+Delhi+110002,+India/Dr+Shroff+Charity+Eye+Hospital,+5027,+Kedarnath+Road,+Beside+Vani+Prakashan,+Daryaganj,+New+Delhi,+Delhi+110002,+India/SC-4B,+Basant+Rd,+Delhi+110055,+India/3113,+Sang+Trashan+Marg,+Kaseru+Walan,+Paharganj,+New+Delhi,+Delhi+110055,+India/@28.6426793,77.2198618,15z/data=!3m1!4b1!4m26!4m25!1m5!1m1!1s0x390cfcd91fe43afd:0xef6845b4da489dd1!2m2!1d77.2434193!2d28.6415535!1m5!1m1!1s0x390cfcdf9fb395bb:0x7d8962875ec3432e!2m2!1d77.2420112!2d28.645873!1m5!1m1!1s0x390cfd4744055f3f:0x6a3ea6a5a658cc3e!2m2!1d77.2164889!2d28.6394484!1m5!1m1!1s0x390cfd4193df763b:0xc93187db8d27b07e!2m2!1d77.21275!2d28.644072!3e2
+        /^https?:\/\/(?:www\.)?google\.(?:com?\.)?[a-z]+\/maps\/(dir\/[^\/]+)\/([^@\?]+)\/(@[^\/]+)/i,
         /^https?:\/\/(?:www\.)?google\.(?:com?\.)?[a-z]+\/maps\/(dir\/[^\/]+)\/([^\/\?]+)\/?(@[^\/]+)?/i,
 
         // search
@@ -62,7 +66,7 @@ module.exports = {
         var map = "https://www.google.com/maps/embed/v1/" + gmap.mode + "?key=" + api_key;
 
         if (gmap.q && gmap.mode != 'streetview') {
-            map = map + (gmap.mode == "directions" ? "&destination=" : "&q=") + gmap.q;
+            map = map + (gmap.mode == "directions" ? "&destination=" : "&q=") + gmap.q + (gmap.waypoints ? '&waypoints=' + gmap.waypoints : '');
         }
 
         if (gmap.origin) {
@@ -140,6 +144,16 @@ module.exports = {
 
         // Search query is always returned by urlMatch, even if empty
         gmap.q = urlMatch[2];
+
+        if (/\//i.test(gmap.q)) { // directions with waypoints, the last one is the destination
+            var waypoints = gmap.q.split('/');
+            gmap.q = waypoints[waypoints.length - 1];
+            gmap.waypoints = '';
+            for (i = 0; i < waypoints.length-1; i++) {
+                gmap.waypoints += (i == 0 ? '' : '|') + waypoints[i];
+
+            }            
+        }
 
         // Coordinates - if given: urlMatch[3]
         if (urlMatch.length > 3 && urlMatch[3]) {
