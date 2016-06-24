2aa9a6a57f302b34d25f016d9187d3f0d24609a0
ASAP <alexander.sapountzis@gmail.com>
2016-06-24 18:56:53 -0400

    Initial attempt at ENVRMNT plugin

--- plugins/domains/envrmnt.com.js
+++ plugins/domains/envrmnt.com.js
@@ -1,29 +1,36 @@
 console.log("envrmnt.com loaded");
 
 module.exports = {
-
-    mixins: [
-        "og-title",
-        "og-image",
-        "canonical"
-    ],
-
     re: [
         /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/share\/\?share=([\w\-]+)\/([\w\-]+)\/?$/i
     ],
 
-    getMeta: function(urlMatch) {
-        return;
-    },
+    mixins: [
+        "og-image",
+        "favicon",
+        "canonical",
+        "video",
+        "twitter-description",
+        "media-detector",
+        "og-site",
+        "twitter-title"
+    ],
 
-    getLinks: function(urlMatch, meta) {
+    getLinks: function(urlMatch) {
         var fullUrl = urlMatch[0];
         var videoId = urlMatch[2];
         var playerId = urlMatch[1];
+        var src = fullUrl;
+        var html = "<iframe id='"+videoId+"' src='"+src+"'></iframe>";
+
         return [{
-          type: CONFIG.T.text_html,
-          rel: [CONFIG.R.app, CONFIG.R.inline, CONFIG.R.html5],
-          html: "<h1>Boom</h1>"
+            type: CONFIG.T.text_html,
+            rel: [
+                CONFIG.R.player,
+                CONFIG.R.inline,
+                CONFIG.R.html5
+            ],
+            html: html
         }];
     }
 };
