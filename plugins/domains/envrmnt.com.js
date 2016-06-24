97c9e9fd39d8e3302410216a13d8522408100e83
Raphael Eidus <raphael.eidus@huffingtonpost.com>
2016-06-24 11:14:53 -0400

    use some built in data

--- plugins/domains/envrmnt.com.js
+++ plugins/domains/envrmnt.com.js
@@ -2,22 +2,29 @@ console.log("envrmnt.com loaded");
 
 module.exports = {
 
+    mixins: [
+        "og-title",
+        "og-image",
+        "canonical"
+    ],
+
     re: [
         /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/share\/\?share=([\w\-]+)\/([\w\-]+)\/?$/i
     ],
 
     getMeta: function(urlMatch) {
-      return;
+        return;
     },
 
-    getLink: function(urlMatch) {
-      var fullUrl = urlMatch[0];
-      var videoId = urlMatch[2];
-      var playerId = urlMatch[1];
-      return {
-        type: CONFIG.T.text_html,
-        rel: [CONFIG.R.app, CONFIG.R.inline, CONFIG.R.html5],
-        html: "<h1>Boom</h1>"
-      };
+    getLinks: function(urlMatch, meta) {
+        console.log(__width, __height);
+        var fullUrl = urlMatch[0];
+        var videoId = urlMatch[2];
+        var playerId = urlMatch[1];
+        return [{
+          type: CONFIG.T.text_html,
+          rel: [CONFIG.R.app, CONFIG.R.inline, CONFIG.R.html5],
+          html: "<h1>Boom</h1>"
+        }];
     }
 };
