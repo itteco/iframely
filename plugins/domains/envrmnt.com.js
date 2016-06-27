1d57daa482a199870c8cb7728f52a6eec6f39688
Raphael Eidus <raphael.eidus@huffingtonpost.com>
2016-06-27 11:36:57 -0400

    update embed code to include a script

--- plugins/domains/envrmnt.com.js
+++ plugins/domains/envrmnt.com.js
@@ -1,8 +1,7 @@
-console.log("envrmnt.com loaded");
-
 module.exports = {
     re: [
-        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/share\/\?share=([\w\-]+)\/([\w\-]+)\/?$/i
+        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/share\/\?share=(?:[\w\-]+)\/([\w\-]+)\/?$/i,
+        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/#\/video\/([\w\-]+)\/?$/i
     ],
 
     mixins: [
@@ -18,10 +17,9 @@ module.exports = {
 
     getLinks: function(urlMatch) {
         var fullUrl = urlMatch[0];
-        var videoId = urlMatch[2];
-        var playerId = urlMatch[1];
-        var src = fullUrl;
-        var html = "<iframe id='"+videoId+"' src='"+src+"'></iframe>";
+        var videoId = urlMatch[1];
+        var src = "http://cloud-www.envrmnt.com/embed/#/video/"+videoId;
+        var html = '<script async src="http://cloud-www.envrmnt.com/scripts/embed_v1.js"></script><div style="width: 100%; height: 0px; position: relative; padding-bottom: 56.2493%;"><iframe id="envrmnt_'+videoId+'" data-envrmnt-id="'+videoId+'" src="'+src+'" frameborder="0" allowfullscreen style="width: 100%; height: 100%; position: absolute;"></iframe></div>';
 
         return [{
             type: CONFIG.T.text_html,
