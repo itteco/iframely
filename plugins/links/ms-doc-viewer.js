7358d0e4ac2f0c9e2439717f9edab6a4f4aad442
Ivan Paramonau <i.paramonau@gmail.com>
2016-06-29 02:47:53 -0400

    docs viewer: limit file size to 10mb

--- plugins/links/ms-doc-viewer.js
+++ plugins/links/ms-doc-viewer.js
@@ -1,8 +1,11 @@
+// https://support.office.com/en-gb/article/View-Office-documents-online-1cc2ea26-0f7b-41f7-8e0e-6461a104544e?ui=en-US&rs=en-GB&ad=GB
+
 module.exports = {
 
     getLink: function(url, __nonHtmlContentData) {
 
-        if (/application\/vnd\.openxmlformats\-officedocument|ms\-powerpoint|msword|ms\-excel|ms\-office/.test(__nonHtmlContentData.type)) {
+        if (/application\/vnd\.openxmlformats\-officedocument|ms\-powerpoint|msword|ms\-excel|ms\-office/.test(__nonHtmlContentData.type)
+            && (!__nonHtmlContentData.content_length || __nonHtmlContentData.content_length < 10 * 1024 * 1024)) {
             return {
                 href: "https://view.officeapps.live.com/op/embed.aspx?src=" + encodeURIComponent(url),
                 type: CONFIG.T.text_html,
