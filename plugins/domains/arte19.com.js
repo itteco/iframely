0a61464b276d6dd390c3b3cdfda13661b3e31100
Ivan Paramonau <i.paramonau@gmail.com>
2016-07-01 10:05:36 -0400

    domains: allow themes for Art19.com

---
+++ plugins/domains/arte19.com.js
@@ -0,0 +1,53 @@
+var $ = require('cheerio');
+
+module.exports = {
+
+    re: [
+        /^(https?:\/\/art19\.com\/shows\/[a-zA-Z0-9\-_]+\/episodes\/[a-zA-Z0-9\-_]+)/i
+    ],
+
+    mixins: [
+        "oembed-title",
+        "oembed-description",
+        "oembed-thumbnail",
+        "oembed-site",
+        "domain-icon"
+    ],
+
+    getLink: function(oembed, options) {
+
+        if (oembed.html) {
+
+            var $container = $('<div>');
+            try{
+                $container.html(oembed.html);
+            } catch(ex) {}
+
+            var $iframe = $container.find('iframe');
+
+            if ($iframe.length == 1) {
+
+                var player = $iframe.attr('src');
+                var theme = options.getProviderOptions('art19.theme') || 'light-gray-blue';
+
+                if (/theme=\w+/.test(player)) {
+                    player.replace (/theme=\w+/, theme); 
+                } else {
+                    player += '?theme=' + theme;
+                }
+
+                return {
+                    href: player,
+                    type: CONFIG.T.text_html,
+                    rel: [CONFIG.R.player, CONFIG.R.html5],
+                    height: oembed.height
+                };
+            }
+        }
+    },
+
+    tests: [
+        "https://art19.com/shows/fox-sports-live/episodes/4c924063-ca48-487c-ad93-b2b3485a267c",
+        "https://art19.com/shows/the-audible/episodes/6cfafe96-16e8-44bc-8d18-6a69ff807c63"
+    ]
+};
