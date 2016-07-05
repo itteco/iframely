d107ff4178db760ad35ce864b26eb34ca2f7b7cd
Nazar Leush <n.leush@gmail.com>
2016-07-05 12:43:37 +0300

    [core] use http-parser-js : fix issues such as corrupt HTTP headers

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 (function() {
+
+    // Monkey patch before you require http for the first time.
+    process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
+
     var _ = require('underscore');
     var path = require('path');
     var fs = require('fs');
