cb975cef1748176b9a916fae4a83edc9662c5daf
ASAP <alexander.sapountzis@gmail.com>
2016-07-07 12:06:23 -0400

    update embed

--- plugins/domains/envrmnt.com.js
+++ plugins/domains/envrmnt.com.js
@@ -1,7 +1,6 @@
 module.exports = {
     re: [
-        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/share\/\?share=(?:[\w\-]+)\/([\w\-]+)\/?$/i,
-        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/#\/video\/([\w\-]+)\/?$/i
+        /^https?:\/\/(?:cloud-)?www\.envrmnt\.com\/embed\/v1\/#\/video\/([\w\-]+)\?([\w\-]+)$/i
     ],
 
     mixins: [
@@ -15,22 +14,28 @@ module.exports = {
         "twitter-title"
     ],
 
+
+    getMeta: function (urlMatch) {
+        return {
+            videoId: urlMatch[1]
+        }
+    },
+
     getLinks: function(urlMatch) {
         var fullUrl = urlMatch[0];
         var videoId = urlMatch[1];
+        var userUUID = urlMatch[2];
 
-        var scriptURL = "http://cloud-www.envrmnt.com/scripts/embed_v1.js";
+        var scriptURL = "http://fe-www.envrmnt.com/lib/ext-embed-v1.js";
         var wrapperCSS = "width: 100%; height: 0px; position: relative; padding-bottom: 56.2493%;";
-        var iframeSrc = "http://cloud-www.envrmnt.com/embed/#/video/"+videoId;
+        var iframeSrc = "http://www.envrmnt.com/embed/v1/#/video/"+videoId;
         var iframeCss = "width: 100%; height: 100%; position: absolute;";
-        var fullscreenCSS = ".envrmnt--fullscreen {top: 0; right: 0; bottom: 0; left: 0; height: 100%; width: 100%; position: fixed; z-index: 9999999;}";
 
         var html = '<script async src="'+scriptURL+'"></script>'+
-                   '<style>'+fullscreenCSS+'</style>'+
                    '<div style="'+wrapperCSS+'">'+
-                   '<iframe id="envrmnt_'+videoId+'" data-envrmnt-id="'+videoId+'" '+
+                   '<div><iframe id="envrmnt_'+videoId+'" data-envrmnt-id="'+videoId+'" data-vrplayer '+
                    'src="'+iframeSrc+'" frameborder="0" allowfullscreen '+
-                   'style="'+iframeCss+'"></iframe>'+
+                   'style="'+iframeCss+'"></iframe></div>'+
                    '</div>';
 
         return [{
