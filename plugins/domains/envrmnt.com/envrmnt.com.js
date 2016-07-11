7379a84d4413bf387ce4d35ba01323f1f02faa24
Ivan Paramonau <i.paramonau@gmail.com>
2016-07-11 13:38:31 -0400

    domains: ssl-proof envrmnt.com

--- plugins/domains/envrmnt.com/envrmnt.com.js
+++ plugins/domains/envrmnt.com/envrmnt.com.js
@@ -1,6 +1,6 @@
 module.exports = {
     re: [
-        /^https?:\/\/(?:[a-z]+-)?www\.envrmnt\.com\/#\/video\/([\w\-]+)\??([\w\-]+)?$/i
+        /^https?:\/\/([a-z\-]+)\.envrmnt\.com\/(?:#\/)?video\/([\w\-]+)\??([\w\-]+)?$/i
     ],
 
     provides: 'envrmnt',
@@ -15,7 +15,7 @@ module.exports = {
     },
 
     getData: function(urlMatch, request, cb) {
-        var videoId = urlMatch[1];
+        var videoId = urlMatch[2];
 
         var metaURI = "http://media.envrmnt.com/media/vrexperience/" + videoId;
 
@@ -35,17 +35,19 @@ module.exports = {
 
     },
 
-    getLinks: function(envrmnt) {
+    getLinks: function(envrmnt, urlMatch) {
 
         return [{
             type: CONFIG.T.text_html,
             rel: [
                 CONFIG.R.player,
                 CONFIG.R.inline,
+                CONFIG.R.ssl,
                 CONFIG.R.html5
             ],
             template_context: {
-                id: envrmnt.uuid
+                id: envrmnt.uuid,
+                subdomain: urlMatch[1]
             }
         }, {
             href: envrmnt.thumbnailImageUrl,
@@ -59,6 +61,7 @@ module.exports = {
     },
         "http://www.envrmnt.com/#/video/6ec3f3f8-69f4-4b29-b64d-14992e19005f?3e6d6290-cd65-46b8-b4a6-1b1b6e3674d9",
         "http://www.envrmnt.com/#/video/619125f4-8c30-4549-bddb-4ece058797cb?c2efeabd-bb85-4363-b23c-e7f3eb7e160c",
-        "http://www.envrmnt.com/#/video/ba1e1de1-54db-4962-8cac-40cd66aad44b"
+        "http://www.envrmnt.com/#/video/ba1e1de1-54db-4962-8cac-40cd66aad44b",
+        "http://vrxp-www.envrmnt.com/#/video/6ec3f3f8-69f4-4b29-b64d-14992e19005f?3e6d6290-cd65-46b8-b4a6-1b1b6e3674d9"
     ]
 };
