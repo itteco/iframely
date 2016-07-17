a8c795d94333a9daa50d477b9623e9f53c043b6a
Ivan Paramonau <i.paramonau@gmail.com>
2016-07-17 05:32:10 -0400

    domains: add hockeydb.com

---
+++ plugins/domains/hockeydb.com.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+    re: /^https?:\/\/www\.hockeydb\.com\/ihdb\/stats\/pdisplay\.php\?pid=(\d+)/i,
+
+    mixins: ["*"],
+
+    getLink: function(urlMatch, cheerio) {
+
+        var links = [{
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.app, CONFIG.R.html5],
+            html:'<script type="text/javascript" src="http://www.hockeydb.com/em/?pid=' + urlMatch[1] + '"></script>',
+            // href: 'http://www.hockeydb.com/em/?pid=' + urlMatch[1],
+            width: 604
+        }];
+
+        // we also need a thumbnail for SSL fallback to summary card
+        var $img = cheerio('img[itemprop*="image"]');
+        if ($img.length) {
+
+            console.log('img=' + $img);
+
+            links.push({
+                href: $img.attr('src'),
+                rel: CONFIG.R.thumbnail,
+                type: CONFIG.T.image
+            });
+        }        
+
+        return links;
+    },
+
+    tests: [
+        "http://www.hockeydb.com/ihdb/stats/pdisplay.php?pid=89716",
+        "http://www.hockeydb.com/ihdb/stats/pdisplay.php?pid=73254"
+    ]
+};
