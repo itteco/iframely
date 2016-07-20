514f3ef763851604570c84ad1821edf4a6fe8849
Nazar Leush <n.leush@gmail.com>
2016-07-20 18:32:35 +0300

    [server] implement graceful shutdown

---
+++ graceful-shutdown.js
@@ -0,0 +1,46 @@
+var sysUtils = require('./utils');
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = function(server) {
+
+    // Solution got from: https://github.com/nodejs/node-v0.x-archive/issues/9066#issuecomment-124210576
+
+    var state = new EventEmitter;
+    state.shutdown = false;
+
+    server.shutdown = function () {
+        server.close(function() {
+            process.exit(0);
+        });
+        state.shutdown = true;
+        state.emit('shutdown');
+    };
+    server.on('connection', function (socket) {
+        function destroy() {
+            if (socket.HAS_OPEN_REQUESTS === 0) socket.destroy();
+        }
+        socket.HAS_OPEN_REQUESTS = 0;
+        state.once('shutdown', destroy);
+        socket.once('close', function () {
+            state.removeListener('shutdown', destroy);
+        });
+    });
+    server.on('request', function (req, res) {
+        var socket = req.connection;
+        socket.HAS_OPEN_REQUESTS++;
+        res.on('finish', function () {
+            socket.HAS_OPEN_REQUESTS--;
+            if (state.shutdown && socket.HAS_OPEN_REQUESTS === 0) {socket.destroy();}
+        });
+    });
+
+    // Bind to termination events.
+
+    function gracefulExit() {
+        sysUtils.log('Start graceful stutdown: wait connections to close.');
+        server.shutdown();
+    }
+    process.on('SIGTERM', gracefulExit);
+    process.on('SIGINT', gracefulExit);
+
+};
--- server.js
+++ server.js
@@ -119,3 +119,7 @@ console.log('');
 console.log(' - support@iframely.com - if you need help');
 console.log(' - twitter.com/iframely - news & updates');
 console.log(' - github.com/itteco/iframely - star & contribute');
+
+if (!CONFIG.DEBUG) {
+    require('./graceful-shutdown')(listener);
+}
