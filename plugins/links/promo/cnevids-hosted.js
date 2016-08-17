f22babc81dc3c2c77e8e95b22f9d161c7ce655d5
Ivan Paramonau <i.paramonau@gmail.com>
2016-08-17 11:03:21 -0400

    generic: add hosted CNE vids

---
+++ plugins/links/promo/cnevids-hosted.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    provides: 'oembedLinks',
+
+    getData: function(video_src) {
+        
+        if (/^https?:\/\/player\.cnevids\.com\/embed\/[0-9a-zA-Z]+\/[0-9a-zA-Z]+/i.test(video_src)) {
+            
+            return {oembedLinks: [{
+                    href: "https://player.cnevids.com/services/oembed?url=" + video_src,                    
+                    rel: 'alternate',
+                    type: 'application/json+oembed'
+                }]}
+
+        } 
+    }
+
+    // http://video.self.com/watch/self-x-tone-it-up-challenge 
+
+};
