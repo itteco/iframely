dc63dd1b8311e57bffc506d0a7d045a1ef67c288
Ivan Paramonau <i.paramonau@gmail.com>
2016-08-17 12:54:33 -0400

    domains: return bbc

---
+++ plugins/domains/bbc.com.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+    mixins: [
+        "*"
+    ],
+
+    getLink: function(twitter) {
+
+        if (twitter.player && twitter.player.value) {
+
+            var playlist = twitter.player.value.match(/playlist=(\/\w+\/[a-zA-Z0-9-]+\-\d+)\w?&/);
+
+            if (playlist) {                
+                
+                return {
+                    href: "http://www.bbc.com/" + playlist[1] + '/embed',
+                    rel: [CONFIG.R.player],
+                    type: CONFIG.T.text_html,
+                    "aspect-ratio": 16/9,
+                    'padding-bottom': 225, 
+                    scrolling: 'no'
+                }
+            }
+        }
+    },
+
+    tests: [
+        "http://www.bbc.com/news/science-environment-23767323",
+        "http://www.bbc.com/mundo/media-36726262",
+        "http://www.bbc.com/news/business-35324289"
+    ]
+};
