d73bfe06ef81e1c3001ef5b25f3c9d97ab0c0c5b
Ivan Paramonau <i.paramonau@gmail.com>
2016-08-17 14:09:37 -0400

    domains: adding NPR music video section

--- plugins/domains/npr.org/npr.sections.js
+++ plugins/domains/npr.org/npr.sections.js
@@ -25,14 +25,30 @@ module.exports = {
                     'max-width': 800
                 };
             }
-        }
 
+        } else { // try embedded video for NPR Music section
+
+            var $player = cheerio('[data-jwplayer]');
+
+            if ($player.length) {
+                
+                var config = JSON.parse($player.attr('data-jwplayer'));
+                
+                return {
+                    href: config.embedLink,
+                    type: CONFIG.T.text_html,
+                    rel: [CONFIG.R.player, CONFIG.R.html5],
+                    'aspect-ratio': 16 / 9
+                };
+            }
+        }
     },
 
     tests: [
         "http://www.npr.org/sections/thetwo-way/2016/04/11/473874785/hundreds-protest-gerrymandering-campaign-finance-laws-on-capitols-steps",
         "http://www.npr.org/sections/alltechconsidered/2015/04/29/401236656/libraries-make-space-for-3-d-printers-rules-are-sure-to-follow",
-        "http://www.npr.org/sections/itsallpolitics/2015/08/27/434872755/exactly-what-kind-of-socialist-is-bernie-sanders"
+        "http://www.npr.org/sections/itsallpolitics/2015/08/27/434872755/exactly-what-kind-of-socialist-is-bernie-sanders",
         // "http://www.npr.org/2016/05/04/476793671/first-listen-mudcrutch-2"
+        "http://www.npr.org/2016/08/12/489769830/anderson-paak-the-free-nationals-tiny-desk-concert" // jw player
     ]
 };
