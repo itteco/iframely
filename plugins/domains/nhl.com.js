f5ffc920e4c3a29f5a35a887080bc5a5c60f9d94
Ivan Paramonau <i.paramonau@gmail.com>
2016-08-25 12:09:47 -0400

    domains: support reverse embeds URLs for NHL

--- plugins/domains/nhl.com.js
+++ plugins/domains/nhl.com.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     re: [        
-        /https?:\/\/(?:www\.)?nhl\.com(\/\w+\/)video\/([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i,
-        /https?:\/\/(?:www\.)?nhl\.com(\/)video\/([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i
+        /https?:\/\/(?:www\.)?nhl\.com(\/\w+\/)video\/(?:embed\/)?([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i,
+        /https?:\/\/(?:www\.)?nhl\.com(\/)video\/(?:embed\/)?([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i        
     ],
 
     mixins: ["*"],
@@ -25,6 +25,8 @@ module.exports = {
     tests: [
         "https://www.nhl.com/blackhawks/video/whatsyourgoal-emilie/t-277443408/c-40739903",
         "https://www.nhl.com/video/toews-ot-winner/t-277350912/c-40861003",
-        "https://www.nhl.com/video/niskanens-go-ahead-goal/t-277983160/c-40970503"
+        "https://www.nhl.com/video/niskanens-go-ahead-goal/t-277983160/c-40970503",
+        "https://www.nhl.com/video/embed/abramov-interview-video/t-277521762/c-44299003",
+        "https://www.nhl.com/ru/video/embed/abramov-interview-video/t-277521762/c-44299003"
     ]
 };
