f4d0542cfe62eddc8690ed927ab02f52b7f2536b
Ivan Paramonau <i.paramonau@gmail.com>
2016-09-02 12:46:21 -0400

    domains: vox cards do not work under ssl

--- plugins/domains/vox.com/vox.cards.js
+++ plugins/domains/vox.com/vox.cards.js
@@ -13,7 +13,7 @@ module.exports = {
                     slug: urlMatch[1]
                 },
                 type: CONFIG.T.text_html,
-                rel: [CONFIG.R.app, CONFIG.R.ssl]
+                rel: [CONFIG.R.app] // as of Sept 2, 2016 - SSL throws active warnings
         }
     },
 
