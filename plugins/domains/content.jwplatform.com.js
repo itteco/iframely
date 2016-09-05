e14fc0a1acdf1b5fb99617849ec854978bb8ac13
Ivan Paramonau <i.paramonau@gmail.com>
2016-09-05 14:03:51 -0400

    promo: fix incorrect aspect ratio for some jw players

---
+++ plugins/domains/content.jwplatform.com.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    mixins: [
+        "og-image",
+        "canonical",
+        "og-description",
+        "media-detector",
+        "og-title"
+    ],
+
+    // fixes incorrect aspect ratio of 320 / 260
+    getLink: function(twitter) {
+
+        if (twitter.player && twitter.player.value) {
+
+            var aspect = twitter.player.width / twitter.player;
+                
+            return {
+                href: twitter.player.value,
+                rel: [CONFIG.R.player, CONFIG.R.html5],
+                type: CONFIG.T.text_html,
+                "aspect-ratio": aspect < 4/3 ? 16/9 : aspect
+            }
+        }
+    },
+
+    tests: [
+        "https://content.jwplatform.com/players/s6tol0gj-plsZnDJi.html"
+    ]
+};
