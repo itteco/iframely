a8b0c9ac31959f43452c37a61318005621767760
Ivan Paramonau <i.paramonau@gmail.com>
2016-09-15 09:36:32 -0400

    domains: keep up with espn

--- plugins/domains/espn.com.js
+++ plugins/domains/espn.com.js
@@ -1,23 +1,18 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/(?:www\.)?espn\.com\/video\/clip\?id=(espn):(\d+)/i,
-        /^https?:\/\/(?:www\.)?(espn)\.com\/video\/clip\?id=(\d+)/i,
-        /^https?:\/\/xgames\.espn\.com\/(xgames)\/video\/(\d+)\//i
+        /^https?:\/\/(?:www\.)?espn\.com\/video\/clip\?id=espn:(\d+)/i,
+        /^https?:\/\/(?:www\.)?espn\.com\/video\/clip\?id=(\d+)/i,
+        /^https?:\/\/xgames\.espn\.com\/xgames\/video\/(\d+)\//i
     ],
 
     mixins: ["*"],
 
     getLink: function(urlMatch) {
 
-        // Fallback to Twitter:player as it was before March 8, 2016
-
-        var player_id = urlMatch[1] === 'xgmames' ? 'id=' + urlMatch[2] + '&omniReportSuite=wdgespexpn' :
-                        'cms=espn&id=espn:' + urlMatch[2];
-
         return {
-            href: 'https://espn.go.com/video/iframe/twitter/?' + player_id,
-            type: CONFIG.T.maybe_text_html, // ping href to check that it doesn't 404
+            href: 'http://www.espn.com/core/video/iframe?id=' + urlMatch[1] + '&endcard=false',
+            type: CONFIG.T.text_html,
             rel: [CONFIG.R.player, CONFIG.R.html5],
             "aspect-ratio": 16/9
         };
