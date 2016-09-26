8efaba8d634cf61f9781fcd40abca5d479e3becb
Ivan Paramonau <i.paramonau@gmail.com>
2016-09-26 11:15:56 -0400

    domains: support for NBC news sections

--- plugins/domains/nbcnews.com.js
+++ plugins/domains/nbcnews.com.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/www\.nbcnews\.com\/videos?\/[a-zA-Z0-9-]+\-(\d+)/i,
+        /^https?:\/\/www\.nbcnews\.com\/(?:[a-z\-]+\/)?videos?\/[a-zA-Z0-9-]+\-(\d+)/i,
         /^https?:\/\/www\.nbcnews\.com\/widget\/video-embed\/(\d+)/i
     ],
 
@@ -31,6 +31,7 @@ module.exports = {
 
     tests: [
         "http://www.nbcnews.com/video/obama-america-is-not-as-divided-as-some-suggest-721895491854",
-        "http://www.nbcnews.com/video/watch-live-president-obama-speaks-from-new-york-768509507658"
+        "http://www.nbcnews.com/video/watch-live-president-obama-speaks-from-new-york-768509507658",
+        "http://www.nbcnews.com/nightly-news/video/wife-s-video-shows-deadly-encounter-between-keith-scott-and-police-772184131883"
     ]
 };
