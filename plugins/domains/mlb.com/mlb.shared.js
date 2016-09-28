e68e35b986a2226ca59f8cab78371de98c886098
Ivan Paramonau <i.paramonau@gmail.com>
2016-09-28 11:06:03 -0400

    domains: support embed urls for MLB

---
+++ plugins/domains/mlb.com/mlb.shared.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    re: [
+        /^https?:\/\/m\.mlb\.com\/shared\/video\/embed\/embed\.html\?content_id=(\d+)&topic_id=(\d+)/
+        
+    ],
+
+    getLink: function(urlMatch, cb) {
+
+        cb ({
+            redirect: "http://m.mlb.com/video/?content_id=" + urlMatch[1] + "&topic_id=" + urlMatch[2]            
+        });
+    },
+
+    tests: [{
+        noFeeds: true,
+        skipMethods: ["getLink"]
+    },
+        "http://m.mlb.com/shared/video/embed/embed.html?content_id=721945083&topic_id=6479266&width=400&height=224&property=mlb"
+    ]
+};
