7d579ea260bc7e8667db5d51c740fe6b101f596d
Ivan Paramonau <i.paramonau@gmail.com>
2016-10-24 12:17:49 -0400

    domains: add piktochart

---
+++ plugins/domains/piktochart.com/piktochart.com.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    re: /^https?:\/\/magic\.piktochart\.com\/output\/(\d+\-[\-a-zA-Z-0-9_]+)/i,
+
+    mixins: [
+        "*"
+    ],    
+
+    getLink: function(urlMatch) {
+        return {
+
+            template_context: {
+                id: urlMatch[1]
+            },
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.reader, CONFIG.R.html5, CONFIG.R.ssl],
+        };
+    },
+
+    tests: [
+        "https://magic.piktochart.com/output/16331484-new-piktochart",
+        "https://magic.piktochart.com/output/16505417-projet-immigration-copy"
+    ]
+
+};
