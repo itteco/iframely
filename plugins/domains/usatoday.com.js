5081be6719196a4cba81efdb1729304b7302a794
Ivan Paramonau <i.paramonau@gmail.com>
2016-11-07 08:37:27 -0500

    domains: add usatoday network

---
+++ plugins/domains/usatoday.com.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+    re: [
+        /^https?:\/\/www\.(\w+)\.com\/media\/cinematic\/video\/(\d{7,9})\/[a-zA-Z0-9\-\_]+\/?$/i,
+        /^https?:\/\/www\.(\w+)\.com\/videos\/\w+\/\w+\/\d{4}\/\d{2}\/\d{2}\/(\d{7,9})\/?$/i        
+    ],
+
+    provides: "gannettVideo",
+
+    mixins: [
+        "*"
+    ],
+
+    getData: function (urlMatch, twitter) {
+
+        if (/^https?:\/\/www\.gannett\-cdn\.com\//i.test(twitter.image)) {
+            return {
+                gannettVideo: {
+                    id: urlMatch[2],
+                    domain: urlMatch[1]
+                }
+            }
+        }
+    },
+
+    getLink: function (gannettVideo) {
+        return {
+            href: 'http://www.' + gannettVideo.domain +'.com/videos/embed/' + gannettVideo.id + '/?fullsite=true',
+            rel: [CONFIG.R.player, CONFIG.R.html5],
+            type: CONFIG.T.maybe_text_html, // let validators check it
+            'aspect-ratio': 16/9,
+            scrolling: 'no'
+        }
+    },
+
+    tests: [
+        "http://www.usatoday.com/media/cinematic/video/87694100/abdul-jabbar-mocks-trump-says-im-jordan/",
+        "http://www.usatoday.com/videos/news/nation/2016/07/29/87694100/",
+        "http://www.usatoday.com/videos/life/people/2016/11/03/93261598/",
+        "http://www.usatoday.com/videos/news/humankind/2016/09/20/90730062/",
+        "http://www.desertsun.com/media/cinematic/video/92390930/police-chief-quit-ignoring-red-flags/",
+        ""
+    ]
+};
