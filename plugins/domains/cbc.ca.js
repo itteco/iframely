1cff86f9c4e163e335f3b50a37927087aed7d0fe
Ivan Paramonau <i.paramonau@gmail.com>
2016-11-15 14:59:19 -0500

    domains: better sizing for CBC podcasts

--- plugins/domains/cbc.ca.js
+++ plugins/domains/cbc.ca.js
@@ -14,18 +14,26 @@ module.exports = {
             return;
         }
 
-        var href = twitter.player.value.replace(/^https:\/\//, 'http://'); // autoPlay = true will be removed by generic validators
-
-        return {
+        var href = twitter.player.value.replace(/^https:\/\//, 'http://'); // autoPlay = true will be removed by generic validators        
+        var player = {
                 href: href,
                 type: CONFIG.T.text_html,
-                rel: [CONFIG.R.player, CONFIG.R.html5],
-                "aspect-ratio": twitter.player.width / twitter.player.height
+                rel: [CONFIG.R.player, CONFIG.R.html5],                
             };
+
+        if (twitter.image && /^https?:\/\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+$/i.test(twitter.image)) {
+            player.height = 180;            
+        }  else {
+            player["aspect-ratio"] = twitter.player.width / twitter.player.height;
+        }
+
+        return player;
     },
 
     tests: [
         "http://www.cbc.ca/player/play/2558388650",
-        "http://www.cbc.ca/player/Shows/Shows/Dragons+Den/ID/2558469970/"
+        "http://www.cbc.ca/player/Shows/Shows/Dragons+Den/ID/2558469970/",
+        "http://www.cbc.ca/player/play/2695081582",
+        "http://www.cbc.ca/player/play/2695940101"
     ]
 };
