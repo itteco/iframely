80f09be65e07785e8d1505e5b6288179bcc99bb3
Ivan Paramonau <i.paramonau@gmail.com>
2016-11-15 10:54:55 -0500

    domains: better support for iTunes singles

--- plugins/domains/itunes.apple.com/apple.music.js
+++ plugins/domains/itunes.apple.com/apple.music.js
@@ -1,8 +1,9 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/(?:geo\.)?itunes\.apple\.com\/(\w{2})\/(album)(?:\/[^\/]+)?\/id(\d+)\?i=(\d+)?/i,
-        /^https?:\/\/(?:geo\.)?itunes\.apple\.com\/(\w{2})\/(album|playlist)(?:\/[^\/]+)?\/id(?:pl\.)?(\d+)/i
+        /^https?:\/\/(?:geo\.)?itunes\.apple\.com\/(\w{2})\/(album)(?:\/[^\/]+)?\/id(\d+)\?i=(\d+)?/i,        
+        /^https?:\/\/(?:geo\.)?itunes\.apple\.com\/(\w{2})\/(album|playlist)(?:\/[^\/]+)?\/id(?:pl\.)?(\d+)/i,
+        /^https?:\/\/(?:geo\.)?itunes\.apple\.com\/()(album)\/id(\d+)\?/i
     ],
 
     mixins: [
@@ -12,6 +13,7 @@ module.exports = {
     getLink: function(urlMatch) {
 
         var country = urlMatch[1];
+
         var type =  urlMatch[2];        
         var id = urlMatch[4] || urlMatch[3];
 
@@ -20,7 +22,7 @@ module.exports = {
         }
 
         return {
-            href: '//tools.applemusic.com/embed/v1/' + type + '/' + (type === 'playlist' ? '/pl.':'/' ) + id + '?country=' + country,
+            href: '//tools.applemusic.com/embed/v1/' + type + (type === 'playlist' ? '/pl.':'/' ) + id + '?country=' + country,
             type: CONFIG.T.text_html,
             rel: [CONFIG.R.html5, CONFIG.R.player],
             "height": type === 'playlist' || type === 'album' ? 500 : 110
@@ -30,6 +32,7 @@ module.exports = {
     tests: [
         'https://itunes.apple.com/us/album/12-12-12-concert-for-sandy/id585701590?v0=WWW-NAUS-ITSTOP100-ALBUMS&ign-mpt=uo%3D4',
         'https://itunes.apple.com/us/album/id944094900?i&ls=1',
+        'https://itunes.apple.com/album/id1170687816?ls=1',
         'https://geo.itunes.apple.com/us/album/trap-or-die-3/id1163301872?app=music',
         'https://geo.itunes.apple.com/us/album/fake-love/id1168503200?i=1168503281&mt=1&app=music',
         'https://itunes.apple.com/ca/album/greatest-feat.-kendrick-lamar/id1149503804?i=1149503816&app=music&ign-mpt=uo%3D4',
