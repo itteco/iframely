fb5efddd76e37448d8424829caccd07a0ac5419e
Ivan Paramonau <i.paramonau@gmail.com>
2017-01-13 16:31:32 -0500

    domains: fix thescene

--- plugins/links/promo/cnevids-hosted.js
+++ plugins/links/promo/cnevids-hosted.js
@@ -12,10 +12,18 @@ module.exports = {
                     type: 'application/json+oembed'
                 }]}
 
-        } 
+        } else if (/^https?:\/\/player\-backend\.cnevids\.com\/embed\/[0-9a-zA-Z]+\/[0-9a-zA-Z]+/i.test(video_src)) {
+            return {
+                __promoUri: {
+                    url: video_src.replace (/player\-backend/, 'player'),
+                    rel: 'No rel=promo is required' // this field is just for debugging here. Not required
+                }
+            };            
+        }
     }
 
     // http://video.self.com/watch/self-x-tone-it-up-challenge
     // http://video.architecturaldigest.com/watch/the-simple-beauty-of-rafael-vinoly-s-architecture
+    // https://thescene.com/watch/pitchfork/pitchfork-live-watch-jessy-lanza-perform-never-enough?source=player_scene_logo
 
 };
