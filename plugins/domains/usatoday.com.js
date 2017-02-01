84bbb8a86a535157a3276552e514df5e74b64420
Ivan Paramonau <i.paramonau@gmail.com>
2017-02-01 12:49:30 -0500

    domains: cover usatoday domains with '-'

--- plugins/domains/usatoday.com.js
+++ plugins/domains/usatoday.com.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/www\.(\w+)\.com\/media\/cinematic\/video\/(\d{7,9})\/[a-zA-Z0-9\-\_:\.]+\/?$/i,
-        /^https?:\/\/www\.(\w+)\.com\/media\/cinematic\/video\/(\d{7,9})\/?$/i,
-        /^https?:\/\/www\.(\w+)\.com\/videos\/\w+\/(?:[a-z0-9\/]+)?\d{4}\/\d{2}\/\d{2}\/(\d{7,9})\/?$/i,
-        /^https?:\/\/www\.(\w+)\.com\/videos\/\w+\/(?:[a-z0-9\/]+)?\d{4}\/\d{2}\/\d{2}\/[a-zA-Z0-9\-\_\.:]+\/(\d{7,9})\/?$/i
+        /^https?:\/\/www\.([a-z\-]+)\.com\/media\/cinematic\/video\/(\d{7,9})\/[a-zA-Z0-9\-\_:\.]+\/?$/i,
+        /^https?:\/\/www\.([a-z\-]+)\.com\/media\/cinematic\/video\/(\d{7,9})\/?$/i,
+        /^https?:\/\/www\.([a-z\-]+)\.com\/videos\/\w+\/(?:[a-z0-9\-\/]+)?\d{4}\/\d{2}\/\d{2}\/(\d{7,9})\/?$/i,
+        /^https?:\/\/www\.([a-z\-]+)\.com\/videos\/\w+\/(?:[a-z0-9\-\/]+)?\d{4}\/\d{2}\/\d{2}\/[a-zA-Z0-9\-\_\.:]+\/(\d{7,9})\/?$/i
     ],
 
     provides: "gannettVideo",
@@ -47,4 +47,25 @@ module.exports = {
         "http://www.usatoday.com/videos/news/2017/01/07/what-know-ft.-lauderdale-airport-shooter/96291014/",
         "http://www.cincinnati.com/media/cinematic/video/9430427/"
     ]
+
+    /*
+        http://www.guampdn.com/videos/news/nation/2015/08/18/31948487/  
+        http://www.courier-journal.com/videos/sports/college/kentucky/2015/08/17/31862551/
+        http://www.newarkadvocate.com/videos/sports/high-school/football/2015/08/15/31789999/
+        http://www.citizen-times.com/videos/news/2015/08/17/31865067/
+        http://www.bucyrustelegraphforum.com/videos/news/2015/08/15/31799953/
+        http://www.sctimes.com/videos/weather/2015/08/17/31839437/
+        http://www.baxterbulletin.com/videos/news/local/2015/08/17/31843911/
+        http://www.delmarvanow.com/videos/sports/high-school/2015/08/18/31933549/
+        http://www.courier-journal.com/videos/entertainment/2015/08/18/31920575/
+        http://www.detroitnews.com/videos/sports/nfl/lions/2015/08/19/31954181/
+        http://www.press-citizen.com/videos/news/education/k-12/2015/08/18/31959369/
+        http://www.tennessean.com/videos/entertainment/2015/08/18/31958929/
+        http://www.coloradoan.com/videos/sports/2015/08/18/31951489/
+        http://www.thenewsstar.com/videos/sports/college/gsu/2015/08/18/31950105/
+        http://www.hawkcentral.com/videos/sports/college/iowa/football/2015/08/13/31628619/
+        http://www.sheboyganpress.com/videos/sports/golf/2015/08/16/31830213/
+        http://www.packersnews.com/videos/sports/nfl/packers/2015/08/15/31800211/
+        http://www.shreveporttimes.com/videos/news/2015/08/18/31906711/
+    */    
 };
