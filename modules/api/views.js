baca76575647d4392bfab0f8276f5e48e505fd5a
Nazar Leush <n.leush@gmail.com>
2017-04-11 21:35:37 +0300

    check omit_css in oembed html code generation

--- modules/api/views.js
+++ modules/api/views.js
@@ -144,7 +144,8 @@ module.exports = function(app) {
                 autoplayMode: getBooleanParam(req, 'autoplay'),
                 aspectWrapperClass:     omit_css ? CONFIG.DEFAULT_OMIT_CSS_WRAPPER_CLASS : false,
                 maxWidthWrapperClass:   omit_css ? CONFIG.DEFAULT_MAXWIDTH_WRAPPER_CLASS : false,
-                omitInlineStyles: omit_css
+                omitInlineStyles: omit_css,
+                forceWidthLimitContainer: true
             });
 
             var forceGroup = req.query.group ? getBooleanParam(req, 'group') : CONFIG.GROUP_LINKS;
@@ -395,7 +396,8 @@ module.exports = function(app) {
             });
 
             var oembed = oembedUtils.getOembed(uri, result, {
-                mediaPriority: getBooleanParam(req, 'media')
+                mediaPriority: getBooleanParam(req, 'media'),
+                omit_css: getBooleanParam(req, 'omit_css')
             });
 
             if (req.query.format === "xml") {
