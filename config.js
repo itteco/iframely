21f4ec305bbc127323ddaea0c9f45b9a5e56fd3a
Nazar Leush <n.leush@gmail.com>
2017-05-03 22:06:18 +0300

    [config] better constant name

--- config.js
+++ config.js
@@ -41,7 +41,7 @@
 
         SKIP_IFRAMELY_RENDERS: false,
         DEFAULT_ASPECT_RATIO: 16 / 9,
-        MAX_VERTICAL_ASPECT: 1,
+        MAX_VERTICAL_ASPECT_RATIO: 1,
 
         DEFAULT_OMIT_CSS_WRAPPER_CLASS: 'iframely-responsive',
         DEFAULT_MAXWIDTH_WRAPPER_CLASS: 'iframely-embed',
