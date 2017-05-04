ed939471b11719164236de30dc5a60a2961bfb05
Ivan Paramonau <i.paramonau@gmail.com>
2017-05-04 09:42:44 -0400

    facebook: support for iFrame srcs of comment embeds

--- plugins/domains/facebook.com/facebook.redirects.js
+++ plugins/domains/facebook.com/facebook.redirects.js
@@ -7,7 +7,8 @@ module.exports = {
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/login\.php/i,
         /^https?:\/\/(m|\w{2}\-\w{2})\.facebook\.com/i,
         /^https?:\/\/(?:touch\.|www\.)?facebook\.com\/l\.php\?u=/i,
-        /^https?:\/\/www\.facebook\.com\/plugins\/(?:video|post)\.php\?href=/i 
+        /^https?:\/\/www\.facebook\.com\/plugins\/(?:video|post)\.php\?href=/i,
+        /^https?:\/\/www\.facebook\.com\/plugins\/comment_embed\.php\?href=/i         
     ],
 
     getData: function(url, meta, cb) {
@@ -19,13 +20,14 @@ module.exports = {
             return cb({redirect: url.replace(/(m|\w{2}\-\w{2})\.facebook\.com/i, "www.facebook.com")});
         }
 
-        if (url.indexOf('facebook.com/l.php?u=') > -1 || url.indexOf('facebook.com/plugins/video.php?href=') > -1 || url.indexOf('facebook.com/plugins/post.php?href=') > -1) {
+        if (url.indexOf('facebook.com/l.php?u=') > -1 || /facebook\.com\/plugins\/(?:video|post|comment_embed)\.php\?href=/i.test(url)) {
             var uri = URL.parse(url,true);
             var query = uri.query;
 
             // https://www.facebook.com/l.php?u=https://www.youtube.com/watch?v=OpONaotsgow
             // https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcfgflint%2Fvideos%2Fvb.170211653052916%2F1080088265398579%2F%3Ftype%3D3&show_text=0&width=560
             // https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FWhiteMooseCafe%2Fposts%2F1789080394705902
+            // https://www.facebook.com/plugins/comment_embed.php?href=https%3A%2F%2Fwww.facebook.com%2FNowThisPolitics%2Fvideos%2F1544622832235868%2F%3Fcomment_id%3D1544625588902259
             return cb({redirect: decodeURIComponent(query.u || query.href)});
         }
 
