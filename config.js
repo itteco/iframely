aa3b2d405f98fadbb9bc0e021f4d7ac5f5d53567
Nazar Leush <n.leush@gmail.com>
2017-05-18 18:46:03 +0300

    fix word

--- config.js
+++ config.js
@@ -39,7 +39,7 @@
 
         FB_USER_AGENT: 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
 
-        ACCEPT_LANGUAGE_POSTFIX: ';q=0.9,en-US;q=0.8,en;q=0.7,*;q=0.5',
+        ACCEPT_LANGUAGE_SUFFIX: ';q=0.9,en-US;q=0.8,en;q=0.7,*;q=0.5',
 
         SKIP_IFRAMELY_RENDERS: false,
         DEFAULT_ASPECT_RATIO: 16 / 9,
