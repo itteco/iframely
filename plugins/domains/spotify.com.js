7911a59ba4ee07220bb8ed792b89f4ee3006aa5b
Ivan Paramonau <i.paramonau@gmail.com>
2017-05-31 16:30:06 -0400

    domains: spotify web api is no longer available

--- plugins/domains/spotify.com.js
+++ plugins/domains/spotify.com.js
@@ -5,7 +5,8 @@ module.exports = {
     mixins: [
         "oembed-title",
         "oembed-thumbnail",
-        "oembed-site"
+        "oembed-site",
+        "domain-icon"
     ],
 
     // keep dependency on oEmbed only. Otherwise, there's redirect to relative path for "play.*" and no embeds as a result
@@ -25,16 +26,12 @@ module.exports = {
         // if embed code contains <iframe>, return src
         if ($iframe.length == 1) {
 
-            return [{
+            return {
                 href: $iframe.attr('src'),
                 type: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.ssl, CONFIG.R.html5],
                 height: oembed.height || 80
-            }, {
-                href: "http://d2c87l0yth4zbw.cloudfront.net/i/_global/favicon.png",
-                type: CONFIG.T.image,
-                rel: CONFIG.R.icon
-            }]
+            }
         }
 
     },
@@ -50,6 +47,12 @@ module.exports = {
         "http://open.spotify.com/track/6ol4ZSifr7r3Lb2a9L5ZAB",
         "http://open.spotify.com/user/cgwest23/playlist/4SsKyjaGlrHJbRCQwpeUsz",
         "http://open.spotify.com/album/42jcZtPYrmZJhqTbUhLApi",
-        "https://open.spotify.com/user/bradgarropy/playlist/0OV99Ep2d1DCENJRPuEtXV"
+        "https://open.spotify.com/user/bradgarropy/playlist/0OV99Ep2d1DCENJRPuEtXV",
+        "http://open.spotify.com/track/6ol4ZSifr7r3Lb2a9L5ZAB",
+        "https://open.spotify.com/track/4by34YzNiEFRESAnBXo7x4",
+        "https://open.spotify.com/track/2qZ36jzyP1u29KaeuMmRZx",
+        "http://open.spotify.com/track/7ldU6Vh9bPCbKW2zHE65dg",
+        "https://play.spotify.com/track/2vN0b6d2ogn72kL75EmN3v",
+        "https://play.spotify.com/track/34zWZOSpU2V1ab0PiZCcv4"        
     ]
 };
