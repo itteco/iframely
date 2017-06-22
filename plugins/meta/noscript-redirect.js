0be9125e655e61bc9fae7c8e58bdb1e1b36535a1
Ivan Paramonau <i.paramonau@gmail.com>
2017-06-22 13:46:25 -0400

    domains: fix for twitter homepage and domain-icon

--- plugins/meta/noscript-redirect.js
+++ plugins/meta/noscript-redirect.js
@@ -2,7 +2,7 @@ module.exports = {
 
     getData: function(url, meta, cb) {
 
-        return cb((meta.refresh && (!meta['html-title'] || meta['html-title'] === '' || !meta.title || /re\-?direct/i.test(meta['html-title']))) 
+        return cb((meta.refresh && (!meta.description || /re\-?direct/i.test(meta['html-title']))) 
             ? {
                 redirect: meta.refresh
             } : null);
