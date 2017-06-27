89da7f0caf58147ef4f4c6f6801a11992acfa337
Nazar Leush <n.leush@gmail.com>
2017-06-27 15:44:15 +0300

    disable log for noindex-robots

--- plugins/meta/noindex-robots.js
+++ plugins/meta/noindex-robots.js
@@ -2,10 +2,6 @@ module.exports = {
 
     getData: function(url, meta, cb) {
 
-    	if (meta.robots && /noindex/i.test(meta.robots) && !meta.description && !meta.og && !meta.twitter) {
-			console.log("robots not allowed for " + url);
-		}
-
         return cb(meta.robots && /noindex/i.test(meta.robots) && !meta.description && !meta.og && !meta.twitter
             ? {
                responseStatusCode: 403
