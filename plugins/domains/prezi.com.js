aea709155511383f18b5f2084e6267f9aa2ddf2d
Ivan Paramonau <i.paramonau@gmail.com>
2017-06-27 08:01:29 -0400

    domains: make prezi more reliable

--- plugins/domains/prezi.com.js
+++ plugins/domains/prezi.com.js
@@ -1,30 +1,39 @@
 module.exports = {
 
-    re: [
-        /^https?:\/\/prezi\.com\/(?!embed)(?!bin)(?!press)([a-z0-9_-]+)\/[a-z0-9_-]+/i,
-        /^https?:\/\/prezi\.com\/embed\/([a-z0-9_-]+)\//i,
-        /^https?:\/\/prezi\.com\/bin\/preziloader\.swf\?prezi_id=([a-z0-9_-]+)/i
-    ],
-
     mixins: ["*"],
 
     getMeta: function(meta) {
-        return {
-            author_url: meta.prezi_for_facebook.author
+        if (meta.ld && meta.ld.mediaobject) {
+            return {
+                author: meta.ld.mediaobject.author.name,
+                author_url: meta.prezi_for_facebook.author,
+                date: meta.ld.mediaobject.datecreated
+            }
         }
     },
 
-    getLink: function(urlMatch) {
+    getLink: function(meta) {
 
-        return {
-            href: 'https://prezi.com/embed/' + urlMatch[1] + '/',
-            type: CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.html5],
-            "aspect-ratio": 550 / 400
+        var player;
+
+        if (meta.og && meta.og.video && meta.og.video.url) {
+            player = meta.og.video.url;
+        } else if (meta.ld && meta.ld.mediaobject && meta.ld.mediaobject.contenturl) {
+            player = meta.ld.mediaobject.contenturl;
+        }
+
+        if (player && /^https?:\/\/prezi\.com\/bin\/preziloader\.swf\?prezi_id=([a-z0-9_-]+)/i.test(player)) {
+            return {
+                href: 'https://prezi.com/embed/' + player.match(/^https?:\/\/prezi\.com\/bin\/preziloader\.swf\?prezi_id=([a-z0-9_-]+)/i)[1] + '/',
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.html5],
+                "aspect-ratio": 550 / 400
+            }
         }
     },
 
     tests: [
-        "http://prezi.com/hvsanqexuoza/designthinking-vs-leanstartup/"
+        "http://prezi.com/hvsanqexuoza/designthinking-vs-leanstartup/",
+        "https://prezi.com/penxp_esmqzu/e-learning-mooc-spoc-et-compagnie/"
     ]
 };
