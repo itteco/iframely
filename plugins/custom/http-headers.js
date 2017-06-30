2e9f8ff28e4963cab4ec0785a511f5ec4fe57953
Ivan Paramonau <i.paramonau@gmail.com>
2017-06-30 16:35:51 -0400

    custom: expose headers if required

---
+++ plugins/custom/http-headers.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+	provides: "headers",
+
+    getData: function(htmlparser, cb) {
+
+        if (htmlparser.request && htmlparser.request.response && htmlparser.request.response.headers) {
+            return cb (null, {
+                headers: htmlparser.request.response.headers
+            })
+        } else {
+            cb();
+        }
+    }
+};
