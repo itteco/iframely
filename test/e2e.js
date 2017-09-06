e181adc35f38bed3eb3231f09fd56fef4e5c7ab0
Nazar Leush <n.leush@gmail.com>
2017-09-06 22:32:07 +0300

    tests: try fix blacklist test

--- test/e2e.js
+++ test/e2e.js
@@ -192,7 +192,7 @@ describe('meta endpoint', function() {
     request(BASE_IFRAMELY_SERVER_URL)
         .get('/iframely?url=' + url)
         .end(function(err, res) {
-          chai.expect(res.statusCode).to.equal(418);
+          chai.expect(res.statusCode).to.equal(417);
           chai.expect(res.body).to.deep.equal({
             error: {
               source: 'iframely',
