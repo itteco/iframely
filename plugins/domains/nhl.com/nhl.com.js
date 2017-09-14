5e6df995ff38d52e0844435b1a1f5e401c5c7f77
Ivan Paramonau <i.paramonau@gmail.com>
2017-09-14 12:15:12 -0400

    tests: fix for NHL redirects

--- plugins/domains/nhl.com/nhl.com.js
+++ plugins/domains/nhl.com/nhl.com.js
@@ -2,34 +2,19 @@ module.exports = {
 
     re: [
         /https?:\/\/(?:www\.)?nhl\.com(\/\w+\/)video\/(?:embed\/)?([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i,
-        /https?:\/\/(?:www\.)?nhl\.com(\/)video\/(?:embed\/)?([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i,
-        /https?:\/\/(?:www\.)?nhl\.com\/video\/([a-zA-Z0-9\-]+\/c\-\d+)/i
+        /https?:\/\/(?:www\.)?nhl\.com(\/)video\/(?:embed\/)?([a-zA-Z0-9\-]+\/t\-\d+\/c\-\d+)/i
     ],
 
     mixins: ["*"],
 
-    getLinks: function(urlMatch, url, meta, options, cb) {
+    getLinks: function(urlMatch, url, meta) {
 
-        if (urlMatch.length < 3) {
-
-            if (meta.canonical && url !== meta.canonical && (!options.redirectsHistory || options.redirectsHistory.indexOf(meta.canonical) === -1)) {
-
-                return cb ({
-                    redirect: meta.canonical
-                });
-            } else {
-                return cb (null, null);
-            }
-
-        } else {
-
-            return cb (null, {
-                href: "https://www.nhl.com" + urlMatch[1] + "video/embed/" + urlMatch[2] + "?autostart=false",
-                type: CONFIG.T.text_html,
-                rel: [CONFIG.R.player,  CONFIG.R.html5],
-                "aspect-ratio": 540 / 310,
-                autoplay: 'autostart=true'
-            });
+        return {
+            href: "https://www.nhl.com" + urlMatch[1] + "video/embed/" + urlMatch[2] + "?autostart=false",
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.player,  CONFIG.R.html5],
+            "aspect-ratio": 540 / 310,
+            autoplay: 'autostart=true'
         }
     },
 
--- plugins/links/embedURL/checkEmbedURL.js
+++ plugins/links/embedURL/checkEmbedURL.js
@@ -4,8 +4,6 @@ module.exports = {
 
     getData: function(meta, whitelistRecord) {
 
-        console.log(JSON.stringify(whitelistRecord));
-
         // Allow slow cheerio parser, if whitelisted and no ld-json,
         // But do not call, if there is twitter:player as it's usually the best one
         if (whitelistRecord.isAllowed('html-meta.embedURL') && !(meta.ld && meta.ld.videoobject && whitelistRecord.date > new Date(1485530476990))
