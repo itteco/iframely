088e0a0008aa9aca30785b98cea044df254f683c
Ivan Paramonau <i.paramonau@gmail.com>
2017-09-14 11:25:29 -0400

    general: fix embedURLs

--- plugins/links/embedURL/checkEmbedURL.js
+++ plugins/links/embedURL/checkEmbedURL.js
@@ -4,9 +4,11 @@ module.exports = {
 
     getData: function(meta, whitelistRecord) {
 
+        console.log(JSON.stringify(whitelistRecord));
+
         // Allow slow cheerio parser, if whitelisted and no ld-json,
         // But do not call, if there is twitter:player as it's usually the best one
-        if (whitelistRecord.isAllowed('html-meta.embedURL') && !(meta.ld && whitelistRecord.date > new Date(1485530476990))
+        if (whitelistRecord.isAllowed('html-meta.embedURL') && !(meta.ld && meta.ld.videoobject && whitelistRecord.date > new Date(1485530476990))
             && !(whitelistRecord.isAllowed('twitter.player') && meta.twitter && meta.twitter.card == "player")) {
             return {
                 __allowEmbedURL: true
