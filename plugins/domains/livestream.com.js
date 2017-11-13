d2992076c6d6b83846607464ec6a429a5b909132
Ivan Paramonau <i.paramonau@gmail.com>
2017-11-13 11:42:45 -0500

    domains: fix some livestreams

--- plugins/domains/livestream.com.js
+++ plugins/domains/livestream.com.js
@@ -1,10 +1,12 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/livestream\.com\/\w+\/events\/(\d+)(?:[^\/])?$/,
-        /^https?:\/\/livestream\.com\/accounts\/(\d+)\/events\/(\d+)\/player/,
-        /^https?:\/\/livestream\.com\/\w+\/events\/(\d+)(?:[^\/])?$/,
-        /^https?:\/\/livestream\.com\/\w+\/\w+\/?\?t=\d+$/
+        /^https?:\/\/livestream\.com\/(\w+)\/events\/(\d+)(\/videos?\/\d+)?/i,
+        /^https?:\/\/livestream\.com\/accounts\/(\d+)\/events\/(\d+)(\/videos?\/\d+)?/i,
+        /^https?:\/\/livestream\.com\/accounts\/(\d+)\/(\w+)(\/videos?\/\d+)?/i,
+        /^https?:\/\/livestream\.com\/(\w+)\/events\/(\d+)(\/videos?\/\d+)?/i,
+        /^https?:\/\/livestream\.com\/(\w+)\/(\w+)(\/videos?\/\d+)/i,
+        /^https?:\/\/livestream\.com\/(\w+)\/(\w+)(?:\/?\?t=\d+)?$/i        
     ],    
 
     mixins: [
@@ -13,8 +15,9 @@ module.exports = {
 
     getLink: function (meta, urlMatch) {
 
-        var event_id = urlMatch[2] ? urlMatch[2] : urlMatch[1];
-        var account_id = urlMatch[2] ? urlMatch[1] : null;
+        var account_id = /^\d+$/.test(urlMatch[1]) ? urlMatch[1] : null;
+        var event_id = /^\d+$/.test(urlMatch[2]) ? urlMatch[2] : null;
+        var video_id = urlMatch[3] ? urlMatch[3] : '';
 
         if (!account_id && meta['apple-itunes-app'] && /https?:\/\/livestream\.com\/accounts\/\d+\/events\/\d+\/?/i.test(meta['apple-itunes-app'])) {
             account_id = meta['apple-itunes-app'].match(/https?:\/\/livestream\.com\/accounts\/(\d+)\/events\/\d+/i)[1];
@@ -26,7 +29,7 @@ module.exports = {
 
         if (event_id && account_id) {
             return {
-                href: "https://livestream.com/accounts/" + account_id + "/events/" + event_id + "/player?autoPlay=false",
+                href: "https://livestream.com/accounts/" + account_id + "/events/" + event_id + video_id + "/player?autoPlay=false",
                 type: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.html5],
                 "aspect-ratio": 16/9,
@@ -38,7 +41,15 @@ module.exports = {
 
     tests: [
         "https://livestream.com/abeldanger/events/6926279",
+        "https://livestream.com/accounts/242049/events/1978582",
+        "https://livestream.com/ironman/events/7777204/videos/163920520",
         "https://livestream.com/sevierheights/maincampus?t=1486305168991",
-        "https://livestream.com/accounts/2787677/events/6862936/player?width=0&height=0&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false"
+        "https://livestream.com/kywildcatstv/mbbmediaday2017",
+        "https://livestream.com/kywildcatstv/mbbmediaday2017/videos/164155010",
+        "https://livestream.com/myanmaritv/live",
+        "https://livestream.com/accounts/1862284/ChannelersOfChange/videos/161518819?t=1503250404953",
+        "https://livestream.com/accounts/1862284/ChannelersOfChange",
+        "https://livestream.com/accounts/2787677/events/6862936/player?width=0&height=0&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false",
+        "https://livestream.com/kywildcatstv/vermontpost/videos/165870460"
     ]
 };
