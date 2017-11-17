994fdbcccab6b72bffcb169a270627c69d4cabed
Ivan Paramonau <i.paramonau@gmail.com>
2017-11-17 10:09:20 -0500

    domains: more Giphy media redirects

--- plugins/domains/i.giphy.com.js
+++ plugins/domains/i.giphy.com.js
@@ -2,7 +2,7 @@ module.exports = {
 
     re: [
         /https?:\/\/i\.giphy\.com\/(\w+)\.gif(\?.*)?$/i,
-        /https?:\/\/media\d?\.giphy\.com\/media\/(\w+)\/(?:giphy|source|\d+)(?:_\w)?\.gif$/i,        
+        /https?:\/\/media\d?\.giphy\.com\/media\/(\w+)\/(?:giphy|source|\d+)(?:_?\w)?\.gif$/i,        
         /https?:\/\/giphy\.com\/gifs\/(\w+)\/html5$/i,
         /https?:\/\/giphy\.com\/embed\/(\w+)/i        
     ],
@@ -21,6 +21,7 @@ module.exports = {
         "http://media.giphy.com/media/m4r4RTpCzkh0I/giphy.gif",
         "http://i.giphy.com/10rNBP8yt1LUnm.gif",
         "http://giphy.com/gifs/FC8MlptXIrCWk/html5",
-        "https://media.giphy.com/media/3o6Zt09XtyqOJWVgRO/source.gif"
+        "https://media.giphy.com/media/3o6Zt09XtyqOJWVgRO/source.gif",
+        "https://media3.giphy.com/media/r7GbhvJfeMlig/200w.gif"
     ]
 };
