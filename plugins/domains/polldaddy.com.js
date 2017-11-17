3ed60272de1372fa7e4b5f7ae8ec237533808544
Ivan Paramonau <i.paramonau@gmail.com>
2017-11-17 10:04:37 -0500

    domains: better support for polldaddy surveys

--- plugins/domains/polldaddy.com.js
+++ plugins/domains/polldaddy.com.js
@@ -1,7 +1,8 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/polldaddy\.com\/poll\/([0-9]+)/i
+        /^https?:\/\/(?:\w+\.)?polldaddy\.com\/poll\/([0-9]+)/i,
+        /^https?:\/\/(?:\w+\.)?polldaddy\.com\/s\//i
     ],
 
     mixins: [
@@ -13,11 +14,11 @@ module.exports = {
         "twitter-title"
     ],
 
-    getLink: function(oembed) {
+    getLink: function(oembed, url) {
 
         return {
             type: CONFIG.T.text_html,
-            rel: [CONFIG.R.survey, CONFIG.R.ssl, CONFIG.R.html5],
+            rel: /^https?:\/\/(?:\w+\.)?polldaddy\.com\/s\//i.test(url) ? [CONFIG.R.survey, CONFIG.R.inline, CONFIG.R.ssl, CONFIG.R.html5] : [CONFIG.R.survey, CONFIG.R.ssl, CONFIG.R.html5],
             html: oembed.html.replace(/src=\"http:\/\/static\.polldaddy\.com/, "src=\"https://secure.polldaddy.com"),
             "min-width": 332
         };
@@ -25,6 +26,7 @@ module.exports = {
 
     tests: [
         "https://polldaddy.com/poll/7451882/?s=twitter",
-        "http://polldaddy.com/poll/9113163/"
+        "http://polldaddy.com/poll/9113163/",
+        "https://wordpressdotorg.polldaddy.com/s/wordpress-2017-survey"
     ]
 };
