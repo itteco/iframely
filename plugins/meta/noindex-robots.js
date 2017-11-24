a8e3da27530eabdd3521add86a82807c27d0b542
Ivan Paramonau <i.paramonau@gmail.com>
2017-11-24 10:27:18 -0500

    general: ignore robots directives if there's oembed

--- plugins/meta/noindex-robots.js
+++ plugins/meta/noindex-robots.js
@@ -1,8 +1,11 @@
 module.exports = {
 
-    getData: function(url, meta, cb) {
+    getData: function(url, meta, __noOembedLinks, cb) {
 
         return cb(meta.robots && /noindex/i.test(meta.robots) && !meta.description && !meta.og && !meta.twitter
+        		&& !(meta.alternate && meta.alternate.filter(function(link) {
+        			return /^(application|text)\/(xml|json)\+oembed$/i.test(link.type);
+    			}))
             ? {
                responseStatusCode: 403,
                message: "The robots directive of this page prevents Iframely from parsing it"
