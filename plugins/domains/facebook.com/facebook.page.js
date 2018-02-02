15f80cb649982814ef9c57ba5e513ab20c6487ff
Ivan Paramonau <i.paramonau@gmail.com>
2018-02-02 08:19:17 -0500

    domains: remove debug rel for FB pages

--- plugins/domains/facebook.com/facebook.page.js
+++ plugins/domains/facebook.com/facebook.page.js
@@ -59,7 +59,7 @@ module.exports = {
 
             links.push ({
                 type: CONFIG.T.text_html,
-                rel: [CONFIG.R.app, CONFIG.R.ssl, CONFIG.R.html5, CONFIG.R.inline],
+                rel: [CONFIG.R.app, CONFIG.R.ssl, CONFIG.R.html5],
                 html: html,
                 "max-width": oembed.width
             });
