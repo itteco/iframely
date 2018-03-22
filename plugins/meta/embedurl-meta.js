3f740eec80210e40cc63727193186daa3d0b98b7
Ivan Paramonau <i.paramonau@gmail.com>
2018-03-22 15:12:57 -0400

    general: parse more of embedurl meta

--- plugins/meta/embedurl-meta.js
+++ plugins/meta/embedurl-meta.js
@@ -6,10 +6,11 @@ module.exports = {
 
         return {
             duration: schemaVideoObject.duration,
-            date: schemaVideoObject.uploadDate || schemaVideoObject.datePublished || schemaVideoObject.dateCreated,
+            date: schemaVideoObject.uploadDate || schemaVideoObject.datePublished || schemaVideoObject.dateCreated || schemaVideoObject.uploaddate,
             // title and description are useful e.g. for Yahoo! Screen
             title: schemaVideoObject.name,
-            description: schemaVideoObject.description
+            description: schemaVideoObject.description,
+            views: schemaVideoObject.interactionCount || schemaVideoObject.interactioncount
         };
     }
 };
