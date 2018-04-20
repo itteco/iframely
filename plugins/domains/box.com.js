14742f979285f5dc91ff08a2710d9764bd215ef7
Ivan Paramonau <i.paramonau@gmail.com>
2018-04-20 13:45:24 -0400

    domains: add more URL patterns for box.com

--- plugins/domains/box.com.js
+++ plugins/domains/box.com.js
@@ -2,7 +2,7 @@ module.exports = {
 
     re: [
         /^https:\/\/app\.box\.com\/(?:embed|embed_widget)?\/?s\/([a-zA-Z0-9]+)/,
-        /^https:\/\/(\w+)\.app\.box\.com\/s\/([a-zA-Z0-9]+)/,
+        /^https:\/\/(\w+)\.app\.box\.com\/(?:embed|embed_widget)?\/?s\/([a-zA-Z0-9]+)/,
         /^https:\/\/app\.box\.com\/embed\/preview\/([a-zA-Z0-9]+)/,
         /^https:\/\/(\w+)\.app\.box\.com\/embed\/preview\/([a-zA-Z0-9]+)/,
     ],
@@ -15,7 +15,7 @@ module.exports = {
 
     	return {
     		href: urlMatch.length > 2 ? "https://" + urlMatch[1] + ".app.box.com/embed_widget/s/" + urlMatch[2] : "https://app.box.com/embed_widget/s/" + urlMatch[1],
-    		rel: [CONFIG.R.reader, CONFIG.R.html5, CONFIG.R.ssl],
+    		rel: [CONFIG.R.reader, CONFIG.R.oembed, CONFIG.R.html5, CONFIG.R.ssl],
     		type: CONFIG.T.text_html,
     		"aspect-ratio": 500 / 400
     	}
