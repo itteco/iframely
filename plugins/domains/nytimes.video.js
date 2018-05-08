c13b097faed67c548710cde57d41e6ff3cb212da
Ivan Paramonau <i.paramonau@gmail.com>
2018-05-08 11:53:00 -0400

    domains: update NYTimes video player src

--- plugins/domains/nytimes.video.js
+++ plugins/domains/nytimes.video.js
@@ -13,8 +13,8 @@ module.exports = {
 
         if (meta.medium == "video") {
             return {
-                href: "//static01.nyt.com/video/players/offsite/index.html?videoId=" + urlMatch[1],
-                type: CONFIG.T.text_html,
+                href: "https://www.nytimes.com/video/players/offsite/index.html?videoId=" + urlMatch[1],
+                type: CONFIG.T.maybe_text_html, // may return X-Frame-Options at the moment. Check it.                
                 rel: [CONFIG.R.player, CONFIG.R.html5],
                 "aspect-ratio": 16/9,
                 "padding-bottom": 71 + 1
@@ -23,6 +23,7 @@ module.exports = {
     },
 
     tests: [
+        "http://www.nytimes.com/video/movies/100000002478606/anatomy-of-a-scene-gravity.html?smid=nytimesarts",
         "http://www.nytimes.com/video/nyregion/100000003880254/a-complicated-love-story.html?playlistId=1194811622241",
         "http://www.nytimes.com/video/realestate/100000003852081/block-by-block-hoboken.html?playlistId=1194811622241",
         "http://www.nytimes.com/video/world/middleeast/100000004055530/turkey-footage-shows-plane-blast.html",
