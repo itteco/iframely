84262b8111a7b3f4bae05790875f47403a8e9ccf
Ivan Paramonau <i.paramonau@gmail.com>
2018-06-11 17:37:26 -0400

    tests: fix twitch

--- plugins/domains/twitch.tv.js
+++ plugins/domains/twitch.tv.js
@@ -1,10 +1,8 @@
 module.exports = {
 
     re: [
-        /^https?:\/\/(?:www\.|go\.)?twitch\.tv\/([a-zA-Z0-9_]+)$/i,
-        /^https?:\/\/(?:www\.|go\.)?twitch\.tv\/([a-zA-Z0-9_]+)\/v\/(\d+)/i,
-        /^https?:\/\/(?:www\.|go\.)?twitch\.tv\/(video)\/(\d+)/i,
-        /^https?:\/\/player\.twitch\.tv\/\?channel=([a-zA-Z0-9_]+)/i
+        /^https?:\/\/(?:www\.|go\.)?twitch\.tv\/[a-zA-Z0-9_]+\/v\/(\d+)/i,
+        /^https?:\/\/(?:www\.|go\.)?twitch\.tv\/video\/(\d+)/i
     ],
 
     mixins: [
@@ -27,13 +25,8 @@ module.exports = {
         if (!meta.og || (meta.og && meta.og.video)) {
 
             // add only potentially missing options for each a channel and a clip
-            return !urlMatch[2] ? {
-                href: "//player.twitch.tv/?channel=" + urlMatch[1]+"&autoplay=false",
-                type: CONFIG.T.text_html,
-                rel: [CONFIG.R.player, CONFIG.R.html5],
-                "aspect-ratio": 16 /9 
-            } : {
-                href: "//player.twitch.tv/?video=v" + urlMatch[2]+'&autoplay=true',
+            return {
+                href: "//player.twitch.tv/?video=v" + urlMatch[1]+'&autoplay=true',
                 type: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.autoplay],
                 "aspect-ratio": 16 /9 
@@ -51,12 +44,6 @@ module.exports = {
     tests: [{
         noFeeds: true, skipMethods: ["getMeta", "getData"]
     },
-        "https://www.twitch.tv/imaqtpie"
-
-        // channels marked for mature audiences cannot be supported:
-        // - those are the same as generic pages like /store
-        // ex.: "http://www.twitch.tv/adultswim",
-
         //"https://www.twitch.tv/riotgames/v/72749628"
     ]
 };
