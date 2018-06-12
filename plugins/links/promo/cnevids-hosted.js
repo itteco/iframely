ef4461275eda2c9c90b4ebcccd91fb2d1c115117
Ivan Paramonau <i.paramonau@gmail.com>
2018-06-11 22:51:19 -0400

    known players: support more of cnevids

--- plugins/links/promo/cnevids-hosted.js
+++ plugins/links/promo/cnevids-hosted.js
@@ -2,10 +2,10 @@ module.exports = {
 
     getData: function(video_src) {
         
-        if (/^https?:\/\/player(?:\-[a-z-Z\-]+)?\.cnevids\.com\/(?:embed|iframe)\/[0-9a-zA-Z]+\/[0-9a-zA-Z]+/i.test(video_src)) {
+        if (/^https?:\/\/player(?:\-[a-z-Z\-]+)?\.cnevids\.com\/(?:embed|iframe|script)\/[0-9a-zA-Z]+\/[0-9a-zA-Z]+/i.test(video_src)) {
             return {
                 __promoUri: {
-                    url: video_src.replace (/player\-[a-z-Z\-]+/, 'player'),
+                    url: video_src.replace(/player\-[a-z-Z\-]+/, 'player').replace('/script/', '/iframe/'),
                     rel: 'No rel=promo is required' // this field is just for debugging here. Not required
                 }
             };            
