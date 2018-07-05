dd5b4ffdefd38668ae872d3e0775c3380905a003
Ivan Paramonau <i.paramonau@gmail.com>
2018-07-05 04:32:51 -0400

    youtube: add nocookie option

--- plugins/domains/youtube.com/youtube.playlist.js
+++ plugins/domains/youtube.com/youtube.playlist.js
@@ -17,9 +17,10 @@ module.exports = {
     getLinks: function(urlMatch, oembed, options) {
 
         var params = options.getProviderOptions('youtube.playlist_params', '');
+        var domain = /^https?:\/\/www\.youtube-nocookie\.com\//i.test(urlMatch[0]) || options.getProviderOptions('youtube.nocookie', false) ? 'youtube-nocookie' : 'youtube';
 
         return {
-            href: 'https://www.youtube.com/embed/videoseries?list=' + urlMatch[1] + params.replace(/^\?/, '&'),
+            href: 'https://www.' + domain + '.com/embed/videoseries?list=' + urlMatch[1] + params.replace(/^\?/, '&'),
             rel: [CONFIG.R.player, CONFIG.R.html5],
             type: CONFIG.T.text_html,
             "aspect-ratio": oembed.width && oembed.height ? oembed.width / oembed.height : 16/9,
