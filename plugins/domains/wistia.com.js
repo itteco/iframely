ce25e64c8cad8c9d5acb26c5031cbffda7941230
Ivan Paramonau <i.paramonau@gmail.com>
2018-10-29 22:05:48 -0400

    domains: validate wistia audios

--- plugins/domains/wistia.com.js
+++ plugins/domains/wistia.com.js
@@ -11,31 +11,33 @@ module.exports = {
 
     getLinks: function(oembed, urlMatch, url) {
 
-        var params = "";
-        // Extract  ?start=123
-        var start = url.match(/(?:start|time)=(\d+)/i);
-        if (start) {
-            params =  "?time=" + start[1];
-        }
+        if (oembed.type === 'video') { 
 
-        return [{
-            href: "//fast.wistia.net/embed/iframe/" + urlMatch[2] + params,
-            type: CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.html5],
-            "aspect-ratio": oembed.width / oembed.height,
-            autoplay: "autoPlay=true"
-        }, {
-            href: "http://wistia.com/favicon.ico",
-            type: CONFIG.T.image,
-            rel: CONFIG.R.icon
-        }];
+            var params = "";
+            // Extract  ?start=123
+            var start = url.match(/(?:start|time)=(\d+)/i);
+            if (start) {
+                params =  "?time=" + start[1];
+            }
+
+            return [{
+                href: "//fast.wistia.net/embed/iframe/" + urlMatch[2] + params,
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.html5],
+                "aspect-ratio": oembed.width / oembed.height,
+                autoplay: "autoPlay=true"
+            }, {
+                href: "http://wistia.com/favicon.ico",
+                type: CONFIG.T.image,
+                rel: CONFIG.R.icon
+            }];
+        } // else - 'rich' is for audio embeds. Let it fall back to generic parsers
     },
 
 
     tests: [{
         noFeeds: true
     },
-        "http://appsumo.wistia.com/medias/fudkgyoejs",
-        "http://convinceandconvert.wistia.com/medias/52lpg5691w?utm_campaign=Argyle+Social-2013-04&utm_medium=Argyle+Social&utm_source=General+Use&utm_term=2013-04-24-13-23-55"
+        "http://appsumo.wistia.com/medias/fudkgyoejs"
     ]
 };
