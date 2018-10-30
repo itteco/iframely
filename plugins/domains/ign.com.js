4b9f901e9899f8d3b96d9e520e05443477e80a63
Ivan Paramonau <i.paramonau@gmail.com>
2018-10-30 10:40:13 -0400

    domains: one more url pattern for IGN

--- plugins/domains/ign.com.js
+++ plugins/domains/ign.com.js
@@ -3,9 +3,10 @@ var _ = require('underscore');
 module.exports = {
 
     re: [
-        /^https?:\/\/\w+\.ign\.com\/\w+\/videos\/\d+\/\w+\/[a-zA-Z0-9-]+/i,
-        /^https?:\/\/\w+\.ign\.com\/videos\/\d+\/\d+\/\d+\/[a-zA-Z0-9-]+/i,
-        /^https?:\/\/\w+\.ign\.com\/(?:\w+\/)?[a-zA-Z0-9-]+\/\d+\/video\//i
+        /^https?:\/\/\w+\.ign\.com\/\w+\/videos?\/\d+\/\w+\/[a-zA-Z0-9-]+/i,
+        /^https?:\/\/\w+\.ign\.com\/videos?\/\d+\/\d+\/\d+\/[a-zA-Z0-9-]+/i,
+        /^https?:\/\/\w+\.ign\.com\/(?:\w+\/)?[a-zA-Z0-9-]+\/\d+\/videos?\//i,
+        /^https?:\/\/\w+\.ign\.com\/\w+\/video\/\d+\/[a-zA-Z0-9-]+/i
     ],
 
 
@@ -35,6 +36,7 @@ module.exports = {
     },
         "https://me.ign.com/en/videos/112217/video/our-favorite-games-of-new-york-comic-con",
         "https://me.ign.com/en/videos/111115/fix/ps4-firmware-30-new-battlefront-details-ign-daily",
+        "https://me.ign.com/en/video/155053/spyro-reignited-trilogy-dino-mines-gameplay",
         "https://me.ign.com/en/game-of-thrones-season-7/152850/video/game-of-thrones-what-tyrion-thinking-during-that-scene-in-se"
     ]
 };
