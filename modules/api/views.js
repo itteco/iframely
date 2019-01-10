989a1a0826c644ee70a5f33c2b759b1a8d03e1c3
Nazar Leush <n.leush@gmail.com>
2019-01-10 21:38:56 +0200

    api: remove options from response

--- modules/api/views.js
+++ modules/api/views.js
@@ -136,6 +136,8 @@ module.exports = function(app) {
                 delete result.safe_html;
             }
 
+            delete result.options;
+
             if (!CONFIG.SKIP_IFRAMELY_RENDERS) {
                 var render_link = _.find(result.links, function(link) {
                     return link.html
