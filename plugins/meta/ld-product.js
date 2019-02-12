0af8759fef7a7f81a10367321c4ffbab347b79f1
Ivan Paramonau <i.paramonau@gmail.com>
2019-02-12 18:38:05 -0500

    generic: slightly better handler for ld product

--- plugins/meta/ld-product.js
+++ plugins/meta/ld-product.js
@@ -5,11 +5,13 @@ module.exports = {
         if (ld.product) {
             return {
                 price: ld.product.price,
-                currency: ld.product.currency_code || ld.product.currency,
+                currency: ld.product.currency_code || ld.product.currency || ld.product.priceCurrency || ld.product.pricecurrency,
                 brand: ld.product.brand,
                 category: ld.product.category,
                 availability: ld.product.availability,
-                offers: ld.product.offers
+                offers: ld.product.offers,
+                title: ld.product.name,
+                description: ld.product.description
             };
         }
     }
