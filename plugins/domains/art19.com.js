93d10fdfed3e8b43de8d1c3c72becd4c98e869b4
Ivan Paramonau <i.paramonau@gmail.com>
2019-02-22 15:13:08 -0500

    declare theme color option for art19

--- plugins/domains/art19.com.js
+++ plugins/domains/art19.com.js
@@ -29,12 +29,12 @@ module.exports = {
             if ($iframe.length == 1) {
 
                 var player = $iframe.attr('src');
-                var theme = options.getProviderOptions('art19.theme') || 'light-gray-blue';
+                var theme = options.getRequesOptions('players.theme', 'light');
 
                 if (/theme=\w+/.test(player)) {
-                    player.replace (/theme=\w+/, theme); 
+                    player.replace (/theme=\w+/, theme == 'light' ? 'light-gray-blue' : 'dark-blue'); 
                 } else {
-                    player += '?theme=' + theme;
+                    player += '?theme=' + (theme == 'light' ? 'light-gray-blue' : 'dark-blue');
                 }
 
                 return {
@@ -42,7 +42,15 @@ module.exports = {
                     type: CONFIG.T.text_html,
                     rel: [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.oembed], // keep rel oembed here - it prevents validators from removing embed srcz
                     height: oembed.height,
-                    scrolling: 'no'
+                    scrolling: 'no',
+                    options: {
+                        label: 'Theme color',
+                        value: theme,
+                        values: {
+                            light: 'Light',
+                            dark: 'Dark'
+                        }                        
+                    }
                 };
             }
         }
@@ -55,7 +63,6 @@ module.exports = {
         ]
     },
         "https://art19.com/shows/fox-sports-live/episodes/4c924063-ca48-487c-ad93-b2b3485a267c",
-        "https://art19.com/shows/the-audible/episodes/6cfafe96-16e8-44bc-8d18-6a69ff807c63",
         "https://art19.com/shows/the-audible/episodes/ba9724d3-8a79-4e8f-a5dd-0f816f217e50/embed?theme=light-gray-blue",
         "https://art19.com/shows/skip-and-shannon-undisputed/embed"
     ]
