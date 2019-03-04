14831421c419b92d026ac16e5ae25d6545ea1517
Ivan Paramonau <i.paramonau@gmail.com>
2019-03-03 21:06:10 -0500

    options: add dark theme for simplecast

--- plugins/domains/simplecast.com.js
+++ plugins/domains/simplecast.com.js
@@ -1,27 +1,43 @@
 module.exports = {
 
     // used for hosted simplecasts
-    re: /https?:\/\/simplecast\.com\/card\/[a-zA-Z0-9\-]+/i,
+    re: [
+        /^https?:\/\/simplecast\.com\/(?:card|s)\/([a-zA-Z0-9\-]+)/i,
+        /^https?:\/\/embed\.simplecast\.com\/([a-zA-Z0-9\-]+)/i        
+    ],
 
     mixins: ["*"],
 
-    getLink: function(urlMatch) {
+    getLink: function(urlMatch, options) {
+
+        // https://help.simplecast.com/getting-started/how-to-share-your-episodes-on-social-media-and-your-website
+        var theme = options.getRequestOptions('players.theme', 'light');
 
         return {
-            href: urlMatch[0],
+            href: "https://embed.simplecast.com/" + urlMatch[1] + '?color=' + (theme === 'dark' ? '3d3d3d' : 'F5F5F5'),
             type: CONFIG.T.text_html,
-            rel: [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.oembed], // oembed rel makes it pass url!=canonical validation
-            height: 94
+            rel: [CONFIG.R.player, CONFIG.R.html5],
+            height: 200,
+            options: {
+                theme: {
+                    value: theme,
+                    values: {
+                        dark: "Dark theme"
+                    }
+                }
+            }
         };
     },
 
     tests: [
-        "https://simplecast.com/card/6e203f4f"
+        "https://simplecast.com/card/6e203f4f?color=3d3d3d",
+        "https://embed.simplecast.com/c68f4e5d"
         /*
         http://bikeshed.fm/54
         http://bikeshed.fm/57
         http://podcast.emojiwrap.com/episodes/53140-4-but-some-people-hated-the-blob
         https://objectsharp.com/podcast/cosmos-db-everything-you-wanted-to-know-about-microsoft-azures-nosql-offering-in-about-30-minutes/
+        http://podcast.thegadgetflow.com/mark-campbell-inventurex
         */        
     ]
 };
