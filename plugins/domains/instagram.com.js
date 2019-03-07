a497980ff7496455a1e317bdc66597ffb1f38413
Ivan Paramonau <i.paramonau@gmail.com>
2019-03-07 11:19:24 -0500

    domains: speed up instagram load a little

--- plugins/domains/instagram.com.js
+++ plugins/domains/instagram.com.js
@@ -96,12 +96,14 @@ module.exports = {
 
 
             if (/src=\"\/\/www\.instagram.com\/embed\.js\"/i.test(html)) {
-                html = html.replace ('src="//www.instagram.com/embed.js"', 'src="//platform.instagram.com/en_US/embeds.js"');
+                html = html.replace (/src="\/\/www\.instagram\.com\/embed\.js"/, 'src="https://platform.instagram.com/en_US/embeds.js"');
             }
 
             if (options.getProviderOptions('locale')) {
                 html = html.replace('/en_US/embeds.js', '/' + options.getProviderOptions('locale').replace('-', '_') + '/embeds.js');
             }
+            // en_US locale scripts redirects to ./embed.js
+            html = html.replace(/(?:https?:)?\/\/platform\.instagram\.com\/en_US\/embeds\.js/, 'https://www.instagram.com/embed.js');
 
             if (/instagram.com\/tv\//i.test(html)) {
                 // html has /tv/ links in it - but those actually don't work as of 8/27/2018
