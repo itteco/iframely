16f2f1e1e4846dc10c9c6fc28ed5a88efb993624
Ivan Paramonau <i.paramonau@gmail.com>
2019-03-12 15:41:59 -0400

    options: add artwork-only player for libsyn

--- plugins/domains/libsyn.com.js
+++ plugins/domains/libsyn.com.js
@@ -10,23 +10,28 @@ module.exports = {
 
             var is_video = twitter.stream && twitter.stream.value && /\.mp4$/i.test(twitter.stream.value);
 
-            var href = twitter.player.value.match(/^https?:\/\/html5\-player\.libsyn\.com\/embed\/episode\/id\/\d+/i)[0];
-            href += '/theme/custom';
+            var href = twitter.player.value.match(/^https?:\/\/html5\-player\.libsyn\.com\/embed\/episode\/id\/\d+/i)[0];            
 
             if (!is_video) {
 
-                if (options.getRequestOptions('libsyn.render_playlist', false)) {
-                    href += '/height/400';
-                    href += '/direction/backward/render-playlist/yes';
+                if (!options.getRequestOptions('players.horizontal', true)) {
+                    href += '/theme/standard';
                 } else {
-                    href += '/height/90';
-                }
+                    href += '/theme/custom';
 
-                if (options.getRequestOptions('libsyn.hide_artwork', false)) {
-                    href += '/thumbnail/no';
-                }
+                    if (options.getRequestOptions('libsyn.render_playlist', false)) {
+                        href += '/height/400';
+                        href += '/direction/backward/render-playlist/yes';
+                    } else {
+                        href += '/height/90';
+                    }
 
-                href += '/custom-color/' + options.getProviderOptions('libsyn.color', '87A93A');
+                    if (options.getRequestOptions('libsyn.hide_artwork', false)) {
+                        href += '/thumbnail/no';
+                    }
+
+                    href += '/custom-color/' + options.getProviderOptions('libsyn.color', '87A93A');
+                }
             }
 
             var player = {
@@ -42,19 +47,37 @@ module.exports = {
 
                 player.rel.push(CONFIG.R.audio);
 
-                player.options = {
-                    hide_artwork : {
-                        label: 'Hide artwork',
-                        value: /\/thumbnail\/no/.test(href)
-                    },
-                    render_playlist: {
-                        label: 'Add playlist',
-                        value: /\/render\-playlist\/yes/.test(href)
-                    }                    
-                };
+                if (!options.getRequestOptions('players.horizontal', true)) {
+
+                    player.options = {
+                        horizontal: {
+                            label: 'Artwork-only player',
+                            value: true
+                        }
+                    };
+                    player['aspect-ratio'] = 1;
+                    player['padding-bottom'] = 46;
+
+                } else {
+
+                    player.options = {
+                        hide_artwork : {
+                            label: 'Hide artwork',
+                            value: /\/thumbnail\/no/.test(href)
+                        },
+                        render_playlist: {
+                            label: 'Add playlist',
+                            value: /\/render\-playlist\/yes/.test(href)
+                        },
+                        horizontal: {
+                            label: 'Artwork-only player',
+                            value: false
+                        }
+                    };
 
                 player.height = player.href.match(/height\/(\d+)/)[1];
                 player.scrolling = 'no';
+                }
             }
                 
             return player;
