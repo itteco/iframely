97a7280e41c3500876de7b96355f7061544b64a8
Ivan Paramonau <i.paramonau@gmail.com>
2019-03-13 16:23:55 -0400

    domains: validate simplecast 404s

--- plugins/domains/simplecast.com.js
+++ plugins/domains/simplecast.com.js
@@ -41,7 +41,7 @@ module.exports = {
 
         return {
             href: href,
-            type: CONFIG.T.text_html,
+            accept: CONFIG.T.text_html,
             rel: [CONFIG.R.player, CONFIG.R.html5],
             height: horizontal && theme !== 'dark' ? 94 : 200,
             options: opts
