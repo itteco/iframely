aca08db626f6b6458ff41e0a2f10770b3f442d80
Ivan Paramonau <i.paramonau@gmail.com>
2019-10-16 22:29:21 -0400

    tests: fix 500px

--- plugins/domains/500px/500px.com.js
+++ plugins/domains/500px/500px.com.js
@@ -24,7 +24,7 @@ module.exports = {
     tests: [{
         feed: "https://500px.com/upcoming.rss"
     },
-        "http://500px.com/photo/13541787?from=upcoming",
+        "https://web.500px.com/photo/13541787/Long-After-Sunset-In-The-Black-Mountains-by-Jim-Ross/",
         "https://500px.com/photo/56891080/frozen-by-ryan-pendleton?ctx_page=1&from=user&user_id=116369"
     ]
 };
