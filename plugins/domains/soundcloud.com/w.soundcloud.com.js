e53b321c4cd0997c27b165bd0bd8c08c0bd3e372
Ivan Paramonau <i.paramonau@gmail.com>
2020-01-05 14:08:04 -0500

    soundcloud: support more redirects for player widgets URLs

--- plugins/domains/soundcloud.com/w.soundcloud.com.js
+++ plugins/domains/soundcloud.com/w.soundcloud.com.js
@@ -1,26 +1,26 @@
+const URL = require('url');
+
 module.exports = {
 
     re: [
-        /^https:?\/\/w\.soundcloud\.com\/player\/?\?\/?(?:visual=true&)?url=([^&]+)/i,
+        /^https:?\/\/w\.soundcloud\.com\/player\/?\?/i,
         /^https:?\/\/m\.soundcloud\.com\//i
     ],
 
-    getData: function(url, urlMatch, cb) {
+    getData: function(url, cb) {
 
-        var redirect = urlMatch[1] ? urlMatch[1] : url.replace(/^https:?\/\/m\./, 'https://');
+        var query = URL.parse(url, true).query;
+        var mobileCanonical = url.replace(/^https:?\/\/m\./, 'https://');
 
-        cb ({
-            redirect: decodeURIComponent(redirect)
-        });
+        cb ({redirect: query && query.url ? decodeURIComponent(query.url) : mobileCanonical});
     },
 
     tests: [{
         noFeeds: true,
-      
-    },
-        // https://w.soundcloud.com/player/?/url=https://api.soundcloud.com/tracks/212506132
+        },
         // https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/282055227%3Fsecret_token%3Ds-Ct4TV&color=00cc11&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false
         // https://m.soundcloud.com/luke_kelvin_music/leak-lady-gaga-i-wanna-be-with-you
-        // test doesn't pass due to redirect anyway
+        // https://w.soundcloud.com/player?auto_play=false&origin=twitter&show_artwork=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F349557245&visual=true
+        // tests don't pass due to redirect anyway
     ]
 };
