23b71c7c3685eb4b9220a074ef030f56da01fae3
Ivan Paramonau <i.paramonau@gmail.com>
2020-03-11 09:38:45 -0400

    tests: fix tumblr regexp

--- plugins/domains/tumblr.com/tumblr.api.js
+++ plugins/domains/tumblr.com/tumblr.api.js
@@ -5,7 +5,7 @@ module.exports = {
 
     re: [
         /^https?:\/\/([a-z0-9-]+\.tumblr\.com)\/(post|image)\/(\d+)(?:\/[a-z0-9-]+)?/i,
-        /^https?:\/\/([a-z-\.]+)\/(post)\/(\d{9,14})(?:\/[a-z0-9-]+)?(?:\?.*)?$/i
+        /^https?:\/\/([a-z-\.]+)\/(post)\/(\d{9,14})(?:\/[a-z0-9-]+)?(?:\?.*)?(?:#.*)?$/i
     ],
 
     provides: 'tumblr_post',
