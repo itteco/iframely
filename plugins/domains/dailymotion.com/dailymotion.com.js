0df9c5a66e37b406439d719571a98af1fda2e7a4
garmoncheg <garmon1@gmail.com>
2020-03-27 18:02:05 +0200

    domain: dailymotion params enabled (#244)
    * domain: dailymotion params enabled
    
    * domain: dailymotion transmition of get params
    
    * domain: dailymotion remove autoplay
    
    * domain: dailymotion query string config possibility
    
    * domain: dailymotion check href exists
    
    * domain: dailymotion proper mixins and actual methods used

--- plugins/domains/dailymotion.com/dailymotion.com.js
+++ plugins/domains/dailymotion.com/dailymotion.com.js
@@ -1,24 +1,38 @@
+const querystring = require('querystring');
+
 module.exports = {
 
     mixins: [
-
-        // These mixins are disabled as otherwise the age-restricted vids don't work (htmlparser follows re-direct to another URL)
-        //"canonical",
-        //"video",
-
+        /**
+         *  This mixin does not allow Dailymotion parameters to be transmitted
+         *
+         *     //"oembed-video"
+         */
         "oembed-title",
         "oembed-author",
         "oembed-site",
         "oembed-thumbnail",
-        "oembed-video"
+        "domain-icon",
+        "og-description",
+        "canonical",
+        "video"
     ],
 
-    getLink: function (url, oembed, cb) {
-        cb(null, {
-            href: "http://static1.dmcdn.net/images/apple-touch-icon.png.vcbf86c6fe83fbbe11",
-            type: CONFIG.T.image_icon,
-            rel: CONFIG.R.icon
-        });
+    getLink: function (url, oembed, options) {
+        var playlistParams = querystring.parse(options.getProviderOptions('dailymotion.get_params', '').replace(/^\?/, ''));
+        var qs = querystring.stringify(playlistParams);
+        var href = oembed.getIframeAttr('src');
+
+        if (href && oembed.height) {
+            return {
+                href: href + (href.indexOf("?") > -1 ? "&" : "?") + qs,
+                type: CONFIG.T.text_html,
+                "rel": [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.ssl, CONFIG.R.oembed],
+                "aspect-ratio": oembed.width / oembed.height,
+                scrolling: 'no',
+                autoplay: "autoplay=1"
+            };
+        }
     },
 
     tests: [{
@@ -26,6 +40,7 @@ module.exports = {
     },
         "http://www.dailymotion.com/video/x10bix2_ircam-mani-feste-2013-du-29-mai-au-30-juin-2013_creation#.Uaac62TF1XV",
         "http://www.dailymotion.com/swf/video/xcv6dv_pixels-by-patrick-jean_creation",
-        "http://www.dailymotion.com/embed/video/xcv6dv_pixels-by-patrick-jean_creation"
+        "http://www.dailymotion.com/embed/video/xcv6dv_pixels-by-patrick-jean_creation",
+        "https://dailymotion.com/embed/video/x5yiamz?queue-enable=false"
     ]
 };
