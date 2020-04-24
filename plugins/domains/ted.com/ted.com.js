17517e7ada4e96a9d97fc6fa123b3b666ca1a5e5
garmoncheg <garmon1@gmail.com>
2020-04-24 16:57:02 +0300

    domain: ted.com proper language handling

--- plugins/domains/ted.com/ted.com.js
+++ plugins/domains/ted.com/ted.com.js
@@ -1,9 +1,12 @@
+const URL = require("url");
+
 module.exports = {
 
     re: /^https?:\/\/(?:www\.)?ted\.com\/talks\//i,
 
     provides: [
-        "oembedLinks"
+        "oembedLinks",
+        "tedLangs"
     ],    
 
     mixins: [
@@ -18,21 +21,9 @@ module.exports = {
         "oembed-title",
     ],
 
-    getLink: function(oembed, meta, options) {
-        let languageLabels = options.getProviderOptions('languageLabels', {});
-
-        let langs = {};
-        meta.alternate.forEach(function(alternative) {
-            if (typeof(alternative) === "string" && /\?/.test(alternative)) {
-                /** Expect `alternative` to be like:
-                 *  https://www.ted.com/talks/greta_thunberg_the_disarming_case_to_act_right_now_on_climate_change?language=hr
-                 */
-                const langCode = new URLSearchParams(alternative.split('?')[1]).get('language');
-                langs[langCode] = languageLabels[langCode] || langCode;
-            }
-        });
-        const locale = options.getRequestOptions('ted.locale', 'en');
+    getLink: function(oembed, tedLangs, options) {
         const iframe = oembed.getIframe();
+        const locale = options.getRequestOptions('ted.locale', 'en');
         if (iframe && oembed.height) {
             return {
                 type: CONFIG.T.text_html,
@@ -43,7 +34,7 @@ module.exports = {
                     locale: {
                         label: "Locale",
                         value: locale,
-                        values: langs
+                        values: tedLangs
                     }
                 },
             }
@@ -52,12 +43,34 @@ module.exports = {
     },
 
     getData: function(url, meta, options, cb) {
-        var src = 'http://www.ted.com/services/v1/oembed.json?url=' + encodeURIComponent(meta.canonical.toLowerCase());
+        let languageLabels = options.getProviderOptions('languageLabels', {});
+        let langs = {};
+        meta.alternate.forEach(function(alternative) {
+            if (typeof(alternative) === "string" && /\?/.test(alternative)) {
+                /** Expect `alternative` to be like:
+                 *  https://www.ted.com/talks/greta_thunberg_the_disarming_case_to_act_right_now_on_climate_change?language=hr
+                 */
+                const langCode = new URLSearchParams(alternative.split('?')[1]).get('language');
+                langs[langCode] = languageLabels[langCode] || langCode;
+            }
+        });
+        var query = URL.parse(url, true).query;
+        var lang = query.language || options.getRequestOptions('ted.locale');
+        lang = lang ? lang.toLowerCase() : lang;
+        var is_valid_lang = lang && meta.alternate && meta.alternate instanceof Array && meta.alternate.some(function (link) {
+            return typeof link.indexOf === 'function' && link.indexOf('language=' + lang > -1);
+        });
+
+        let oembedUrl = is_valid_lang ? `${meta.canonical.toLowerCase()}?language=${lang}` : meta.canonical.toLowerCase();
+        let src = 'http://www.ted.com/services/v1/oembed.json?url=' + encodeURIComponent(oembedUrl);
+        src += (is_valid_lang ? '&language=' + lang : '');
+
         cb (null, {oembedLinks: [{
                 href: src,
                 rel: 'alternate',
                 type: 'application/json+oembed'
             }],
+            tedLangs: langs
         });
     },
 
