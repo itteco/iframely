6eec080015ffd4768dd77e64a1ea9e31ed98570e
garmoncheg <garmon1@gmail.com>
2020-04-28 19:22:29 +0300

    domain: maps.yandex.ru short url match

---
+++ plugins/domains/maps.yandex.ru/maps.yandex.ru.short.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    re: /^https:\/\/yandex\.ru\/maps\/-\//,
+
+    mixins: [
+        "*"
+    ],
+
+    getLink: function(url) {
+        return {
+            href: url.replace(/\/maps\/-\//, '/map-widget/v1/-/'),
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.app, CONFIG.R.html5, CONFIG.R.ssl],
+            "aspect-ratio": 4/3
+        };
+    },
+
+    tests: [
+        "https://yandex.ru/maps/-/CWS~5QYp"
+    ]
+};
