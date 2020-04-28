1e9ed0b21155bda48f45b284d39b16ac3922d0af
garmoncheg <garmon1@gmail.com>
2020-04-28 04:29:25 +0300

    Domain/ted options (#252)
    * domains: ted subtitles languages added to options editor

--- plugins/domains/ted.com/ted.com.js
+++ plugins/domains/ted.com/ted.com.js
@@ -1,11 +1,12 @@
-const URL = require("url");
+const URL = require('url');
 
 module.exports = {
 
     re: /^https?:\/\/(?:www\.)?ted\.com\/talks\//i,
 
     provides: [
-        "oembedLinks"
+        "oembedLinks",
+        "tedLangs"
     ],    
 
     mixins: [
@@ -18,37 +19,112 @@ module.exports = {
         "keywords",
         "oembed-site",
         "oembed-title",
-        "oembed-video"
     ],
 
-    getData: function(url, meta, options, cb) {
+    getLink: function(oembed, tedLangs) {
+        const iframe = oembed.getIframe();
 
-        var src = 'http://www.ted.com/services/v1/oembed.json?url=' + encodeURIComponent(meta.canonical);
+        if (iframe && oembed.height) {
 
-        if (!/language=/.test(meta.canonical)) {
-            var query = URL.parse(url,true).query;
-            var lang = (options.getProviderOptions('locale') && options.getProviderOptions('locale').replace(/(\_|\-)\w+$/i, '')) || query.language;
-            lang = lang ? lang.toLowerCase() : lang;
-            var is_valid_lang = lang && meta.alternate && meta.alternate instanceof Array && meta.alternate.some(function(link) {
-                return typeof link.indexOf === 'function' && link.indexOf('language='+lang > -1);
-            });
-            src += (is_valid_lang ? '&language=' + lang : '');
+            let link = {
+                href: iframe.src,
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.oembed],
+                "aspect-ratio": oembed.width / oembed.height
+            };
+
+            if (tedLangs.language) {
+                link.options = tedLangs;
+            }
+
+            return link;
         }
 
-        cb (null, {oembedLinks: [{
-                href: src,
+    },
+
+    getData: function(url, meta, options, cb) {
+        let availableLanguages = {};
+
+        /**
+         * Fill available languages from `<link rel="alternate" href="...?language=...",
+         * But skip English as it doesn't turn subtitles on.
+         */
+        if (meta.alternate) {
+            meta.alternate.forEach(function(alternative) {
+                if (typeof(alternative) === "string" && /\?/.test(alternative)) {
+                    const query = URL.parse(alternative.toLowerCase(), true).query;
+                    
+                    if (query.language && query.language !== 'en') {
+                        availableLanguages[query.language] = CONFIG.LC && CONFIG.LC[query.language] || query.language;
+                    }
+
+                }
+            });
+        }
+
+        const query = URL.parse(url.toLowerCase(), true).query;
+
+        let language = options.getRequestOptions(
+                        'ted.language', 
+                        query.language 
+                            || options.getProviderOptions('locale') && options.getProviderOptions('locale').replace(/(\_|\-)\w+$/i, '')
+                        ) || '';
+
+        if (!availableLanguages[language]) {
+            /** oEmbed request fails with 404 if language isn't valid... */
+            language = '';
+            meta.canonical = (meta.canonical || url).replace(/\??language=[\w_\-]+/, '');
+        }
+
+        let data = {
+            oembedLinks: [{
+                href: 'https://www.ted.com/services/v1/oembed.json?url=' 
+                    + encodeURIComponent(meta.canonical || url)                    
+                    + (language !== '' ? '&language=' + language : ''),
                 rel: 'alternate',
                 type: 'application/json+oembed'
             }],
-            message: 'Add "?language=" into your URL for TED subtitles.' // This is here to prevent fallback to default parsers
-        });
+            tedLangs: {}
+        };
+
+        if (Object.keys(availableLanguages).length > 0) {
+            /**
+             * Options form must be able to drop URL's language.
+             * The only way to do so is with any non-empty special value.
+             * It won't pass language validation and will be dropped to ''.
+             */
+            if (/* url. */ query.language && query.language !== 'en') {
+                availableLanguages['-'] = '';
+                if (language === '') {
+                    language = '-';
+                }
+            } else if (Object.keys(availableLanguages).length > 1) {
+                availableLanguages[''] = '';
+            } else {
+                /** 
+                 * For a single language, leave it alone and don't add an empty value
+                 * so the options form transforms into a checkbox. 
+                 * https://iframely.com/docs/options#an-exception-to-options-mapping
+                 */
+            }
+            
+            data.tedLangs = {
+                language: {
+                    label: "Subtitles",
+                    value: language,
+                    values: availableLanguages
+                }
+            }
+        }
+        /** `cb` is needed to be one tick ahead of oembedLinks auto-discovery. */
+        return cb (null, data);
     },
 
     tests: [{
-        page: "http://www.ted.com/talks",
+        page: "https://www.ted.com/talks",
         selector: "#browse-results a"
     }, {skipMethods: ['getData']},
-        "http://www.ted.com/talks/kent_larson_brilliant_designs_to_fit_more_people_in_every_city",
+        "https://www.ted.com/talks/kent_larson_brilliant_designs_to_fit_more_people_in_every_city",
         "https://www.ted.com/talks/neha_narula_the_future_of_money?language=zh-TW"
     ]
 };
