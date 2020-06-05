6bd13665d92ecc50da10a3ab44e884c8d72fbf5a
garmoncheg <garmon1@gmail.com>
2020-06-05 18:19:18 +0300

    fallback: prerender usage (#262)
    * fallback: prerender usage
    
    * fallback: prerender proper config option check
    
    * fallback: put back fb-fallback
    
    * fallback: prerender fix url
    
    * fallback: fix potential recursion
    
    * fallback: prerender fix url
    
    * fallback: prerender fix canonical url
    
    * fallback: prerender proper canonical fix

--- plugins/links/react-app-fb-fallback.js
+++ plugins/links/react-app-fb-fallback.js
@@ -9,7 +9,7 @@ module.exports = {
 
     getData: function(url, __appFlag, options, cb) {
 
-        if (options.user_agent === CONFIG.FB_USER_AGENT) {
+        if (options.user_agent === CONFIG.FB_USER_AGENT || CONFIG.PRERENDER_URL) {
             return cb();
         }
 
