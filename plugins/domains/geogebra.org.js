8336cfce44307daebc066d475b62b3c9f7ab139c
garmoncheg <garmon1@gmail.com>
2020-06-09 21:38:23 +0300

    domain: geogebra.org more link types

--- plugins/domains/geogebra.org.js
+++ plugins/domains/geogebra.org.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     re: [
+		/^https?:\/\/(?:tube|www)\.geogebra\.org\/(?:m|classic|graphing)\/(?:[a-zA-Z0-9]+)#material\/([a-zA-Z0-9#]+)/i,
     	/^https?:\/\/(?:tube|www)\.geogebra\.org\/(?:m|classic|graphing)\/([a-zA-Z0-9]+)/i,
     	/^https?:\/\/(?:tube|www)\.geogebra\.org\/student\/m([a-zA-Z0-9]+)/i,    	
     	/^https?:\/\/(?:tube|www)\.geogebra\.org\/m\/([a-zA-Z0-9]+)/i    	
@@ -12,11 +13,14 @@ module.exports = {
 
     // https://wiki.geogebra.org/en/Embedding_in_Webpages
     getData: function(url, urlMatch, options, cb) {
+		var regExp = /^https?:\/\/(?:tube|www)\.geogebra\.org\/(?:m|classic|graphing)\/([a-zA-Z0-9]+)/i;
+    	var m = regExp.test(url) && url.match(regExp)[1];
 
-    	var m = /^https?:\/\/(?:tube|www)\.geogebra\.org\/(?:classic|graphing)\/([a-zA-Z0-9]+)/i.test(url) 
-    		&& url.match(/^https?:\/\/(?:tube|www)\.geogebra\.org\/(?:classic|graphing)\/([a-zA-Z0-9]+)/i)[1];
-
-    	if (m && (!options.redirectsHistory || options.redirectsHistory.indexOf('https://www.geogebra.org/m/' + m) === -1)) {
+    	if (m
+			&& (
+				!options.redirectsHistory
+				|| options.redirectsHistory.indexOf('https://www.geogebra.org/m/' + m) === -1)
+			&& m === urlMatch[1]) {
 	        cb (null, {
 	        	__promoUri: {url: 'https://www.geogebra.org/m/' + m} // this is to avoid 401 errors on redirects to /m/..
 	        });
@@ -30,6 +34,7 @@ module.exports = {
 	}, {skipMethods: ['getData']},
         "http://tube.geogebra.org/m/60391",
         "http://www.geogebra.org/m/141300",
+		"https://www.geogebra.org/m/wn2U6Hj6#material/ysf5TjUB",
         "https://www.geogebra.org/classic/hs52mgmq",
         "https://www.geogebra.org/graphing/pse7krdf"
     ]
