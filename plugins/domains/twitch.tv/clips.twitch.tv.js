21e92f58120cc928e0737d1db228f4cd7e704318
Ivan Paramonau <i.paramonau@gmail.com>
2020-06-17 11:37:06 -0400

    twitch: fix frame-ancestors for clips too

---
+++ plugins/domains/twitch.tv/clips.twitch.tv.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    re: [
+        /^https?:\/\/clips.twitch\.tv\/([^\?\/]+)(?:\?[^\?]+)?$/i,
+        /^https?:\/\/www\.twitch\.tv\/\w+\/clip\/([^\?\/]+)/i
+    ],
+
+    mixins: ['*'],
+
+    // This is here just as a fallback and tests. There's actually ld.video on the page.
+    getData: function(urlMatch, meta) {
+        if (!(meta.ld && meta.ld.video) && urlMatch[1] !== 'embed') {
+            return {
+                schemaVideoObject: {
+                    embedURL: `https://clips.twitch.tv/embed?clip=${urlMatch[1]}&autoplay=false`
+                }
+            }
+        }
+    },
+
+    tests: [{skipMethods: ['getData']},
+        "https://clips.twitch.tv/UgliestBenevolentAnteaterM4xHeh",
+        "https://www.twitch.tv/tfue/clip/AgileCallousOpossumBudBlast"
+    ]
+};
