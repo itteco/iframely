444f963a4d37547aab88a9f2277f88858cdcb5d2
garmoncheg <garmon1@gmail.com>
2020-07-06 21:59:26 +0300

    domain: scribd.com remove redundancy and 401 Error

--- plugins/domains/scribd.com/scribd.com-error.js
+++ plugins/domains/scribd.com/scribd.com-error.js
@@ -1,23 +1,10 @@
 module.exports = {
 
-    re: [
-        /^https?:\/\/(?:www|\w{2})\.scribd\.com\/(doc|document|embeds|presentation|fullscreen)\/(\d+)/i
-    ],
-
-    mixins: [
-        "oembed-title",
-        "oembed-thumbnail",
-        "oembed-site",
-        "oembed-author",
-        "og-image",
-        "domain-icon",
-        "og-description"
-    ],
+    re: require('./scribd.com').re,
 
     getData: function(oembedError, options, cb) {
         if (oembedError === 401){
             cb({
-                responseStatusCode: 401,
                 message: "Scribd doesn't support embedding of private documents"
             }, null)
         } else {
