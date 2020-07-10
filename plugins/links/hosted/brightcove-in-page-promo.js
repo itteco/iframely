63536a3d6aae292a72db8d0f268336d89ccdb41a
garmoncheg <garmon1@gmail.com>
2020-07-10 16:09:06 +0300

    hosted: brightcove hosted when data-iframe-url provided

--- plugins/links/hosted/brightcove-in-page-promo.js
+++ plugins/links/hosted/brightcove-in-page-promo.js
@@ -5,26 +5,44 @@
 
 module.exports = {
 
-    provides: '__promoUri',
+    provides: ['__promoUri', '__brightcoveEmbed'],
+
+    getLink: function(__brightcoveEmbed) {
+        return {
+            href: __brightcoveEmbed,
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.player, CONFIG.R.html5],
+            "aspect-ratio": 16 / 9
+        }
+    },
 
     getData: function(cheerio, __allowBrightcoveInPage) {
 
-        /* we are looking for following video on the page, and check that embed is allowed
-                    <video
-                        id="video-js-4306274716001"
-                        data-account="1125911414"
-                        data-player="VJ949r8Fg"
-                        data-embed="default"
-                        data-video-id="4306274716001"
-                        class="main-media__video-player video-js"
-                        autoplay>
-                    </video>
-                    <script src="//players.brightcove.net/1125911414/VJ949r8Fg_default/index.min.js"></script>
+        /**
+         * we are looking for following video on the page, and check that embed is allowed
+         *          <video
+         *              id="video-js-4306274716001"
+         *               data-account="1125911414"
+         *               data-player="VJ949r8Fg"
+         *               data-embed="default"
+         *               data-video-id="4306274716001"
+         *               data-iframe-url="[ URL of Embed ]"
+         *               class="main-media__video-player video-js"
+         *               autoplay>
+         *           </video>
+         *           <script src="//players.brightcove.net/1125911414/VJ949r8Fg_default/index.min.js"></script>
         */
 
         var $video = cheerio('video.video-js');
 
         if ($video.length == 1) {
+
+            var dataUrl = $video.attr('data-iframe-url');
+            if (dataUrl) {
+                return {
+                    __brightcoveEmbed: dataUrl
+                }
+            }
 
             var embed = $video.attr('data-embed');
             var account = $video.attr('data-account');
