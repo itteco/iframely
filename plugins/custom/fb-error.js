a54641e59ce8b813bdc9e6923c68f78bf4c5e368
Ivan Paramonau <i.paramonau@gmail.com>
2020-09-16 10:56:53 -0400

    Handle FB APIs errors

--- plugins/custom/fb-error.js
+++ plugins/custom/fb-error.js
@@ -1,29 +1,48 @@
 module.exports = {
 
-    getLink: function(oembedError, cb) {
+    getLink: function(oembedError, url, cb) {
 
         // https://developers.facebook.com/docs/graph-api/using-graph-api/error-handling/
         // Though the errors we actually need are not in the doc...
         const KNOWN_CODES = {
-            24: 404,
-            100: 415
+            2: 408,    // Downtime
+            24: 404,   // Seems to be 404 for Instagrams
+
+            100: 404,  // https://developers.facebook.com/docs/graph-api/reference/oembed-video/
+            901: 417,  // ./oembed-post/, ./oembed-page/
+
+            104: 415,            
+            368: 408   // Temporary blocked
         }
 
-        const error = oembedError.body 
-                    && oembedError.body.error && oembedError.body.error.code 
-                    && KNOWN_CODES[oembedError.body.error.code] /* is defined */
-                    ? KNOWN_CODES[oembedError.body.error.code]
-                    : oembedError;
+        let result = {}; // When left empty - it allows a  fallback to generic parsers and get the accurate http code.
+        const fbError = oembedError.body && oembedError.body.error || {};
 
-        let result = {
-            responseError: error
-        };
+        const error = fbError.code 
+                    && KNOWN_CODES[fbError.code] /* is defined */
+                    ? KNOWN_CODES[fbError.code]
+                    : null;
+
+        if (!error && 
+                (!oembedError.body // Connection issue like ERR_HTTP2_STREAM_ERROR
+                    || fbError.is_transient) // Unexpected transient error
+                ) {            
+            error = 408;
+        }
+
+        if (error) {
+            result.responseError = error;
+        }
 
         if (error === 415) {
             result.message = CONFIG.FB_ERROR_MESSAGE || 
                 "HEADS UP: Facebook & Instagram now require your own access_token configured. " 
                 + " See https://github.com/itteco/iframely/issues/284";
+        } else if (fbError.message) {
+            result.message = fbError.message;
         }
+
+        console.log('Facebook oembed api - error getting oembed for', url, JSON.stringify(fbError), JSON.stringify(result));
 
         return cb(result);
     },
