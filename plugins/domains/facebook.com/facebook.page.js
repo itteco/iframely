2afd7555e5cd1e6afb07533870c85d1e55ff673c
Ivan Paramonau <i.paramonau@gmail.com>
2020-10-02 14:14:53 -0400

    facebook: return options form for fb pages

--- plugins/domains/facebook.com/facebook.page.js
+++ plugins/domains/facebook.com/facebook.page.js
@@ -7,7 +7,11 @@ module.exports = {
 
     mixins: [
         "domain-icon",
-        "*"
+        "og-image",
+        "canonical",
+        "og-description",
+        "oembed-site",
+        "og-title"
         // "fb-error" // Otherwise the HTTP redirect won't work for URLs like http://www.facebook.com/133065016766815_4376785445728063
     ],
 
@@ -17,7 +21,7 @@ module.exports = {
          * however, HTML for user profiles did not work.
          * Some checks below are legacy ones to skip user profiles. Perhaps, no longer needed as of Oct 24, 2020.
          */
-        if ((meta.ld && meta.ld.organization && /blockquote/.test(oembed.html)) 
+        if ((meta.ld && !meta.ld.person && /blockquote/.test(oembed.html)) 
             || (meta.al && meta.al.android && meta.al.android.url && !/\/profile\//.test(meta.al.android.url) && /blockquote/.test(oembed.html))
             || (meta['html-title'] && /security check required/i.test(meta['html-title']) && /blockquote/.test(oembed.html)) ) {
 
@@ -34,7 +38,6 @@ module.exports = {
             html = options.getRequestOptions('facebook.small_header', false)
                 ? html.replace(/data\-small\-header=\"(?:false|0)?\"/i, 'data-small-header="true"')
                 : html.replace(/data\-small\-header=\"(true|1)\"/i, 'data-small-header="false"');
-
 
             return {
                 type: CONFIG.T.text_html,
@@ -71,6 +74,7 @@ module.exports = {
         "https://www.facebook.com/facebook",
         "https://www.facebook.com/hlaskyjanalasaka?fref=nf",
         "https://www.facebook.com/pg/RhulFencing/about/",
+        "https://www.facebook.com/caboreytours/",
         {
             noFeeds: true,
             skipMethods: ['getData']
