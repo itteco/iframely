aed42feba1114548b7f3796775422fb09e8017bd
Ivan Paramonau <i.paramonau@gmail.com>
2020-10-20 08:44:13 -0400

    pdfs: warm up cache preview for google PDF viewer

--- plugins/links/google-docs-viewer.js
+++ plugins/links/google-docs-viewer.js
@@ -13,7 +13,7 @@ module.exports = {
 
                 return {
                     href: "https://docs.google.com/viewer?embedded=true&url=" + encodeURIComponent(url),
-                    type: CONFIG.T.text_html,
+                    accept: CONFIG.T.text_html,
                     rel: [CONFIG.R.reader, CONFIG.R.file, CONFIG.R.ssl, CONFIG.R.html5],
                     "aspect-ratio": 1 / Math.sqrt(2) // standard sqrt(2) aspect for A4 and other standard A* format papers (except North America, of course)
                 }
