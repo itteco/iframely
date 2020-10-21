b35f9426de16707f450889767f5f1c6474ae7212
Martin Sommer <martin.sommer@staffbase.com>
2020-10-21 17:33:37 +0200

    :heavy_minus_sign: Remove forever.js from the Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,27 +1,3 @@
-#!/bin/ash
+#!/bin/sh
 
-export ARGV="$@"
-export ARGC="$#"
-
-function sigterm_handler() {
-    echo "SIGTERM signal received."
-    yarn forever stopall
-}
-
-trap "sigterm_handler; exit" TERM
-
-function entrypoint() {
-    if [ "$ARGC" -eq 0 ]
-    then
-        # Run server in cluster mode by default
-        yarn forever start cluster.js
-    else
-        # Use command line arguments supplied at runtime
-        yarn forever start $ARGV
-    fi
-
-    yarn forever --fifo logs 0 &
-    wait
-}
-
-entrypoint
+exec node cluster.js
