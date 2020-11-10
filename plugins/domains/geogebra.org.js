c15c552863ed78f8147adb84a5988038d85fe5e6
garmoncheg <garmon1@gmail.com>
2020-11-10 15:18:16 +0200

    domain: geogebra.org formatting

--- plugins/domains/geogebra.org.js
+++ plugins/domains/geogebra.org.js
@@ -11,18 +11,21 @@ module.exports = {
         "*"
     ],
 
-    // https://wiki.geogebra.org/en/Embedding_in_Webpages
     getData: function(urlMatch, options, cb) {
-       if (!options.redirectsHistory|| options.redirectsHistory.indexOf('https://www.geogebra.org/m/' + urlMatch[1]) === -1) {
-           cb (null, {__promoUri: {url: 'https://www.geogebra.org/material/iframe/id/' + urlMatch[1]} });
-       } else {
-           cb(null, null)
-       }
+        /**https://wiki.geogebra.org/en/Embedding_in_Webpages */
+
+        if (!options.redirectsHistory
+            || options.redirectsHistory.indexOf('https://www.geogebra.org/m/' + urlMatch[1]) === -1) {
+
+            cb (null, {__promoUri: {url: 'https://www.geogebra.org/material/iframe/id/' + urlMatch[1]} });
+        } else {
+            cb(null, null)
+        }
     },
 
-    tests: [{
-        noFeeds: true
-    }, {skipMethods: ['getData']},
+    tests: [
+        {noFeeds: true},
+        {skipMethods: ['getData']},
         "http://tube.geogebra.org/m/60391",
         "http://www.geogebra.org/m/141300",
         "https://www.geogebra.org/m/wn2U6Hj6#material/ysf5TjUB",
