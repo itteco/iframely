4f1e2ede616121d89793479652da7fe91254f85c
garmoncheg <garmon1@gmail.com>
2021-01-11 21:30:38 +0200

    domain: facebook.ld simplify

--- plugins/domains/facebook.com/facebook.ld.js
+++ plugins/domains/facebook.com/facebook.ld.js
@@ -2,17 +2,13 @@ module.exports = {
 
     re: require('./facebook.video').re,
 
-    getMeta: function(__allowFBThumbnail, meta) {
+    getMeta: function(__allowFBThumbnail, schemaVideoObject) {
 
-        if (meta.ld && meta.ld.videoobject) {
-
-            return {
-                duration: meta.ld.videoobject.duration.replace(/^T/, 'PT'),
-                date: meta.ld.videoobject.datepublished,
-                views: meta.ld.videoobject.interactioncount
-            }
+        return {
+            duration: schemaVideoObject.duration.replace(/^T/, 'PT'),
+            date: schemaVideoObject.datepublished,
+            views: schemaVideoObject.interactioncount
         }
-
     },
 
     getLink: function(__allowFBThumbnail, schemaVideoObject) {
@@ -31,13 +27,7 @@ module.exports = {
     },
 
     tests: [
-        "http://www.facebook.com/photo.php?v=4253262701205&set=vb.1574932468&type=2&theater",
         "https://www.facebook.com/video.php?v=4392385966850",
-        "https://business.facebook.com/KMPHFOX26/videos/10154356403004012/",
-        "https://www.facebook.com/tv2nyhederne/videos/1657445024271131/?comment_id=1657463030935997",
-        "https://www.facebook.com/sugarandsoulco/videos/1484037581637646/?pnref=story",
-        "https://www.facebook.com/watch/?v=235613163792499",
-        "https://www.facebook.com/watch/?ref=external&v=373114473595228",
         {noFeeds: true}, {skipMixins: ["fb-error"]}
     ]
 };
