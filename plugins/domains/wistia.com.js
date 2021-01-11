82b30d8338384f1dc89fd470aa128d8e2fa24b10
Ivan Paramonau <i.paramonau@gmail.com>
2021-01-11 07:42:30 -0500

    domains: review wistia & return audio support

--- plugins/domains/wistia.com.js
+++ plugins/domains/wistia.com.js
@@ -6,10 +6,22 @@ module.exports = {
         "oembed-site",
         "oembed-title",
         "oembed-thumbnail",
-        "oembed-duration"
+        "oembed-duration",
     ],
 
+    getMeta: function(oembed) {
+        return {
+            'theme-color': oembed.player_color
+        }
+    },
+
     getLinks: function(oembed, urlMatch, url) {
+
+        var icon = {
+            href: "https://wistia.com/static/favicon.ico",
+            type: CONFIG.T.image,
+            rel: CONFIG.R.icon     
+        }
 
         if (oembed.type === 'video') { 
 
@@ -21,23 +33,39 @@ module.exports = {
             }
 
             return [{
-                href: "//fast.wistia.net/embed/iframe/" + urlMatch[2] + params,
+                href: "https://fast.wistia.net/embed/iframe/" + urlMatch[2] + params,
                 type: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.html5],
                 "aspect-ratio": oembed.width / oembed.height,
                 autoplay: "autoPlay=true"
-            }, {
-                href: "http://wistia.com/favicon.ico",
+            }, icon];
+
+        } else if (oembed.type === 'audio') { // Not in spec, but let it be
+            return [{
+                html: oembed.html,
+                type: CONFIG.T.text_html,
+                rel: [CONFIG.R.player, CONFIG.R.audio, CONFIG.R.html5, CONFIG.R.inline],
+                height: 218
+            }, icon];
+
+        } else if (oembed.type === 'photo') { 
+            // Photo also handled well via fallback to generic. 
+            // But it will be faster via domain plugin.
+            return [{
+                href: oembed.url,
                 type: CONFIG.T.image,
-                rel: CONFIG.R.icon
-            }];
-        } // else - 'rich' is for audio embeds. Let it fall back to generic parsers
+                rel: [CONFIG.R.image, CONFIG.R.thumbnail],
+            }, icon];
+        }
+        // else - 'rich' is currently not used any longer (was used for 'audio' before).
     },
 
 
     tests: [{
         noFeeds: true
     },
-        "http://appsumo.wistia.com/medias/fudkgyoejs"
+        "https://appsumo.wistia.com/medias/fudkgyoejs",
+        "https://shluchim.wistia.com/medias/l0mkqobjda",
+        "https://signalophthalmic.wistia.com/medias/hfqfnxk3yb"
     ]
 };
