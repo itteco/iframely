433de14d3f26607b311230c4bcd218c7a98c7d02
Nazar Leush <n.leush@gmail.com>
2021-02-01 15:24:51 +0200

    fix debugger qa notification

--- modules/tests-ui/utils.js
+++ modules/tests-ui/utils.js
@@ -41,19 +41,19 @@ exports.sendQANotification = function(logEntry, data) {
 
     if (CONFIG.SLACK_WEBHOOK_FOR_QA && CONFIG.SLACK_CHANNEL_FOR_QA) {
 
+        var baseAppUrl = CONFIG.baseAppUrl;
+        if (/^\/\/:/.test(baseAppUrl)) {
+            baseAppUrl = 'https:' + baseAppUrl;
+        }
+
         var previewMessage = "[" + logEntry.plugin + "] " + data.message;
-        var message = "<" + CONFIG.baseAppUrl + "/tests#" + encodeURIComponent(logEntry.plugin) + "|[" + logEntry.plugin + "]> " + data.message;
+        var message = "<" + baseAppUrl + "/tests#" + encodeURIComponent(logEntry.plugin) + "|[" + logEntry.plugin + "]> " + data.message;
 
         var errors = logEntry.errors_list.map(function(info) {
             return info.replace(logEntry.plugin + ' - ', '');
         }).join(" - ");
         if (errors) {
             message += " - " + errors;
-        }
-
-        var debuggerBaseUrl = CONFIG.QA_BASE_URL || CONFIG.baseAppUrl;
-        if (/^\/\/:/.test(debuggerBaseUrl)) {
-            debuggerBaseUrl = 'https:' + debuggerBaseUrl;
         }
 
         request({
@@ -81,7 +81,7 @@ exports.sendQANotification = function(logEntry, data) {
                             "text": {
                                 "type": "mrkdwn",
                                 "verbatim": true,
-                                "text": "`<" + debuggerBaseUrl + "/debug?uri=" + encodeURIComponent(logEntry.url) + "|debug>` " + logEntry.url.replace(/^https?:\/\//, '')    // Debug link.
+                                "text": "`<" + (CONFIG.QA_BASE_URL || baseAppUrl) + "/debug?uri=" + encodeURIComponent(logEntry.url) + "|debug>` " + logEntry.url.replace(/^https?:\/\//, '')    // Debug link.
                             }
                         }],
                         "color": COLORS[data.color]
