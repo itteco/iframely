1426b5aecafe1c65fd9d14cd91fbea7dc39259be
Ivan Paramonau <i.paramonau@gmail.com>
2021-02-10 09:45:45 -0500

    domains: review dribbble and bring up-to-date

--- plugins/domains/dribbble.com.js
+++ plugins/domains/dribbble.com.js
@@ -1,58 +1,45 @@
+const PROFILE_RE = /^https?:\/\/dribbble\.com\/([a-zA-Z0-9\-]+)(?:\?[^\/]+)?$/i;
+
 module.exports = {
 
     re: [
         /^https?:\/\/dribbble\.com\/shots\/([a-zA-Z0-9\-]+)/i,
-        /^https?:\/\/dribbble\.com\/([a-zA-Z0-9\-]+)(?:\?[^\/]+)?$/i
+        PROFILE_RE
     ],
 
-    mixins: [
-        "*"
-    ],
+    mixins: ["*"],
+
+    highestPriority: true,
 
     getMeta: function(url, og) {
 
-        // avoid canonical=gif for gif shots that invalidate thumbmails
+        var meta = {};
+        // Avoid canonical=gif for gif shots that invalidate thumbmails.
         if (/\.gif/.test(og.url)) {
-            return {
-                canonical: url
-            }
+            meta.canonical = url;
         }
+
+        if (PROFILE_RE.test(url)) {
+            // Wrap players into a promo card
+            meta.media = 'reader';
+        }
+
+        return meta;
     },
 
-    getLink: function(meta, url, urlMatch, cb) {
+    getLink: function(meta, url, urlMatch) {
 
-        if (meta.og && meta.og.image) {
-
-            var links = [{
+        if (meta.og && meta.og.image && !meta.og.video) {
+            return {
                 href: meta.og.image.url || meta.og.image,
                 type: CONFIG.T.image,
-                rel: meta.og.type === 'profile'? [CONFIG.R.image, CONFIG.R.promo] : CONFIG.R.image,
-                width: meta.twitter.image.width,
-                height: meta.twitter.image.height
-            }];
-
-            if (meta.twitter && meta.twitter.player && /^https?:\/\/dribbble\.com\/shots\/([a-zA-Z0-9\-]+)/i.test(url)) {
-                links.push({
-                    href: meta.twitter.player.value,
-                    media: {
-                        width: meta.twitter.player.width,
-                        height: meta.twitter.player.height,
-                        'max-width': 800
-                    }, 
-                    type: CONFIG.T.text_html,
-                    rel: [CONFIG.R.player, CONFIG.R.gifv, CONFIG.R.html5, CONFIG.R.twitter, 'responsive']
-                });
-            }
-
-            cb(null, links);
-
-        } else {
-            // Attachments pages doesn't have any meta at the moment :\
-            cb ({
-                redirect: urlMatch[0] + urlMatch[1]
-            });
-
+                rel: !PROFILE_RE.test(url) && !meta.og.video ? [CONFIG.R.image, CONFIG.R.promo] : CONFIG.R.image,
+                width: meta.og.image.width,
+                height: meta.og.image.height
+            };
         }
+
+        // The rest of links are well covered now by whitelist parsers + media=reader for profiles.
     },
 
     tests: [ {
@@ -60,10 +47,12 @@ module.exports = {
         selector: ".dribbble-link"
     }, {
         skipMethods: [
-            "getMeta"
+            "getMeta", "getLink"
         ]
     },
-        "http://dribbble.com/shots/1311850-Winter-Is-Coming",
-        "http://dribbble.com/shots/5030547-Chairs-Store-App"
+        "https://dribbble.com/shots/1311850-Winter-Is-Coming",
+        "https://dribbble.com/shots/5030547-Chairs-Store-App",
+        "https://dribbble.com/Sochnik",
+        "https://dribbble.com/shots/5715634-Website-Banner-Slides"
     ]
 };
