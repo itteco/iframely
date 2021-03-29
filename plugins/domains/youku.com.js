4dae454848969482d36868decfdfae323d4c8aa4
Ivan Paramonau <i.paramonau@gmail.com>
2021-03-29 09:28:01 -0400

    domains: avoid timeouts on player validation for Youku

--- plugins/domains/youku.com.js
+++ plugins/domains/youku.com.js
@@ -9,10 +9,9 @@ module.exports = {
     mixins: ["*"],
 
     getLink: function (urlMatch) {
-
         return {
-            href: "//player.youku.com/embed/"+ urlMatch[1],
-            accept: CONFIG.T.text_html,
+            href: "https://player.youku.com/embed/"+ urlMatch[1],
+            type: CONFIG.T.text_html,
             rel: [CONFIG.R.player, CONFIG.R.html5],
             "aspect-ratio": 16/10, //As stated it in docs
             autoplay: "autoplay=true"
@@ -24,6 +23,9 @@ module.exports = {
         selector: ".common_container .pack_pack_cover a"
     },
         "http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
-        "http://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction"
+        "http://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
+        "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html",
+        "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html"
+
     ]
 };
