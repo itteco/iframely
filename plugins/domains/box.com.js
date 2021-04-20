b4beb4b9e470f0cb6cdad28673d66300bc0259e3
Ivan Paramonau <i.paramonau@gmail.com>
2021-04-20 14:13:56 -0400

    domains: extract title for box.com

--- plugins/domains/box.com.js
+++ plugins/domains/box.com.js
@@ -7,19 +7,35 @@ module.exports = {
         /^https:\/\/(\w+)\.app\.box\.com\/embed\/preview\/([a-zA-Z0-9]+)/,
     ],
 
-    mixins: ["favicon", "html-title"],
+    mixins: ["favicon", "html-title", "initial-state"],
+    provides: '__initialStateNeeded',
+
+    getMeta: function(initialState) {
+        var itemKey = Object.keys(initialState).find(key => initialState[key].items instanceof Array);
+        if (itemKey) {
+            var item = initialState[itemKey].items[0];
+            return {
+                title: item.name
+            }
+        }
+
+    },
 
     getLink: function(urlMatch) {
-
-        // docs are at https://developers.box.com/box-embed/
-
+        // Docs are at https://developers.box.com/box-embed/
         return {
             href: urlMatch.length > 2 ? "https://" + urlMatch[1] + ".app.box.com/embed_widget/s/" + urlMatch[2] : "https://app.box.com/embed_widget/s/" + urlMatch[1],
             rel: [CONFIG.R.reader, CONFIG.R.oembed, CONFIG.R.html5, CONFIG.R.ssl],
             type: CONFIG.T.text_html,
             "aspect-ratio": 500 / 400
         }
+    },
 
+    getData: function(options) {
+        options.__VAR_NAME__ = 'Box.postStreamData';
+        return {
+            __initialStateNeeded: true
+        }
     },
 
     tests: [{
