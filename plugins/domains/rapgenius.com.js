901b2854d3d6184d7ca6cabd3d92f659daf9eccc
Ivan Paramonau <i.paramonau@gmail.com>
2021-05-12 10:35:29 -0400

    tests: fix newer genius.com songs

--- plugins/domains/rapgenius.com.js
+++ plugins/domains/rapgenius.com.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    re: /^https?:\/\/(?:[\w\-]+\.)?genius\.com\/(?!jobs)([a-zA-Z0-9\-]+)/i,    
+    re: /^https?:\/\/(?:[\w\-]+\.)?genius\.com\/(?!jobs)([a-zA-Z0-9\-]+)/i,
 
     mixins: [ 
         "domain-icon",
@@ -9,9 +9,15 @@ module.exports = {
 
     getLinks: function(urlMatch, meta, options) {
 
-        if (/\d+/.test(meta['newrelic-resource-path'])) {
+        var id;
 
-            var id = meta['newrelic-resource-path'].match(/\d+/)[0];
+        if (meta.twitter && meta.twitter.app && meta.twitter.app.url && /songs\/(\d+)$/i.test(meta.twitter.app.url.iphone)) {
+            id = meta.twitter.app.url.iphone.match(/songs\/(\d+)$/i)[1];
+        } else if (/\d+/.test(meta['newrelic-resource-path'])) {
+            id = meta['newrelic-resource-path'].match(/\d+/)[0]
+        }
+
+        if (id) {
             var theme = options.getRequestOptions('players.theme', 'light');
 
             return {
