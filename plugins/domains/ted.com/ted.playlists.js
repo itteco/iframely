778847192195ce4eda2f41e88e3bcea9ed6e4164
Ivan Paramonau <i.paramonau@gmail.com>
2021-08-31 10:25:11 -0400

    tests: fix ted playlists for now

--- plugins/domains/ted.com/ted.playlists.js
+++ plugins/domains/ted.com/ted.playlists.js
@@ -15,11 +15,11 @@ module.exports = {
     getLink: function(urlMatch, og) {
         // oEmbed doesn't support playlists yet. Thus, the workaround. 
         
-        if (og.type === "video.other" && og.url) {
+        if (/\/playlists\//.test(og.url)) {
 
             return {
                 href: og.url.replace(/^https?:\/\/(www\.)?/i, "https://embed-ssl.").replace(/\?[^\/]+$/, ''),
-                type: CONFIG.T.text_html,
+                accept: CONFIG.T.text_html,
                 rel: [CONFIG.R.player, CONFIG.R.html5],
                 "aspect-ratio": 4/3
             }
