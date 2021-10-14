33aac7ac7bfeb5bbcc6c9fcb369f235f312665cb
Ivan Paramonau <i.paramonau@gmail.com>
2021-10-14 10:45:38 -0400

    CONFIG: add R.encrypted

--- config.js
+++ config.js
@@ -162,7 +162,8 @@
             audio: 'audio',
             slideshow: 'slideshow',
             playlist: 'playlist',
-            '3d': '3d'
+            '3d': '3d',
+            encrypted: 'encrypted-media'
         },
 
         FEATURES: [ // feature policy: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy#Directives
