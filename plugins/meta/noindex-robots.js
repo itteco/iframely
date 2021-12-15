691b13d0cfd461513c5df630e5f0fd82733bce52
Ivan Paramonau <i.paramonau@gmail.com>
2021-12-15 10:50:31 -0500

    meta: allow to skip no-index robots directive from domain plugins

--- plugins/meta/noindex-robots.js
+++ plugins/meta/noindex-robots.js
@@ -2,7 +2,7 @@ import * as oembedUtils from '../../lib/plugins/system/oembed/oembedUtils.js';
 
 export default {
 
-    getData: function(url, meta, __noOembedLinks, cb) {
+    getData: function(url, meta, __noOembedLinks, options, cb) {
 
         var oembedLinks = oembedUtils.findOembedLinks(null, meta);
 
@@ -13,6 +13,7 @@ export default {
             && !meta.og
             && !meta.twitter
             && !oembedLinks // null if length == 0.
+            && !options.allowNoIndex
             ? {
                responseStatusCode: 403,
                message: "The robots directive of this page prevents Iframely from parsing it"
