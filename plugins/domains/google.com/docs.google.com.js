54785546548dac18ffcc7599de606ed6b340e290
Ivan Paramonau <i.paramonau@gmail.com>
2022-01-24 12:05:44 -0500

    domains: google drive videos and images are n/a

--- plugins/domains/google.com/docs.google.com.js
+++ plugins/domains/google.com/docs.google.com.js
@@ -87,33 +87,12 @@ export default {
             }
 
             return file;
-        } else if (/usp=embed/.test(schemaFileObject.url) && /icon_\d+_(audio|image)_favicon\.ico$/.test(schemaFileObject.faviconUrl)) {
 
-            var file = {
-                rel: [CONFIG.R.file, CONFIG.R.html5],
-                href: schemaFileObject.url.replace(/\/view(?:\?.+)?$/i, '/preview'),
-                accept: CONFIG.T.text_html
-            };
-
-            var type = schemaFileObject.faviconUrl.match(/icon_\d+_(\w+)_favicon\.ico$/)[1]
-            
-            if (type === 'audio') {
-                file.rel.push(CONFIG.R.player);
-                file.rel.push(CONFIG.R.audio);
-                file.height = 60;
-            } else if (type === 'image') {
-                file.rel.push(CONFIG.R.image);
-                file['aspect-ratio'] = 640/480; // unfortunatelly, no way of known image size
-            }
-
-            return file;
-
-        } else {
+        } else if (!/^https?:\/\/drive/.test(schemaFileObject.url)){
             return {
-                message: 'Google has no preview available for this file.'
+                message: 'Google has no preview available for this document.'
             }
         }
-
     },
 
     getData: function(meta, url, urlMatch, cheerio, decode, options, cb) {
@@ -186,10 +165,7 @@ export default {
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vRKtFs55r6ow0rVvoGJLlDyqxD_455wR6_eZ42z8izYGT_UM6hNW0ruFhn26m_SzsoT4AQxZZA968Lp/pubhtml?gid=1443541234&single=true&widget=true&headers=false",
         "https://docs.google.com/spreadsheets/u/1/d/1_tsspyfiH8ZVAOmoCbGJ3gvzGU5zLUb-PEG0-RyjP5E/edit#gid=1926296709",
         "https://docs.google.com/document/d/e/2PACX-1vSeGAfeYcpPAGLX4h0krdMR8HBuCxf3M0H0MlyeQ9GYQzJsJ2KTfZ_iSopp5dUwX3JVOfCpAoEyoXdh/pub",
-        "https://drive.google.com/file/d/1H4K6WkWjycH253U35VVUE8oaTJea_dmp/view",
         "https://drive.google.com/open?id=1rKcaLuY0WzhPqQFCGy_4aIJblnEziy7-",
-        "https://drive.google.com/file/d/17cEEjFg-xJGnKfwQHDPF9mBNsBY8KTsc/preview",
-        "https://drive.google.com/file/d/15rfX_NentPMEgJQ_io6qhctScmpahU00/view",
         "https://docs.google.com/presentation/d/e/2PACX-1vQmrymNWFltfprLl9IX-irRcmvjsL1ahOKAt8YTzuWTdcWyIH2EX6wyUmmJ4ftG3dICaTZ9DCpqXiht/pub?start=false&loop=false&delayms=3000",
         {
             skipMixins: [
