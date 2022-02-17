a4b8454b228fb53e46e6b518b4213610dea0152f
Rowan Hogan <rowan.hogan@gmail.com>
2022-02-17 16:33:07 +1000

    Allow CORS

---
+++ config.production.js
@@ -0,0 +1,11 @@
+export default {
+  CACHE_ENGINE: 'no-cache',
+  allowedOrigins: ["*"],
+  baseAppUrl: "https://iframely.blackbaud.services",
+  providerOptions: {
+    locale: "en_US",
+    twitch: {
+      parent: 'blackbaud-sites.com,blackbaud.services'
+    }
+  }
+}
