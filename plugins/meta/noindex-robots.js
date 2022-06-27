6fb29e13891d1a3571ed3214654909e873f80129
Ivan Paramonau <i.paramonau@gmail.com>
2022-06-27 14:18:05 -0400

    meta: do not run unnecessary cycles in robots check

--- plugins/meta/noindex-robots.js
+++ plugins/meta/noindex-robots.js
@@ -4,17 +4,14 @@ export default {
 
     getData: function(url, meta, __noOembedLinks, options, cb) {
 
-        var oembedLinks = oembedUtils.findOembedLinks(null, meta);
-        var iframelyTargeted = Object.keys(meta).some(key => key.indexOf('iframely') === 0);
-
         return cb(
             meta.robots
             && /noindex/i.test(meta.robots)
             && !meta.description
             && !meta.og
             && !meta.twitter
-            && !oembedLinks // null if length == 0.
-            && !iframelyTargeted
+            && /* !oembedLinks */ !oembedUtils.findOembedLinks(null, meta) // null if length == 0.
+            && /* !iframelyTargeted */ !Object.keys(meta).some(key => key.indexOf('iframely') === 0)
             && !options.allowNoIndex
             ? {
                responseStatusCode: 403,
