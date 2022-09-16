f6912492c4d1d3e610b4a28531261b2afabd9a40
Ivan Paramonau <i.paramonau@gmail.com>
2022-09-15 21:42:52 -0400

    ld: arrays can be in `@type` too
    ex. VRBO
    `"@type": ["Product", "Accommodation"]`

--- plugins/links/embedURL/embedURL.js
+++ plugins/links/embedURL/embedURL.js
@@ -5,14 +5,14 @@ export default {
 
     provides: 'schemaVideoObject',
 
-    getData: function(cheerio, decode, __allowEmbedURL) {
+    getData: function(url, cheerio, decode, __allowEmbedURL) {
 
         /* Let's try to find ld+json in the body first. */
         var $script = cheerio('script[type="application/ld+json"]:contains("VideoObject")').first(); // embedURL can be embedurl, embedUrl, etc.
 
         if ($script.length === 1) {
             try {
-                var ld = utils.parseLDSource($script.html(), decode);
+                var ld = utils.parseLDSource($script.html(), decode, url);
 
                 if (ld && __allowEmbedURL !== 'skip_ld') {
                     return {
