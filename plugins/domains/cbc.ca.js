1446c12b8e0ef9a3307cef11cf703fc3ebe61f53
Ivan Paramonau <i.paramonau@gmail.com>
2022-10-13 14:47:17 -0400

    domains: check CBC player for x-frame-options

--- plugins/domains/cbc.ca.js
+++ plugins/domains/cbc.ca.js
@@ -16,7 +16,7 @@ export default {
 
         var player = {
                 href: twitter.player.value,
-                type: CONFIG.T.text_html,
+                accept: CONFIG.T.text_html,
                 rel: [CONFIG.R.player],
                 'max-width': 1064
             };
