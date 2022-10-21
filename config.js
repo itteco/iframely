80cac6747e08c6418931c4727e3956e9417bc3a9
Nazar Leush <n.leush@gmail.com>
2022-10-21 21:12:41 +0300

    Review config (#463)
    * remove obsolete config
    
    * read local config from yml file
    
    * remove `underscore` from config
    
    Co-authored-by: Ivan Paramonau <i.paramonau@gmail.com>

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-    import * as _ from 'underscore';
     import * as path from 'path';
     import * as fs from 'fs';
+    import * as yaml_config from 'node-yaml-config';
 
     import { fileURLToPath } from 'url';
     import { dirname } from 'path';
@@ -20,7 +20,6 @@
         use_http2: true,
         DEBUG: false,
 
-        SPDY_AGENT_DEFAULT_PORT: 443,
         WHITELIST_URL: 'https://iframely.com/qa/whitelist.json',
         WHITELIST_URL_RELOAD_PERIOD: 60 * 60 * 1000,  // will reload WL every hour, if no local files are found in /whitelist folder
 
@@ -30,7 +29,7 @@
         CACHE_ENGINE: 'node-cache',
         CACHE_TTL: 24 * 60 * 60,
         API_REQUEST_CACHE_TTL: 30 * 24 * 60 * 60,
-        IMAGE_META_CACHE_TTL: 7 *24 * 60 * 60,
+        IMAGE_META_CACHE_TTL: 7 * 24 * 60 * 60,
 
         CACHE_TTL_PAGE_TIMEOUT: 10 * 60,
         CACHE_TTL_PAGE_404: 10 * 60,
@@ -339,14 +338,6 @@
             ]
         },
 
-        REL_OPTIONS: {
-            all: ["ssl"],
-            player: ["responsive", "autoplay"],
-            video: ["responsive", "autoplay"],
-            link: ["reader"],
-            rich: ["reader"]
-        },
-
         // whitelist rel to iframely rel.
         REL_MAP: {
             "article": "reader",
@@ -378,7 +369,7 @@
     if (fs.existsSync(local_config_path)) {
         var local = await import(local_config_path);
         local = local && local.default;
-        _.extend(config, local);
+        Object.assign(config, local);
     }
 
 
@@ -389,19 +380,39 @@
 
     local_config_path = path.resolve(__dirname, "config.local.js");
 
+    var local;
+
     // Try config by NODE_ENV.
     if (fs.existsSync(env_config_path)) {
-        var local = await import(env_config_path);
+        local = await import(env_config_path);
         local = local && local.default;
 
     } else if (fs.existsSync(local_config_path)) {
         // Else - try local config.
-        var local = await import(local_config_path);
+        local = await import(local_config_path);
         local = local && local.default;
     }
 
-    _.extend(config, local);
+    Object.assign(config, local);
 
+    env_config_path = path.resolve(
+        __dirname,
+        "config." + (process.env.NODE_ENV || "local") + ".yml"
+    );
+
+    local_config_path = path.resolve(__dirname, "config.local.yml");
+
+    // Try config by NODE_ENV.
+    if (fs.existsSync(env_config_path)) {
+        local = yaml_config.load(env_config_path);
+    } else if (fs.existsSync(local_config_path)) {
+        // Else - try local config.
+        local = yaml_config.load(local_config_path);
+    } else {
+        local = null;
+    }
+
+    Object.assign(config, local);
 
     if (!config.baseStaticUrl) {
         config.baseStaticUrl = config.baseAppUrl + config.relativeStaticUrl;
