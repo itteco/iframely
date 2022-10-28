df37648dfe22bf52e07a142210ab356315e93ed8
Ivan Paramonau <i.paramonau@gmail.com>
2022-10-27 21:43:37 -0400

    general: for `query` mixin, set "true" and "false" as boolean values

--- plugins/custom/query.js
+++ plugins/custom/query.js
@@ -7,8 +7,17 @@ export default {
     getData: function(url) {
 
         if (/\?/i.test(url)) {
+            var query = URL.parse(url, true).query;
+            // validate and apply boolean 
+            for (const key in query) {
+                if (query[key] === "true") {
+                    query[key] = true;
+                } else if (query[key] === "false") {
+                    query[key] = false;
+                }
+            }
             return {
-                query: URL.parse(url, true).query
+                query: query
             };
         } else {
             return {query: {}}
