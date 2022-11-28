abe5d72b376334763be8ff6bb822bedfef2ae2d0
Ivan Paramonau <i.paramonau@gmail.com>
2022-11-28 16:11:04 -0500

    tests: fix libsyn-hosted

--- plugins/links/hosted/libsyn-hosted.js
+++ plugins/links/hosted/libsyn-hosted.js
@@ -2,14 +2,15 @@ export default {
 
     getData: function(url, video_src, twitter) {
 
-        var stream = twitter.player && twitter.player.stream && twitter.player.stream.value;
+        const stream = twitter.player && twitter.player.stream && twitter.player.stream.value;
+        const video_src_re = /^https?:\/\/(?:html5\-player|play)\.libsyn\.com\/embed\/episode\/id\/(\d+)/i
 
         if (stream
             && /traffic\.libsyn.com\/(?:preview|secure)\/([a-zA-Z0-9\-]+)\//i.test(stream)
-            && /^https?:\/\/html5\-player\.libsyn\.com\/embed\/episode\/id\/(\d+)/i.test(video_src)
+            && video_src_re.test(video_src)
             && /^https?:\/\/[a-z0-9\.\-]+\//i.test(url)) {
 
-            var id = video_src.match(/^https?:\/\/html5\-player\.libsyn\.com\/embed\/episode\/id\/(\d+)/i)[1];
+            const id = video_src.match(video_src_re)[1];
 
             return {
                 __promoUri: {                    
