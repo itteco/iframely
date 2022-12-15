ad12d0f46578a9ea5b97650526c00553bff92b84
Ivan Paramonau <i.paramonau@gmail.com>
2022-12-15 14:31:59 -0500

    domains: detect Twitch soft 404s

--- plugins/domains/twitch.tv/clips.twitch.tv.js
+++ plugins/domains/twitch.tv/clips.twitch.tv.js
@@ -7,19 +7,33 @@ export default {
 
     mixins: ['*'],
 
-    // This is here just as a fallback and tests. There's actually ld.video on the page.
-    getData: function(urlMatch, meta) {
-        if (!(meta.ld && meta.ld.video) && urlMatch[1] !== 'embed') {
-            return {
+    // This is here just as a fallback and tests and 404s.
+    getData: function(urlMatch, meta, cb) {
+
+        if (meta.ld && meta.ld.videoobject 
+            && meta.twitter && meta.twitter.title === 'Twitch'
+            && /^https?:\/\/clips\.twitch\.tv\/embed\?clip=$/i.test(meta.ld.videoobject.embedurl)) {
+
+            return cb({
+                responseStatusCode: 404
+            });
+
+        } else if ((!meta.ld || !meta.ld.videoobject) && urlMatch[1] !== 'embed') {
+
+            return cb(null, {
                 schemaVideoObject: {
                     embedURL: `https://clips.twitch.tv/embed?clip=${urlMatch[1]}&autoplay=false`
                 }
-            }
+            });
+
+        } else {
+            return cb();
         }
     },
 
     tests: [{skipMethods: ['getData']},
         "https://clips.twitch.tv/UgliestBenevolentAnteaterM4xHeh",
-        "https://www.twitch.tv/tfue/clip/AgileCallousOpossumBudBlast"
+        "https://www.twitch.tv/tfue/clip/AgileCallousOpossumBudBlast",
+        "https://www.twitch.tv/tfue/clip/ArtsyEnticingQueleaBrainSlug-kVfsJZyhxv2ggfmm"
     ]
 };
