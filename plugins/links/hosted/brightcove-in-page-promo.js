8f5c9baacaffff4e6f48648f0a8c73ac0975f119
Ivan Paramonau <i.paramonau@gmail.com>
2023-04-20 09:34:49 -0400

    tests: review and add a test url for brightcove-in-page-promo

--- plugins/links/hosted/brightcove-in-page-promo.js
+++ plugins/links/hosted/brightcove-in-page-promo.js
@@ -10,23 +10,23 @@ export default {
     getData: function(cheerio, __allowBrightcoveInPage) {
 
         /**
-         * we are looking for following video on the page, and check that embed is allowed
-         *          <video
-         *              id="video-js-4306274716001"
-         *               data-account="1125911414"
-         *               data-player="VJ949r8Fg"
-         *               data-embed="default"
-         *               data-video-id="4306274716001"
-         *               data-iframe-url="[ URL of Embed ]"
-         *               class="main-media__video-player video-js"
-         *               autoplay>
-         *           </video>
-         *           <script src="//players.brightcove.net/1125911414/VJ949r8Fg_default/index.min.js"></script>
+         * We are looking for following video on the page, and check that embed is allowed
+         * <video <!-- or <video-js> -->
+         *     id="video-js-4306274716001"
+         *      data-account="1125911414"
+         *      data-player="VJ949r8Fg"
+         *      data-embed="default"
+         *      data-video-id="4306274716001"
+         *      data-iframe-url="[ URL of Embed ]"
+         *      class="main-media__video-player video-js"
+         *      autoplay>
+         *  </video>
+         *  <script src="//players.brightcove.net/1125911414/VJ949r8Fg_default/index.min.js"></script>
         */
 
         var $video = cheerio('video.video-js, video-js');
 
-        if ($video.length == 1) {
+        if ($video.length === 1) {
 
             var embed = $video.attr('data-embed');
             var account = $video.attr('data-account');
@@ -53,10 +53,7 @@ export default {
         }
     },
 
-    /**
-    * http://www.hollywoodreporter.com/video/vin-diesel-voicing-groot-guardians-615140
-    * http://www.billboard.com/articles/columns/rock/6605231/eddie-van-halen-addiction-david-lee-roth-touring?mobile_redirection=false
-    * http://www.mirror.co.uk/3am/celebrity-news/stephen-fry-blasted-dangerous-views-7733815
-    */
-
+    tests: [
+        "https://www.military.com/video/aircraft/military-aircraft/tr-3b-aurora-anti-gravity-spacecrafts/2860314511001"
+    ]
 };
