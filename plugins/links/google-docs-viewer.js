ff608c6e289b7a15260d7818fc4bd9d1cf490727
Ivan Paramonau <i.paramonau@gmail.com>
2023-05-12 10:28:41 -0400

    domains: fallback for PDF size detection on some S3 distributions

--- plugins/links/google-docs-viewer.js
+++ plugins/links/google-docs-viewer.js
@@ -1,6 +1,6 @@
 export default {
 
-    getLink: function(url, __nonHtmlContentData, utils, options) {
+    getLink: function(url, __nonHtmlContentData, utils, headers, options, iframelyRun, cb) {
 
         if (!options.getProviderOptions('disableDocViewers', false) &&
             /application\/pdf|text\/rtf/.test(__nonHtmlContentData.type)) {
@@ -17,22 +17,49 @@ export default {
                 utils.getContentType(src, src, options, function(error, data) {
                 });
 
-                return {
+                return cb(null, {
                     href: src,
                     type: CONFIG.T.text_html,
                     rel: [CONFIG.R.reader, CONFIG.R.file],
                     "aspect-ratio": CONFIG.DOC_ASPECT_RATIO
-                }
+                })
+
+            // Amazon S3-powered CloudFront distributions sometimes don't have `content_length` on cache miss,
+            // `accept-encoding: identity` fixes that
+            } else if (typeof __nonHtmlContentData.content_length === 'undefined'
+                && headers.server === "AmazonS3"
+                && options.stopRecursion !== true) {
+
+                var options2 = {...options, ...{
+                    proxy: {
+                        headers: {
+                            "Accept-Encoding": "identity"
+                        }
+                    },
+                    debug: false,
+                    stopRecursion: true, // avoid infinite recursion in processing
+                    refresh: true // avoid using cached version with no content-length header
+                }};
+
+                iframelyRun(url, options2, function(error, data) {
+                    if (!data || !data.links) {
+                        return cb({
+                            message: "This AmazonS3 bucket does not provide content-length, needed for PDF validation"
+                        });
+                    } else {
+                        return cb(error || data.messages && {message: data.messages[0]}, data.links);
+                    }
+                });
 
             } else {
-                return {
+                return cb(null, {
                     message: 'PDF files that are over 10Mb are not supported by Google Docs Viewer'
-                }
+                })
             }
         } else if (/application\/pdf|text\/rtf/.test(__nonHtmlContentData.type)) {
-            return {
+            return cb(null, {
                 message: 'PDFs are not supported per your media settings'
-            }
+            })
         }
     }
 };
