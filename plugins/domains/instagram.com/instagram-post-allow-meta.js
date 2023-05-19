077168bc2522d2e89e625d945f3428ab4dd007f6
Ivan Paramonau <i.paramonau@gmail.com>
2023-05-19 09:53:50 -0400

    domains: detect 404s for IG in a fallback mode

--- plugins/domains/instagram.com/instagram-post-allow-meta.js
+++ plugins/domains/instagram.com/instagram-post-allow-meta.js
@@ -4,9 +4,15 @@ export default {
 
     provides: 'ipOG',
 
-    getData: function(__allowInstagramMeta, meta) {
-        return {
-            ipOG: meta.og ? meta.og : {}
+    getData: function(__allowInstagramMeta, meta, cb) {
+        if (!meta.og && !meta.twitter && !meta.ld 
+            && !meta.title && !meta.description 
+            && meta['html-title'] === 'Instagram') {
+            return cb({responseStatusCode: 404})
+        } else {
+            return cb (null, {
+                ipOG: meta.og ? meta.og : {}
+            })
         }
     }
 };
