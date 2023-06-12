e592a32ef3ebd238cd098b2977e7f8d12f4612a6
itarcontact <55651445+itarcontact@users.noreply.github.com>
2023-06-12 22:01:53 +0200

    Fix problem with rendering Facebook posts with new url (#505)
    * Fix problem with rendering Facebook posts with new url
    
    * Code formatting update
    
    ---------
    
    Co-authored-by: Kamil Walukiewicz <k.walukiewicz@arcontact.pl>

--- plugins/domains/facebook.com/facebook.post.js
+++ plugins/domains/facebook.com/facebook.post.js
@@ -7,6 +7,7 @@ export default {
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/photo\.php\?(?:[^\?]+)?fbid=(\d{10,})/i,
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/photo\/?\?(?:[^\?]+)?fbid=(\d{10,})/i,
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/([a-zA-Z0-9\.\-]+)\/(posts|activity)\/(\d{10,})/i,
+        /^https?:\/\/(?:www|m|business)\.facebook\.com\/([a-zA-Z0-9\.\-]+)\/(posts|activity)\/pfbid([a-zA-Z0-9\.\-]+)/i,
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/([a-zA-Z0-9\.\-]+)\/photos(?:\/[^\/]+)?\/(\d{10,})/i,
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/notes\/([^\/\?]+)\/[^\/]+\/(\d{10,})/i,
         /^https?:\/\/(?:www|m|business)\.facebook\.com\/media\/set\/\?set=[^\/]+(\d{10,})/i
@@ -20,10 +21,10 @@ export default {
 
         var html = oembed.html.replace(/data-width=\"\d+\"/, 'data-width="' + width + '"');
             html = html.replace(/class=\"fb\-video\"/i, 'class="fb-post"'); // thank you FB for not working well with photo.php
-            html = html.replace(/connect\.facebook\.net\/\w{2}_\w{2}\/sdk\.js/i, 
-                'connect.facebook.net/' + options.getProviderOptions('locale', 'en_US').replace('-', '_') + '/sdk.js'); // FB gives it based on server IP, and it has inaccurate IP2Location 
+            html = html.replace(/connect\.facebook\.net\/\w{2}_\w{2}\/sdk\.js/i,
+                'connect.facebook.net/' + options.getProviderOptions('locale', 'en_US').replace('-', '_') + '/sdk.js'); // FB gives it based on server IP, and it has inaccurate IP2Location
 
-        if (/photos?/i.test(url) 
+        if (/photos?/i.test(url)
             && options.getRequestOptions('facebook.hide_text')
             && !/data-show-text="false"/.test(html)) {
             html = html.replace(/data\-show\-text=\"true\"/i, ''); // never is retured from API, but just to future-proof
@@ -51,6 +52,7 @@ export default {
 
     tests: [
         "https://www.facebook.com/noven.roman/posts/555607674475258",
+        "https://www.facebook.com/noven.roman/posts/pfbid02r7N1GwTBDFDqW8cN51BjKeFFmNKGU9vBFpCfF2aV5CevoPy64gYZmCXESTB3XHqAl",
         "https://www.facebook.com/logvynenko/posts/10151487164961783",
         "https://www.facebook.com/chamvermeil/photos/a.398119066992522.1073741828.398102673660828/715129168624842/?type=1&theater",
         "https://www.facebook.com/photo?fbid=3242822485762635&set=pcb.3242829905761893",
