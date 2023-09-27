9f77c0884e95d71167cc11c6779693a9e6d99825
Ivan Paramonau <i.paramonau@gmail.com>
2023-09-27 07:59:11 -0400

    domains: redirect new Dailymotion player to canonical

--- plugins/domains/dailymotion.com/dailymotion.swf.js
+++ plugins/domains/dailymotion.com/dailymotion.swf.js
@@ -1,10 +1,15 @@
 export default {
 
-    re: /^https?:\/\/www\.dailymotion\.com\/(swf|embed)\/video\//i,
+    re: [
+        /^https?:\/\/www\.dailymotion\.com\/(swf|embed)\/video\/([a-zA-Z0-9]+)/i,
+        /^https?:\/\/(?:geo\.)?dailymotion\.com\/player\.html\?video=([a-zA-Z0-9]+)/i
+    ],
 
-    getLink: function (url, cb) {
+    listed: false,
+
+    getLink: function (urlMatch, cb) {
         cb ({
-            redirect: url.replace(/\/(swf|embed)/, '')
+            redirect: 'https://www.dailymotion.com/video/' + urlMatch[1]
         });
     }
 };
