75cbf4ab087d915cdd97c69bfbddc0f29696efa5
Ivan Paramonau <i.paramonau@gmail.com>
2023-09-27 08:17:00 -0400

    typo

--- plugins/links/hosted/known-players.js
+++ plugins/links/hosted/known-players.js
@@ -24,8 +24,8 @@ export default {
         // or DailyMotion, e.g. Liberation, Le Point, L'Express
         urlMatch = video_src.match(/^(?:https?:)?\/\/(?:www\.)?dailymotion\.com\/(?:swf|embed)?\/?video\/([_a-zA-Z0-9\-]+)/i)
                 || video_src.match(/^(?:https?:)?\/\/(?:www\.)?dailymotion\.com\/swf\/([_a-zA-Z0-9\-]+)/i)
-                || video_src.match(/^(?:https?:)?\/\/dai.ly\/([_a-zA-Z0-9\-]+)/i); // e.g. Libération.fr
-                || video_src.match(/^https?:\/\/(?:geo\.)?dailymotion\.com\/player\.html\?video=([a-zA-Z0-9]+)/i)
+                || video_src.match(/^(?:https?:)?\/\/dai.ly\/([_a-zA-Z0-9\-]+)/i) // e.g. Libération.fr
+                || video_src.match(/^https?:\/\/(?:geo\.)?dailymotion\.com\/player\.html\?video=([a-zA-Z0-9]+)/i);
 
         if (urlMatch) {
             return {
