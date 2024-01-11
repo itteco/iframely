db7d0eb5195900419b899a8946c4ad7a738da307
Ivan Paramonau <i.paramonau@gmail.com>
2024-01-10 19:51:41 -0500

    domains: better support of dbdiagram slugs

--- plugins/domains/dbdiagram.io.js
+++ plugins/domains/dbdiagram.io.js
@@ -1,6 +1,6 @@
 export default {
 
-    re: /^https?:\/\/dbdiagram\.io\/(?:d|embed)\/([a-zA-Z0-9]+)/i,
+    re: /^https?:\/\/dbdiagram\.io\/(?:d|embed)\/(?:[a-zA-Z0-9\-]+\-)?([a-zA-Z0-9]+)/i,
 
     mixins: [
         "*"
@@ -20,7 +20,8 @@ export default {
         "https://dbdiagram.io/d/6201630385022f4ee5518762",
         "https://dbdiagram.io/embed/61eeb8797cf3fc0e7c5d74aa",
         "https://dbdiagram.io/d/61f2c0ec85022f4ee5019a3b",
-        "https://dbdiagram.io/embed/5cf4e1101f6a891a6a658ef9"
+        "https://dbdiagram.io/embed/5cf4e1101f6a891a6a658ef9",
+        "https://dbdiagram.io/d/Webtraffic-Syndicate-659ef439ac844320aea87aa0"
     ]
 
 };
