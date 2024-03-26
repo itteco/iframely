7a99085cdbaa35324b40f68b00728d4a7aec81ae
Alexey <siniakinaa@gmail.com>
2024-03-26 18:12:25 +0300

    change urls apple.music and sort by domain

--- plugins/domains/itunes.apple.com/apple.music.js
+++ plugins/domains/itunes.apple.com/apple.music.js
@@ -11,9 +11,7 @@ export default {
 
     highestPriority: true,
 
-    mixins: [
-        "*"
-    ],
+    mixins: ["*"],
 
     getMeta: function () {
         return {
@@ -60,11 +58,9 @@ export default {
         noFeeds: true
     },
         'https://itunes.apple.com/us/album/12-12-12-concert-for-sandy/id585701590?v0=WWW-NAUS-ITSTOP100-ALBUMS&ign-mpt=uo%3D4',
-        'https://itunes.apple.com/us/album/id944094900?i&ls=1',
+        'https://itunes.apple.com/us/album/id1731059947?i&ls=1',
         "https://itunes.apple.com/album/id1125277620",
-        'https://geo.itunes.apple.com/us/album/reaching-for-indigo/id1264016548?app=music',
         'https://itunes.apple.com/us/playlist/the-a-list-country/idpl.87bb5b36a9bd49db8c975607452bfa2b?app=music',
-        'https://geo.itunes.apple.com/us/album/call-me-by-your-name-original-motion-picture-soundtrack/id1300430864?app=music',
         'https://itunes.apple.com/us/playlist/its-lit/idpl.2d4d74790f074233b82d07bfae5c219c?mt=1&app=music',
         'https://itunes.apple.com/jp/playlist/2019-3-16-maison-book-girl-%25E7%25A6%258F%25E5%25B2%25A1drum-son/pl.u-6mo4lNLiBvm0AAx?app=music',
         'https://itunes.apple.com/th/album/icecream/1263324000?i=1263324326',
@@ -72,6 +68,8 @@ export default {
         'https://music.apple.com/jp/album/back-to-the-80s/1458246986',
         'https://music.apple.com/us/album/gypsy-woman-shes-homeless-basement-boy-strip-to-the-bone-mix/1434891258?i=1434891369',
         'https://music.apple.com/fr/post/sa.01cb7f20-f25e-11e6-b1a9-afb0a09d5237',
-        'https://music.apple.com/it/post/sa.82ca58c0-41d5-11ea-a9de-158fbdf307c6'
+        'https://music.apple.com/it/post/sa.82ca58c0-41d5-11ea-a9de-158fbdf307c6',
+        'https://geo.itunes.apple.com/us/album/reaching-for-indigo/id1264016548?app=music',
+        'https://geo.itunes.apple.com/us/album/call-me-by-your-name-original-motion-picture-soundtrack/id1300430864?app=music',
     ]
 };
