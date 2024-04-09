3016c7d0680ad7018af24abcdc30597b308e7ccc
Ivan Paramonau <i.paramonau@gmail.com>
2024-04-09 11:28:20 -0400

    tests: fix error in Dailymotion redirects

--- plugins/domains/dailymotion.com/dailymotion.swf.js
+++ plugins/domains/dailymotion.com/dailymotion.swf.js
@@ -1,14 +1,14 @@
 export default {
 
     re: [
-        /^https?:\/\/www\.dailymotion\.com\/(swf|embed)\/video\/([a-zA-Z0-9]+)/i,
+        /^https?:\/\/www\.dailymotion\.com\/(?:swf|embed)\/video\/([a-zA-Z0-9]+)/i,
         /^https?:\/\/(?:geo\.)?dailymotion\.com\/player\.html\?video=([a-zA-Z0-9]+)/i
     ],
 
     listed: false,
 
     getLink: function (urlMatch, cb) {
-        cb ({
+        return cb({
             redirect: 'https://www.dailymotion.com/video/' + urlMatch[1]
         });
     }
