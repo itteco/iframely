df8d5a91c40eed1aa81b5df10d184ca1404e82b9
Ivan Paramonau <i.paramonau@gmail.com>
2024-04-29 16:44:26 -0400

    domains: support more of washington post videos

--- plugins/domains/washingtonpost.com.js
+++ plugins/domains/washingtonpost.com.js
@@ -1,6 +1,6 @@
 export default {
 
-    re: /^https:\/\/www\.washingtonpost\.com\/(?:video|posttv)\/\w+\/[a-z0-9-]+\/\d+\/\d+\/\d+\/([a-z0-9-]+)_video\.html/i,
+    re: /^https:\/\/www\.washingtonpost\.com\/(?:video|posttv)\/\w+\/(?:\w+\/)?[a-z0-9-]+\/\d+\/\d+\/\d+\/([a-z0-9-]+)_video\.html/i,
 
     mixins: [
         "*"
@@ -23,6 +23,7 @@ export default {
     },
         "https://www.washingtonpost.com/video/world/the-uk-just-voted-to-leave-the-eu-heres-what-that-means/2016/06/24/df857508-38b6-11e6-af02-1df55f0c77ff_video.html",
         "https://www.washingtonpost.com/video/national/michael-gove-whatever-charisma-is-i-dont-have-it/2016/07/01/98f82a76-3f92-11e6-9e16-4cf01a41decb_video.html",
-        "https://www.washingtonpost.com/video/world/kerry-special-relationship-with-uk-will-continue/2016/06/26/01e6695a-3bc2-11e6-9e16-4cf01a41decb_video.html"        
+        "https://www.washingtonpost.com/video/world/kerry-special-relationship-with-uk-will-continue/2016/06/26/01e6695a-3bc2-11e6-9e16-4cf01a41decb_video.html",
+        "https://www.washingtonpost.com/video/local/weather/destructive-storms-slam-south/2024/04/11/096f78b3-55b1-4002-a6b0-0626b0ddd53d_video.html"
     ]
 };
