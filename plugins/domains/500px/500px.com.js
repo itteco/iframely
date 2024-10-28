e7b04830c89e6b86d769c77bee3b5a9531566a46
Ivan Paramonau <i.paramonau@gmail.com>
2024-10-28 08:17:19 -0400

    domains: fix 500px

--- plugins/domains/500px/500px.com.js
+++ plugins/domains/500px/500px.com.js
@@ -4,18 +4,26 @@ export default {
 
     mixins: ["*"],
 
-    getLinks: function(urlMatch, twitter) {
-        if (twitter.card === 'photo' && twitter.image) {
-            return {
-                template_context: {
-                    title: twitter.title + ' | ' + twitter.site,
-                    img_src: twitter.image.src,
-                    id: urlMatch[1]
-                },
-                type: CONFIG.T.text_html,
-                rel: [CONFIG.R.image, CONFIG.R.ssl, CONFIG.R.inline],
-                "aspect-ratio": twitter.image.height ? twitter.image.width / twitter.image.height : null
-            }
+    getLinks: function(urlMatch, twitter, utils, options, cb) {
+        if (twitter.image) {
+            utils.getImageMetadata(twitter.image, options, function(error, data) {
+                if (data.width && data.height) {
+                    return cb(null, {
+                        template_context: {
+                            title: twitter.title + ' | ' + twitter.site,
+                            img_src: twitter.image,
+                            id: urlMatch[1]
+                        },
+                        type: CONFIG.T.text_html,
+                        rel: [CONFIG.R.image, CONFIG.R.ssl, CONFIG.R.inline],
+                        "aspect-ratio": data.width / data.height
+                    })
+                } else {
+                    return cb();
+                }
+            });
+        } else {
+            return cb();
         }
     },
 
