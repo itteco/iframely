c42334e15453665d30c6e09a420b6a205adbf0b0
Ivan Paramonau <i.paramonau@gmail.com>
2024-10-31 10:23:19 -0400

    meta: rename the field for ratings and age-restrictions into "warning"

--- plugins/domains/youtube.com/youtube.video.js
+++ plugins/domains/youtube.com/youtube.video.js
@@ -147,7 +147,7 @@ export default {
             likes: youtube_video_gdata.likeCount,
             dislikes: youtube_video_gdata.dislikeCount,
             views: youtube_video_gdata.viewCount,
-            rating: youtube_video_gdata.ytRating === 'ytAgeRestricted' ? 'age-restricted': null,
+            warning: youtube_video_gdata.ytRating === 'ytAgeRestricted' ? 'age': null,
             site: "YouTube",
             canonical: "https://www.youtube.com/watch?v=" + youtube_video_gdata.id,
             author_url: "https://www.youtube.com/" + (youtube_video_gdata.channelId  ? "channel/" + youtube_video_gdata.channelId : "user/" + youtube_video_gdata.uploader)
