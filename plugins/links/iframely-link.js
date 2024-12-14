346e3e061da7b07683195745d57813bcba407044
Ivan Paramonau <i.paramonau@gmail.com>
2024-12-13 21:35:57 -0500

    iframely protocol: support publishers with custom domain names

--- plugins/links/iframely-link.js
+++ plugins/links/iframely-link.js
@@ -7,7 +7,7 @@ export default {
         if (appname) {
             appname = appname.toLowerCase();
         }
-        
+
         let ignoreIframely = false;
         let links = [];
 
@@ -15,13 +15,22 @@ export default {
             ignoreIframely = true;
         }
 
-        for (const [key, value] of Object.entries(meta)) {
+        for (const [key, v] of Object.entries(meta)) {
             if (key.indexOf(appname) === 0 || (!ignoreIframely && key.indexOf(CONFIG.R.iframely) === 0)) {
+
+                const value = typeof(v) === 'string' ? {href: v} : v; // If link has no `media` and no `type` attributes, HTMLMetaHandler assigns the value=href;
+
+                let wlr = whitelistRecord;
+                if (whitelistRecord.isDefault
+                    && !Array.isArray(value) && value.href) {
+                    wlr = options.getWhitelistRecord(value.href, {exclusiveRel: 'html-meta'});
+                }
+
                 links = links.concat(
                     utils.parseMetaLinks(
                         key, 
-                        typeof(value) === 'string' ? {href: value} : value, // If link has no `media` and no `type` attributes, HTMLMetaHandler assigns the value=href;
-                        whitelistRecord, 
+                        value,
+                        wlr, 
                         ignoreIframely && appname
                     )
                 );
