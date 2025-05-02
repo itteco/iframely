3adffca7089f642e47823470b2dcd45ac0157957
Alexey <siniakinaa@gmail.com>
2025-05-02 15:26:57 +0300

    cartodb.com: update test urls

--- plugins/domains/cartodb.com.js
+++ plugins/domains/cartodb.com.js
@@ -33,11 +33,8 @@ export default {
     tests: [{
         noFeeds: true
     },
-        "https://artbayview.carto.com/viz/f8759b54-a515-11e5-bc9d-0ea31932ec1d/public_map",
-        "https://naelshiab.carto.com/viz/ca48508e-3178-11e5-af3e-0e4fddd5de28",
-        "https://alasdair.cartodb.com/viz/9d46d8a2-f690-11e5-b0a2-0e31c9be1b51/embed_map",
-        "https://smb2stfinitesubs1.cartodb.com/viz/39e625ee-cef3-11e4-b8bb-0e0c41326911/public_map",
-        "https://twittereurope.cartodb.com/u/kike-5/viz/456350ba-377e-11e5-8b42-0e853d047bba/public_map",
-        "https://bloomberg.cartodb.com/u/eanders/viz/6913f4cc-bd05-11e4-a444-7054d21a95e5/public_map"
+        "https://rpa.carto.com/viz/9a23d690-6e00-4c87-8d06-608d8bf7ca18/embed_map",
+        "https://samswey.carto.com/viz/e1005588-93d2-47b6-9a61-f4c0ed001163/embed_map",
+        "https://bloomberg.carto.com/u/eanders/viz/6913f4cc-bd05-11e4-a444-7054d21a95e5/public_map"
     ]
 };
