360004aef8ad1d38c56930eca6ba798f0141f073
Alexey <34516115+siniakinaa@users.noreply.github.com>
2025-09-01 19:45:05 +0300

    piktochart.com: fix regexp for create subdomains, add test URLs (#590)
    * pittochart.com: fix regexp for create subdomains, add test URLs
    
    * piktochart.com: add height
    
    * try to fix piktochart's height
    
    ---------
    
    Co-authored-by: Ivan Paramonau <i.paramonau@gmail.com>

--- plugins/domains/piktochart.com/piktochart.com.js
+++ plugins/domains/piktochart.com/piktochart.com.js
@@ -1,6 +1,6 @@
 export default {
 
-    re: /^https?:\/\/(?:magic|create)\.piktochart\.com\/output\/(\d+\-[\-a-zA-Z-0-9_]+)/i,
+    re: /^https?:\/\/(?:magic|create)\.piktochart\.com\/output\/([\-a-zA-Z-0-9_]+)/i,
 
     mixins: [
         "*"
@@ -13,12 +13,15 @@ export default {
                 id: urlMatch[1]
             },
             type: CONFIG.T.text_html,
-            rel: [CONFIG.R.reader, CONFIG.R.ssl],
+            rel: [CONFIG.R.reader, CONFIG.R.ssl]
         };
     },
 
     tests: [
-        "https://magic.piktochart.com/output/16505417-projet-immigration-copy"
-    ]
+        "https://magic.piktochart.com/output/16505417-projet-immigration-copy",
+        "https://magic.piktochart.com/output/14330224-descripciones-fisicas",
 
+        "https://create.piktochart.com/output/38d889d24156-youtube-pitch-deck",
+        "https://create.piktochart.com/output/04c845534560-buenas-practicas-en-la-elaboracion-de-alimentos-en-el-hogar"
+    ]
 };
