bdea96926b18b4ca575e67f1a93fb434509d9aeb
Alexey <siniakinaa@gmail.com>
2025-09-01 17:43:24 +0300

    pittochart.com: fix regexp for create subdomains, add test URLs

--- plugins/domains/piktochart.com/piktochart.com.js
+++ plugins/domains/piktochart.com/piktochart.com.js
@@ -1,6 +1,6 @@
 export default {
 
-    re: /^https?:\/\/(?:magic|create)\.piktochart\.com\/output\/(\d+\-[\-a-zA-Z-0-9_]+)/i,
+    re: /^https?:\/\/(?:magic|create)\.piktochart\.com\/output\/([\-a-zA-Z-0-9_]+)/i,
 
     mixins: [
         "*"
@@ -18,7 +18,10 @@ export default {
     },
 
     tests: [
-        "https://magic.piktochart.com/output/16505417-projet-immigration-copy"
-    ]
+        "https://magic.piktochart.com/output/16505417-projet-immigration-copy",
+        "https://magic.piktochart.com/output/14330224-descripciones-fisicas",
 
+        "https://create.piktochart.com/output/38d889d24156-youtube-pitch-deck",
+        "https://create.piktochart.com/output/04c845534560-buenas-practicas-en-la-elaboracion-de-alimentos-en-el-hogar"
+    ]
 };
