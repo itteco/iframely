3992061adebfced51e7dc45f339a38f6d20e4dca
Ivan Paramonau <i.paramonau@gmail.com>
2025-09-30 08:36:06 -0400

    domains: handle more of espn country domains

--- plugins/domains/espn.com.js
+++ plugins/domains/espn.com.js
@@ -3,6 +3,7 @@ export default {
     re: [
         /^https?:\/\/((?:www\.)?espn\.com?(?:\.\w{2})?)\/video\/clip\?id=espn:(\d+)/i,
         /^https?:\/\/((?:www\.)?espn\.com?(?:\.\w{2})?)\/video\/clip\?id=(\d+)/i,
+        /^https?:\/\/((?:www\.)?espn\.\w{2,3})\/video\/clip\?id=(\d+)/i,
         /^https?:\/\/((?:\w+\.)?espn\.com?(?:\.\w{2})?)\/(?:videohub\/)?video\/clip\/_\/id\/(\d+)/i,
         /^https?:\/\/((?:www\.)?espn\.com)\/watch\/player\/[^\/]+\/id\/(\d+)/i
     ],
@@ -26,6 +27,7 @@ export default {
         "https://www.espn.com.ar/video/clip?id=10100188",
         "http://www.espn.com/videohub/video/clip/_/id/18883925/categoryid/2378529",
         "https://www.espn.com/watch/player/_/id/27394219/country/us/redirected/true#bucketId=1",
-        "https://espndeportes.espn.com/video/clip/_/id/13326426"
+        "https://espndeportes.espn.com/video/clip/_/id/13326426",
+        "https://www.espn.ph/video/clip?id=44369066"
     ]
 };
