20f963594c22df861ec2c1d01643deea3eb5f2b1
Alexey <siniakinaa@gmail.com>
2025-10-23 08:58:08 +0300

    dailymain.enbeddedvideo: update test URL

--- plugins/domains/dailymail.co.uk/dailymail.embeddedvideo.js
+++ plugins/domains/dailymail.co.uk/dailymail.embeddedvideo.js
@@ -19,7 +19,7 @@ export default {
     tests: [{
         noFeeds: true
     },
-        "https://www.dailymail.co.uk/femail/article-11847441/Watch-new-Duke-Edinburgh-introduces-Sophie-duchess-time.html#v-6755232965847660921",
+        "https://www.dailymail.co.uk/lifestyle/article-11847441/Watch-new-Duke-Edinburgh-introduces-Sophie-duchess-time.html#v-6755232965847660921",
         "https://www.dailymail.co.uk/news/article-3556177/Was-MH17-shot-Ukrainian-fighter-jet-BBC-documentary-claims-Boeing-777-targeted-plane.html#v-8296301435444282732"
     ]
 };
