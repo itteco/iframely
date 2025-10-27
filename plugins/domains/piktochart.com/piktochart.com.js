1bf82e1dc38bdbe4695a7c37bc2df60e328b1cce
Alexey <34516115+siniakinaa@users.noreply.github.com>
2025-10-27 17:47:44 +0300

    piktochart.com: update test URL (#598)

--- plugins/domains/piktochart.com/piktochart.com.js
+++ plugins/domains/piktochart.com/piktochart.com.js
@@ -19,7 +19,7 @@ export default {
 
     tests: [
         "https://magic.piktochart.com/output/16505417-projet-immigration-copy",
-        "https://magic.piktochart.com/output/14330224-descripciones-fisicas",
+        "https://magic.piktochart.com/output/7850098-pm-syllabus",
 
         "https://create.piktochart.com/output/986319273bed-trump-tariffs",
         "https://create.piktochart.com/output/4407f4418cb3-setmana-del-govern-obert-2024"
