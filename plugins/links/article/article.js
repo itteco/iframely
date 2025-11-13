5766acda871c46ce446c7608d8c175cfef8e7f7e
Ivan Paramonau <i.paramonau@gmail.com>
2025-11-13 14:07:48 -0500

    validate that articlebody is not empty

--- plugins/links/article/article.js
+++ plugins/links/article/article.js
@@ -1,10 +1,18 @@
+import * as cheerio from 'cheerio';
+
 export default {
 
-    provides: 'articlebody',
+    provides: 'articlebody', // if not yet provided from LD articlebody
 
     getData: function(__readabilityEnabled, readability, meta, utils) {
-        return {
-            articlebody: utils.encodeText(meta.charset, readability.getHTML())
+
+        const articleHtml = utils.encodeText(meta.charset, readability.getHTML());
+        const $p = cheerio.load(articleHtml)('p');
+
+        if ($p.text()) {
+            return {
+                articlebody: articleHtml
+            }
         }
     },
 
