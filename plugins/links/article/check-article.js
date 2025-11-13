2c57cd1280ba35876d2a8ea5ceb7bb2d308decaf
Ivan Paramonau <i.paramonau@gmail.com>
2025-11-13 10:25:37 -0500

    fix conditions

--- plugins/links/article/check-article.js
+++ plugins/links/article/check-article.js
@@ -9,10 +9,11 @@ export default {
 
         const ld = meta.ld?.newsarticle || meta.ld?.article || meta.ld?.blogposting || meta.ld?.reportagenewsarticle || meta.ld?.socialmediaposting;
 
-        if (ld
-            || (meta.og && (meta.og.type === "article" || meta.og.type === "blog" || meta.og.type === 'website')
-            || meta.twitter?.card === 'summary_large_image'
-            || meta.article)
+        if ((ld
+                || (meta.og && (meta.og.type === "article" || meta.og.type === "blog" || meta.og.type === 'website')
+                || meta.twitter?.card === 'summary_large_image'
+                || meta.article))
+            
             && (options.getRequestOptions('readability.articlebody', false) || CONFIG.providerOptions?.app?.allow_readability === true)) {
 
             if (ld?.articlebody && /\/>/.test(ld.articlebody)) {
