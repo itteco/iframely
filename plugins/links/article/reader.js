7c223b934fc84e3ccbee798c4b3a3289dd2fe51c
Ivan Paramonau <i.paramonau@gmail.com>
2025-11-17 11:12:44 -0500

    [Articlebody] Allow to choose between HTML and TXT formats (#607)
    * [Articlebody] Allow to choose between HTML and TXT formats
    
    * reinstate spaces when texting paragraphs

--- plugins/links/article/reader.js
+++ plugins/links/article/reader.js
@@ -1,7 +1,7 @@
 export default {
 
-    getData: function(articlebody) {
-        if (CONFIG.providerOptions?.app?.allow_readability === true && !CONFIG.SKIP_IFRAMELY_RENDERS) {
+    getData: function(__readabilityEnabled, articlebody) {
+        if (__readabilityEnabled === 'html' && CONFIG.providerOptions?.app?.allow_readability === true && !CONFIG.SKIP_IFRAMELY_RENDERS) {
             return  {
                 safe_html: articlebody
             }
