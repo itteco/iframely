cdf2886a1a3e490ee6c45b85f02cb88598fe5eae
Alexey <siniakinaa@gmail.com>
2026-01-29 15:58:22 +0300

    v.qq.com: add static test URLs, there are no hrefs in selector yet

--- plugins/domains/v.qq.com.js
+++ plugins/domains/v.qq.com.js
@@ -1,7 +1,7 @@
 const re = [
-    /^https?:\/\/v\.qq\.com\/page\/\w\/\w\/\w\/(\w+)\.html$/i,
-    /^https?:\/\/v\.qq\.com\/\w+\/page\/(\w+)\.html$/i,
-    /^https?:\/\/v\.qq\.com\/\w+\/\w\/\w+\.html\?vid=(\w+)$/i,
+    /^https?:\/\/v\.qq\.com\/page\/\w\/\w\/\w\/(\w+)\.html/i,
+    /^https?:\/\/v\.qq\.com\/\w+\/page\/(\w+)\.html/i,
+    /^https?:\/\/v\.qq\.com\/\w+\/\w\/\w+\.html\?vid=(\w+)/i,
     /^https?:\/\/v\.qq\.com\/iframe\/(?:player|preview)\.html\?vid=(\w+)/i,
     /^https?:\/\/v\.qq\.com\/\w\/cover\/\w+\/(\w+)\.html/i
 ];
@@ -23,11 +23,14 @@ export default {
         };
     },
 
-    tests: [{
-        page: "https://v.qq.com/",
-        selector: ".video-banner-item a.poster-pic-container",
-        getUrl: function(url) {
-            return re.some((r) => r.test(url));
-        }
-    }]
+    tests: [
+        "https://v.qq.com/x/cover/mzc00200xxpsogl/j4101ouc4ve.html",
+        "https://v.qq.com/x/cover/qab156ofmrj45xy/r0025sd3txm.html",
+        "https://v.qq.com/x/page/h0305y9ksmw.html",
+        "https://v.qq.com/x/page/d3237p2gogi.html",
+        "https://v.qq.com/page/z/5/3/z01698x4h53.html",
+        "https://v.qq.com/page/o/p/p/o0196h1eppp.html",
+        "https://v.qq.com/boke/page/a/0/5/a031175aee5.html",
+        "http://v.qq.com/boke/page/f/3/p/f0188kmbq3p.html"
+    ]
 };
