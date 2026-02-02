7cdcd17b6db4c40807886223daaa1b07b2949586
Alexey <siniakinaa@gmail.com>
2026-02-02 15:55:26 +0300

    youku.com: update page and selector

--- plugins/domains/youku.com.js
+++ plugins/domains/youku.com.js
@@ -1,6 +1,5 @@
 export default {
 
-    //http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842
     re: [
         /^https?:\/\/v\.youku\.com\/v_show\/id_([a-z0-9=_]{3,})\.html/i,
         /^https?:\/\/news\.youku\.com\/(\w{3,})/i
@@ -19,13 +18,12 @@ export default {
     },
 
     tests: [{
-        page: "http://www.youku.com/",
-        selector: ".channelcommon_container .pack_pack_cover a"
+        page: "https://www.youku.com/ku/lexue",
+        selector: ".hscroll_wrapper_3CJzY .hscroll_content_fdYOj a"
     },
-        "http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
-        "http://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
+        "https://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
+        "https://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
         "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html",
         "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html"
-
     ]
 };
