9eb89318e79d2c83e6a16d82cd8a997a6978b13f
Ivan Paramonau <i.paramonau@gmail.com>
2026-02-02 12:28:33 -0500

    fix youku tests

--- plugins/domains/youku.com.js
+++ plugins/domains/youku.com.js
@@ -1,8 +1,7 @@
 export default {
 
     re: [
-        /^https?:\/\/v\.youku\.com\/v_show\/id_([a-z0-9=_]{3,})\.html/i,
-        /^https?:\/\/news\.youku\.com\/(\w{3,})/i
+        /^https?:\/\/v\.youku\.com\/v_show\/id_([a-z0-9=_]{3,})\.html/i
     ],
 
     mixins: ["*"],
@@ -12,16 +11,11 @@ export default {
             href: "https://player.youku.com/embed/"+ urlMatch[1],
             type: CONFIG.T.text_html,
             rel: CONFIG.R.player,
-            "aspect-ratio": 16/10, //As stated it in docs
-            autoplay: "autoplay=true"
+            "aspect-ratio": 16/10 //As stated it in docs
         }
     },
 
-    tests: [{
-        // https://v.youku.com/video* redirects to https://v.youku.com/v_show/id*
-        page: "https://www.youku.com/ku/lexue",
-        selector: ".hscroll_wrapper_3CJzY .hscroll_content_fdYOj a"
-    },
+    tests: [
         "https://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
         "https://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
         "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html",
