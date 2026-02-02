0aa02a049914bc199ef054e4210f9450d8dddb70
Aliaksei <34516115+siniakinaa@users.noreply.github.com>
2026-02-02 20:51:05 +0300

    [Domains] Review tests in domain plugins (#621)
    * soundcloud.com: fix user profiles tests
    
    * youku.com: update page and selector
    
    * imageshack.com: update selector in tests
    
    * youku.com: add regexp for native hrefs on pages
    
    * imageshack.com: change page and selector to avoid 504 error
    
    * v.qq.com: https instead of http
    
    * better handling of 403 on SoundCloud profiles
    
    * remove redundant regex for youku
    
    * fix youku tests
    
    ---------
    
    Co-authored-by: Ivan Paramonau <i.paramonau@gmail.com>

--- plugins/domains/youku.com.js
+++ plugins/domains/youku.com.js
@@ -1,9 +1,7 @@
 export default {
 
-    //http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842
     re: [
-        /^https?:\/\/v\.youku\.com\/v_show\/id_([a-z0-9=_]{3,})\.html/i,
-        /^https?:\/\/news\.youku\.com\/(\w{3,})/i
+        /^https?:\/\/v\.youku\.com\/v_show\/id_([a-z0-9=_]{3,})\.html/i
     ],
 
     mixins: ["*"],
@@ -13,19 +11,14 @@ export default {
             href: "https://player.youku.com/embed/"+ urlMatch[1],
             type: CONFIG.T.text_html,
             rel: CONFIG.R.player,
-            "aspect-ratio": 16/10, //As stated it in docs
-            autoplay: "autoplay=true"
+            "aspect-ratio": 16/10 //As stated it in docs
         }
     },
 
-    tests: [{
-        page: "http://www.youku.com/",
-        selector: ".channelcommon_container .pack_pack_cover a"
-    },
-        "http://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
-        "http://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
+    tests: [
+        "https://v.youku.com/v_show/id_XNDkwNjg2NzQw.html?f=18736842",
+        "https://v.youku.com/v_show/id_XMTY4NDYyMTQ2OA==.html?f=27354669&from=y1.2-3.4.15#paction",
         "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html",
         "https://v.youku.com/v_show/id_XNDgxNTEzNzk5Mg==.html"
-
     ]
 };
