7032a13ef051956c49ae9ffbefa964a9e31972a9
Alexey <siniakinaa@gmail.com>
2026-02-09 16:00:20 +0300

    tumblr.text: remove bad test URL

--- plugins/domains/tumblr.com/tumblr.text.js
+++ plugins/domains/tumblr.com/tumblr.text.js
@@ -31,8 +31,7 @@ export default {
     },
 
     tests: [{skipMethods: ["getData"]},
-        "http://blog.slides.com/post/84828911898/slides-turns-one-year-old",
-        "https://butts-bouncing-on-the-beltway.tumblr.com/post/804116029629399040",
-        "https://benimpencerelerim.tumblr.com/post/670837733034541056/muhafazakarligin-arkeolojisi"
+        "https://blog.slides.com/post/84828911898/slides-turns-one-year-old",
+        "https://butts-bouncing-on-the-beltway.tumblr.com/post/804116029629399040"
     ]
 };
