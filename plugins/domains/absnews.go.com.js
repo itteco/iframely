508967103420ad0097851a7bea1d55ee5af51361
Alexey <siniakinaa@gmail.com>
2026-02-16 13:17:55 +0300

    absnews.go.com: take into account the redirect to nbsnews.com

--- plugins/domains/absnews.go.com.js
+++ plugins/domains/absnews.go.com.js
@@ -1,7 +1,8 @@
 export default {
 
     re: [
-        /^https?:\/\/abcnews\.go\.com\/\w+\/(?:\w+\/)?video\/[a-zA-Z0-9\-_]+\-(\d+)/i        
+        /^https?:\/\/abcnews\.go\.com\/\w+\/(?:\w+\/)?video\/[a-zA-Z0-9\-_]+\-(\d+)/i,
+        /^https?:\/\/abcnews\.com\/video\/(\d+)/i // format with redirect
     ],
 
     mixins: ["*"],
@@ -9,7 +10,7 @@ export default {
     getLink: function(urlMatch) {
 
         return {
-                href: "https://abcnews.go.com/video/embed?id=" + urlMatch[1],                
+                href: "https://abcnews.com/video/embed?id=" + urlMatch[1],
                 type: CONFIG.T.text_html,
                 rel: CONFIG.R.player,
                 "aspect-ratio": 640 / 360
@@ -21,6 +22,11 @@ export default {
     },
         "https://abcnews.go.com/GMA/News/video/trump-hamas-disarm-disarm-126540156",
         "https://abcnews.go.com/Nightline/video/bob-drag-queen-surprises-year-dressed-bob-drag-43209383",
-        "https://abcnews.go.com/GMA/Wellness/video/increasing-number-children-killed-flu-epidemic-53193259"
+        "https://abcnews.go.com/GMA/Wellness/video/increasing-number-children-killed-flu-epidemic-53193259",
+
+        // format with redirect
+        "https://abcnews.com/video/126540156/",
+        "https://abcnews.com/video/43209383/",
+        "https://abcnews.com/video/53193259/"
     ]
 };
