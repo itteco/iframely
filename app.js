cf36a4ae1430abb46f863d8dcca9f12d31f47a44
Nazar Leush <n.leush@gmail.com>
2026-02-17 16:24:14 +0200

    whitelist initializes itself by default

--- app.js
+++ app.js
@@ -45,8 +45,7 @@ app.use(cacheMiddleware);
 
 import apiViews from './modules/api/views.js';
 import debugViews from './modules/debug/views.js';
-import { initializeWhitelistLoader } from './lib/whitelist.js';
-initializeWhitelistLoader();
+
 apiViews(app);
 debugViews(app);
 
