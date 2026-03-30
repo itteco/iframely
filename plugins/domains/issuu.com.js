66054c7a36e041cfc7befab00150a6c571e3b90f
Alexey <siniakinaa@gmail.com>
2026-03-30 18:34:51 +0300

    issuu.com: fix plugin

--- plugins/domains/issuu.com.js
+++ plugins/domains/issuu.com.js
@@ -1,25 +1,21 @@
 export default {
 
-    re: /^https?:\/\/issuu\.com\/[\w_.-]+\/docs\/([\w_.-]+)/i,
+    re: /^https?:\/\/issuu\.com\/([\w_.-]+)\/docs\/([\w_.-]+)/i,
 
-    mixins: [
-        "oembed-thumbnail",
-        "oembed-author",
-        "oembed-title",
-        "oembed-site",
-        "oembed-description",
-        // "og-video",
-        'domain-icon'
-    ],
+    mixins: [ "*" ],
 
-    getLink: function (oembed) {
+    getLink: function (urlMatch) {
 
-        if (!oembed.html) {
+        if (!urlMatch || urlMatch.length < 3) {
             return;
         }
 
-        // let's make it responsive
-        var html = oembed.html.replace (/style=\"[^\"]+\"/i, 'style="top: 0px; left: 0px; width: 100%; height: 100%; position: absolute;"'); 
+        const embedUrl = `https://e.issuu.com/embed.html?d=${encodeURIComponent(urlMatch[2])}&u=${encodeURIComponent(urlMatch[1])}`;
+
+        let html = `<iframe src="${embedUrl}" style="border:none;width:100%;height:100%;" allowfullscreen allow="fullscreen"></iframe>`;
+
+        html = html.replace(/style=\"[^\"]+\"/i, 'style="top: 0px; left: 0px; width: 100%; height: 100%; position: absolute;"'); 
+
         var aspect = 4/3;
         html = '<div style="left: 0px; width: 100%; height: 0px; position: relative; padding-bottom: ' +
             Math.round(1000 * 100 / aspect) / 1000
@@ -36,6 +32,7 @@ export default {
 
     tests: [
         "https://issuu.com/redbulletin.com/docs/the_red_bulletin_stratos_special_us",
+        "https://issuu.com/cambridgenews/docs/cambridge_news_march_26_2026",
         "https://issuu.com/_dca/docs/edicio_n_diario_de_centro_ame_rica_02_de_junio_de_20",
         "https://issuu.com/visitfaroeislands/docs/tg25-uk-web_compressed",
         "https://issuu.com/cbmicheals/docs/wwii_hero_franklin_micheals",
