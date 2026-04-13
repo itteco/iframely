216e38290dc3ec563623fef9839a793d12680c0b
Alexey <siniakinaa@gmail.com>
2026-04-13 12:36:37 +0300

    23video-hosted: remove direct twentythree.com test URLs

--- plugins/links/hosted/23video-hosted.js
+++ plugins/links/hosted/23video-hosted.js
@@ -18,11 +18,8 @@ export default {
 
     tests: [
         "https://video.itu.dk/live/13796543",
-        "https://video.ku.dk/visual-social-media-lab-farida-vis-anne",
-        "https://video.twentythree.net/intro-to-twentythrees-player-builder",
-        "https://www.fftv.no/skipatruljen-s3e3-voss-resort",
-        "https://videos.23video.com/novo-nordisk",
+        "https://video.ku.dk/video/11827941/visual-social-media-lab-farida-vis-anne",
         "https://video.nextconf.eu/video/1880845/data-without-limits",
-        // "http://videos.theconference.se/paul-adams-solving-real-world-problems", 503
+        "https://www.fftv.no/skipatruljen-s3e3-voss-resort",
     ]
 };
