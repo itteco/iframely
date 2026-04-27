b2c0097e15669838f0d83cc92088399226a2c31b
Alexey <siniakinaa@gmail.com>
2026-04-27 17:39:55 +0300

    Dailymotion playlists are not covered by Oembed already

--- plugins/domains/dailymotion.com/dailymotion.playlist.js
+++ plugins/domains/dailymotion.com/dailymotion.playlist.js
@@ -1,23 +1,40 @@
 export default {
 
-    // Canonical playlists are covered by oEmbed `http://www.dailymotion.com/services/oembed`
+    // Dailymotion oEmbed returns empty body for all playlist URLs.
     re: [
+        /^https?:\/\/www\.dailymotion\.com\/playlist\/([a-zA-Z0-9]+)(?:\?.+)?$/i,
         /^https?:\/\/www\.dailymotion\.com\/embed\/playlist\/([a-zA-Z0-9]+)(?:\?.+)?$/i,
         /^https?:\/\/geo\.dailymotion\.com\/player.html\?playlist=([a-zA-Z0-9]+)(?:&.+)?$/i,
     ],
 
-    getData: function(urlMatch, cb, options) {
-        cb(!options.redirectsHistory
+    getData: function(url, urlMatch, cb, options) {
+        var isCanonical = /^https?:\/\/www\.dailymotion\.com\/playlist\//i.test(url);
+        cb(!options.redirectsHistory && !isCanonical
             ? {
                 redirect: `https://www.dailymotion.com/playlist/${urlMatch[1]}`
             } : null);
     },
 
+    // oEmbed returns empty body for playlist URLs — build the player directly.
+    getLinks: function(urlMatch) {
+        return {
+            href: `https://geo.dailymotion.com/player.html?playlist=${urlMatch[1]}`,
+            type: CONFIG.T.text_html,
+            rel: [CONFIG.R.player],
+            "aspect-ratio": 16 / 9
+        };
+    },
+
     tests: [{skipMethods: ['getData']}, {
         noFeeds: true
     },
-        // "https://www.dailymotion.com/playlist/x6hynp",
+        "https://www.dailymotion.com/playlist/x6hynp",
+        "https://www.dailymotion.com/playlist/x6scov",
+
         "https://www.dailymotion.com/embed/playlist/x6hynp",
+        "https://www.dailymotion.com/embed/playlist/x6scov",
+
+        "https://geo.dailymotion.com/player.html?playlist=x6scov",
         "https://geo.dailymotion.com/player.html?playlist=x6hynp"
     ]
 };
