44b2d8c3ee40106b990674d17ca69241a26fd695
Alexey <siniakinaa@gmail.com>
2026-04-29 16:41:58 +0300

    add all mixins

--- plugins/domains/dailymotion.com/dailymotion.playlist.js
+++ plugins/domains/dailymotion.com/dailymotion.playlist.js
@@ -7,22 +7,24 @@ export default {
         /^https?:\/\/geo\.dailymotion\.com\/player.html\?playlist=([a-zA-Z0-9]+)(?:&.+)?$/i,
     ],
 
+    mixins: ['*'],
+
+    // oEmbed returns empty body for playlist URLs — build the player directly.
+    getLink: function(urlMatch) {
+        return {
+            href: `https://geo.dailymotion.com/player.html?playlist=${urlMatch[1]}`,
+            type: CONFIG.T.text_html,
+            rel: CONFIG.R.player,
+            "aspect-ratio": 16 / 9
+        };
+    },
+
     getData: function(url, urlMatch, cb, options) {
         var isCanonical = /^https?:\/\/www\.dailymotion\.com\/playlist\//i.test(url);
         cb(!options.redirectsHistory && !isCanonical
             ? {
                 redirect: `https://www.dailymotion.com/playlist/${urlMatch[1]}`
             } : null);
-    },
-
-    // oEmbed returns empty body for playlist URLs — build the player directly.
-    getLinks: function(urlMatch) {
-        return {
-            href: `https://geo.dailymotion.com/player.html?playlist=${urlMatch[1]}`,
-            type: CONFIG.T.text_html,
-            rel: [CONFIG.R.player],
-            "aspect-ratio": 16 / 9
-        };
     },
 
     tests: [{skipMethods: ['getData']}, {
@@ -34,7 +36,7 @@ export default {
         "https://www.dailymotion.com/embed/playlist/x6hynp",
         "https://www.dailymotion.com/embed/playlist/x6scov",
 
-        "https://geo.dailymotion.com/player.html?playlist=x6scov",
-        "https://geo.dailymotion.com/player.html?playlist=x6hynp"
+        "https://geo.dailymotion.com/player.html?playlist=x6hynp",
+        "https://geo.dailymotion.com/player.html?playlist=x6scov"
     ]
 };
