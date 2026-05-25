23b6003ca284004a1307194ea5b684c7528a06ee
Ivan Paramonau <i.paramonau@gmail.com>
2026-05-25 15:40:21 -0400

    Revert "delete scribd fallback"
    This reverts commit 2be8da1919f5b69a5fe0a6586122f2acab4fcd80.

---
+++ plugins/domains/scribd.com/scribd.com-error.js
@@ -0,0 +1,48 @@
+import * as URL from "url";
+
+export default {
+
+    re: 'scribd.com',
+
+    provides: ["scribdData"],
+
+    getData: function(oembedError, cb, url, options, urlMatch, request) {
+
+        if (oembedError === 401 && /(\?|&)secret_password=/.test(url)) {
+
+            var secret = URL.parse(url, true).query["secret_password"];
+
+            request({
+                uri: `https://www.scribd.com/doc-page/embed-modal-props/${urlMatch[2]}?secret_password=${secret}`,
+                json: true,
+                prepareResult: function(error, b, data, callback) {
+
+                    if (error) {
+                        return cb(error);
+                    }
+
+                    if (data.access_key) {
+
+                        return cb(null, {
+                            scribdData: {
+                                href: `https://www.scribd.com/embeds/${urlMatch[2]}/content?start_page=1&view_mode=scroll&access_key=${data.access_key}`,
+                                aspect: data.aspect_ratio
+                            },
+                        })
+
+                    } else {
+
+                        return cb(null, {
+                            message: "Scribd doesn't support embedding of private documents"
+                        })
+
+                    }
+                }
+            }, cb);
+        } else {
+            return cb(null, null)
+        }
+
+    },
+
+};
