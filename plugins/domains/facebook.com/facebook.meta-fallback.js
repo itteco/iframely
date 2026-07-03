da8f43ebe3a1d0993ef84016b7b47e28fe46260d
Ivan Paramonau <i.paramonau@gmail.com>
2026-07-03 06:46:03 -0400

    Fix: FB's 404 on valid posts with visibility restrictions

--- plugins/domains/facebook.com/facebook.meta-fallback.js
+++ plugins/domains/facebook.com/facebook.meta-fallback.js
@@ -9,7 +9,8 @@ export default {
 
     getData: function(url, __statusCode, options, cb) {
 
-        return __statusCode !== 429 && __statusCode !== 403 &&__statusCode !== 508
+        return __statusCode !== 429 && __statusCode !== 403 &&__statusCode !== 508 
+                && __statusCode !== 404 // Real 404s are handled by `fb-error`, the redirects to "/unsupportedbrowser" are likely due to the requirement to be logged in, allowing it.
 
             ? cb({
                 responseStatusCode: __statusCode,
@@ -17,7 +18,7 @@ export default {
 
             : cb(null, {
                 meta: {},
-                message: `Facebook is rate-limiting. Meta disabled.`
+                message: __statusCode !== 404 ? 'Facebook is rate-limiting. Meta disabled.' : 'Post visibility seem restricted. Allowing.'
             })
     }
 };
