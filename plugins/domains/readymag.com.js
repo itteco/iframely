e28078de4cd93431de16fb32990a1d629b7abafc
Alexey <siniakinaa@gmail.com>
2026-07-15 15:18:34 +0300

    readymag.com: add 'else'

--- plugins/domains/readymag.com.js
+++ plugins/domains/readymag.com.js
@@ -9,14 +9,15 @@ export default {
     getLink: function(url, headers) {
         if (headers && headers['x-frame-options'] && /^(deny|sameorigin)$/i.test(headers['x-frame-options'])) {
             return {message: 'Enable embedding in your Readymag settings first'};
+        } else {
+            return {
+                // https://help.readymag.com/hc/en-us/articles/4417292690587-Embedding-project-as-iframe
+                href: url,
+                accept: CONFIG.T.text_html, // Headers can prevent iFrame'ing
+                rel: [CONFIG.R.app, CONFIG.R.iframely],
+                'aspect-ratio': 4/3
+            };
         }
-        return {
-            // https://help.readymag.com/hc/en-us/articles/4417292690587-Embedding-project-as-iframe
-            href: url,
-            accept: CONFIG.T.text_html, // Headers can prevent iFrame'ing
-            rel: [CONFIG.R.app, CONFIG.R.iframely],
-            'aspect-ratio': 4/3
-        };
     },
 
     tests: [
